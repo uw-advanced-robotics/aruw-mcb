2154d82cc05f345056b8af50441e8d2ab8c9718b
Matthew <mma35@uw.edu>
2019-11-22 02:06:49 -0800

    starting to consider best way of implementing agitator unjamming with command framework, either some sort of command group or a state machine

--- mcb-2019-2020-project/src/main.cpp
+++ mcb-2019-2020-project/src/main.cpp
@@ -28,6 +28,17 @@ int count = 0;
 
 aruwsrc::control::ExampleCommand* commandWatch;
 
+typedef enum {
+    WAITING,
+    RUNNING,
+    BACK_UNJAM,
+    FORWARD_UNJAM
+} AgitatorUnjamStates;
+
+AgitatorUnjamStates UJ;
+
+
+
 int main()
 {
     Board::initialize();
@@ -51,8 +62,62 @@ int main()
     // reinterpret_cast<aruwsrc::control::AgitatorRotateCommand*>(frictionWheelDefaultCommand.getPointer())->initialize();
     bool prevRead = false;
 
+    if (1 && !1)  // person tells agitator to rotate
+    {
+        modm::SmartPointer rotateAgitator(new AgitatorRotateCommand(&agitator17mm, PI / 2.0f));
+        aruwlib::control::CommandScheduler::addCommand(rotateAgitator);
+    }
+    if (agitator17mm.isAgitatorJammed())
+    {
+        modm::SmartPointer unjamAgitator(new AgitatorRotateCommand(&agitator17mm, - PI / 2.0f));
+        aruwlib::control::CommandScheduler::addCommand(unjamAgitator);
+    }
+    if (1 /*agitator back unjam complete*/)
+    {
+        // agitator foward unjam
+    }
+    if (1 /*that is done*/)
+    {
+        // if that is done without jam, it is unjammed, otherwise, go back to jam backwards state
+    }
+    // we either make the unjam command cover everything necessary for an unjam or we add some sort
+    // of command group/state machine
+
     while (1)
     {
+        switch(UJ)
+        {
+            case WAITING:
+            {
+                if (1)
+                {
+                    modm::SmartPointer rotateAgitator(
+                        new AgitatorRotateCommand(&agitator17mm, PI / 2.0f));
+                    aruwlib::control::CommandScheduler::addCommand(rotateAgitator);
+                }
+
+                break;
+            }
+            case RUNNING:
+            {
+                if (agitator17mm.isAgitatorJammed())
+                {
+                    modm::SmartPointer unjamAgitator(
+                        new AgitatorRotateCommand(&agitator17mm, - PI / 2.0f));
+                    aruwlib::control::CommandScheduler::addCommand(unjamAgitator);
+                }
+                break;
+            }
+            case BACK_UNJAM:
+            {
+                break;
+            }
+            case FORWARD_UNJAM:
+            {
+                break;
+            }
+        };
+
         if (Board::Button::read() == true && !prevRead)
         {
             agitator17mm.agitatorCalibrateHere();
