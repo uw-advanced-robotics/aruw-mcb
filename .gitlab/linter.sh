713b38943e91c1230d065fcee157470ac89059a6
MoSadie <MoSadie@users.noreply.github.com>
2019-11-21 18:52:11 -0800

    Add docs

--- .gitlab/linter.sh
+++ .gitlab/linter.sh
@@ -5,7 +5,7 @@ listTests=(
   "cpplint ./mcb-2019-2020-project/rm-dev-board-a/* ./mcb-2019-2020-project/*"
   "echo"
   "echo cppcheck checks:  ================================================================"
-  "cppcheck ./mcb-2019-2020-project/src ./mcb-2019-2020-project/robot-type ./mcb-2019-2020-project/rm-dev-board-a --enable=all"
+  "cppcheck ./mcb-2019-2020-project/src ./mcb-2019-2020-project/robot-type ./mcb-2019-2020-project/rm-dev-board-a --rules-file=./.gitlab/rules.xml --enable=all --error-exitcode=1"
 )
 for test in "${listTests[@]}"
 do
