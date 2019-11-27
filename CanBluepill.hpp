b805e69159a13713cca892ce200eb80383fc5686
Yeon Joon <yjung19@uw.edu>
2019-11-27 02:44:49 +0000

    Add new file

---
+++ CanBluepill.hpp
@@ -0,0 +1,60 @@
+#ifndef __CAN_BLUEPILL__
+#define __CAN_BLUEPILL__
+
+#include <dji_motor_tx_handler.hpp>
+#include <can_rx_listener.hpp> 
+#include "src/communication/can/can_rx_handler.hpp"
+#include <stdio.h>
+
+namespace aruwlib
+{
+
+    namespace can
+    {
+
+        /** This class extends the CanRxListner.
+         * It has functionality to store and send can messages from CAN1 with valid ids
+         * It can pack a can message with 4 ints
+         * Further it allows for recieving and storing a valid can message, i.e. a message
+         * with valid id
+         */
+        class CanBluepill: public CanRxListner { ;
+            public:
+            
+            /** Creates a can object with given id 
+             * Calls attachReceiveHandler to add to list of on recieve functions
+             */
+            CanBluepill(uint32_t id); 
+            
+            /** This function recives a can message and if the id is valid
+            * stores the message in the global can message
+            */  
+            void processMessage(const modm::can::Message& message); //rx
+
+            /** Given an id, length and four integers, 
+             * packet that into can message and send it 
+             */ 
+            void transferMessage(modm::can::Message can, int len, int16_t val1, int16_t val2 , int16_t val3 , int16_t val4, bool can1Test); //tx
+            
+            void printCanMsg();
+            
+            modm::can::Message can;
+
+            modm::can::Message mcbMessage; 
+
+            private:
+            /** Stores data into the global can message 
+             */ 
+            void storeMessage(const modm::can::Message& message);
+
+            /** Packs the msg.bug[] with the given int byte by byte in little endian
+             */ 
+            void packArray(int16_t x, int byteSize, int startingIndex);//packs arrays
+
+        }
+
+    }
+
+}
+
+
