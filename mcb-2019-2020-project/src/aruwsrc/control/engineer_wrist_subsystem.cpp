020f5f407b1abfdbeb3aff96d3499828c9314f86
Ryan Shappell <rshapp@uw.edu>
2020-01-11 14:54:47 -0800

    start outlining subsystem

---
+++ mcb-2019-2020-project/src/aruwsrc/control/engineer_wrist_subsystem.cpp
@@ -0,0 +1,13 @@
+#include "engineer_wrist_subsystem.hpp"
+
+namespace aruwsrc
+{
+
+namespace control
+{
+
+
+
+}  // namespace control
+
+}  // namespace aruwsrc
---
+++ mcb-2019-2020-project/src/aruwsrc/control/engineer_wrist_subsystem.hpp
@@ -0,0 +1,62 @@
+#ifndef __ENGINEER_WRIST_SUBSYSTEM_HPP__
+#define __ENGINEER_WRIST_SUBSYSTEM_HPP__
+
+#include <modm/math/filter/pid.hpp>
+#include "src/aruwlib/control/command_scheduler.hpp"
+#include "src/aruwlib/control/subsystem.hpp"
+#include "src/aruwlib/motor/dji_motor.hpp"
+
+using namespace aruwlib::control;
+
+namespace aruwsrc
+{
+
+namespace control
+{
+
+class EngineerWristSubsystem : public Subsystem
+{
+ public:
+    EngineerWristSubsystem(
+        aruwlib::motor::MotorId leftMotorId = LEFT_MOTOR_ID,
+        aruwlib::motor::MotorId rightMotorId = RIGHT_MOTOR_ID
+    );
+    
+    void refresh(void);
+
+    void setWristAngle(float newAngle);
+
+ private:
+    static const aruwlib::motor::MotorId LEFT_MOTOR_ID = aruwlib::motor::MOTOR1;
+    static const aruwlib::motor::MotorId RIGHT_MOTOR_ID = aruwlib::motor::MOTOR2;
+    const aruwlib::can::CanBus CAN_BUS_MOTORS = aruwlib::can::CanBus::CAN_BUS1;
+
+    // from the perspective of the grabber, i.e. one will be inverted
+    // motors
+    aruwlib::motor::DjiMotor leftMotor;
+    aruwlib::motor::DjiMotor rightMotor;
+
+    // PID values
+    const float PID_P = 10.0f;
+    const float PID_I = 0.0f;
+    const float PID_D = 0.0f;
+    const float PID_MAX_ERROR_SUM = 0.0f;
+    const float PID_MAX_OUTPUT = 16000;
+
+    modm::Pid<float> leftPositionPid;
+    modm::Pid<float> rightPositionPid;
+
+    float desiredWristAngle;
+
+    float wristCalibrationAngle;
+
+    bool wristIsCalibrated;
+
+    void wristRunPositionPid(void);
+};
+
+}  // namespace control
+
+}  // namespace aruwsrc
+
+#endif //  __ENGINEER_WRIST_SUBSYSTEM_HPP__
