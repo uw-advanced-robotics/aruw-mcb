bfff71427367c8e7fc453470bbc4033252a7ca36
Matthew <mma35@uw.edu>
2020-01-12 22:23:28 -0800

    tested, agitator still works

--- mcb-2019-2020-project/src/main.cpp
+++ mcb-2019-2020-project/src/main.cpp
@@ -59,11 +59,11 @@ int main()
     // modm::SmartPointer rotateCommand(new AgitatorRotateCommand(&agitator17mm, aruwlib::algorithms::PI / 5));
     modm::SmartPointer shootCommand(new ShootSteadyComprisedCommand(&agitator17mm, aruwlib::algorithms::PI / 5, aruwlib::algorithms::PI / 2));
 
-    CommandScheduler::addComprisedCommand(shootCommand);
-    CommandScheduler::removeComprisedCommand(shootCommand, false);
-    CommandScheduler::addComprisedCommand(shootCommand);
-    CommandScheduler::addComprisedCommand(shootCommand);
-    CommandScheduler::addCommand(spinFrictionWheelCommand);
+    // CommandScheduler::addComprisedCommand(shootCommand);
+    // CommandScheduler::removeComprisedCommand(shootCommand, false);
+    // CommandScheduler::addComprisedCommand(shootCommand);
+    // CommandScheduler::addComprisedCommand(shootCommand);
+    // CommandScheduler::addCommand(spinFrictionWheelCommand);
 
     while (1)
     {
