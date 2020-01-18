3c51b02f37c99701144c7bacc7fdc71d6804b1b2
Ryan Shappell <rshapp@uw.edu>
2020-01-18 14:03:11 -0800

    add subsystem hpp and cpp

---
+++ mcb-2019-2020-project/src/aruwsrc/control/engineer/engineer_17mm_reservoir_subsystem.cpp
@@ -0,0 +1,13 @@
+#include "engineer_17mm_reservoir_subsystem.hpp"
+
+namespace aruwsrc
+{
+
+namespace control
+{
+
+
+
+}  // namespace control
+
+}  // namespace aruwsrc
---
+++ mcb-2019-2020-project/src/aruwsrc/control/engineer/engineer_17mm_reservoir_subsystem.hpp
@@ -0,0 +1,16 @@
+#ifndef __ENGINEER_17MM_RESERVOIR_SUBSYSTEM_HPP__
+#define __ENGINEER_17MM_RESERVOIR_SUBSYSTEM_HPP__
+
+namespace aruwsrc
+{
+
+namespace control
+{
+
+    
+
+}  // namespace control
+
+}  // namespace aruwsrc
+
+#endif  // __ENGINEER_17MM_RESERVOIR_SUBSYSTEM_HPP__
