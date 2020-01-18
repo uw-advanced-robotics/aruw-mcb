c4f28437ff14d61720276f9a6ed388ef5b2990da
Ryan Shappell <rshapp@uw.edu>
2020-01-18 15:18:58 -0800

    initial outlining of cpp, still need to do calibration calc

--- mcb-2019-2020-project/src/aruwsrc/control/engineer/engineer_17mm_reservoir_subsystem.cpp
+++ mcb-2019-2020-project/src/aruwsrc/control/engineer/engineer_17mm_reservoir_subsystem.cpp
@@ -5,8 +5,66 @@ namespace aruwsrc
 
 namespace control
 {
+    Engineer17mmReservoirSubsystem::Engineer17mmReservoirSubsystem() :
+        reservoirMotor(RESERVOIR_MOTOR_ID, CAN_BUS_MOTORS, false),
+        reservoirPositionPid(PID_P, PID_I, PID_D, PID_MAX_ERROR_SUM, PID_MAX_OUTPUT),
+        desiredReservoirAngle(0.0f),
+        reservoirCalibrationAngle(0.0f),
+        reservoirIsCalibrated(false)
+    {}
 
+    void Engineer17mmReservoirSubsystem::refresh()
+    {
+        reservoirRunPositionPid();
+    }
 
+    void Engineer17mmReservoirSubsystem::setReservoirAngle(float newAngle)
+    {
+        desiredReservoirAngle = newAngle;
+    }
+
+    float Engineer17mmReservoirSubsystem::getReservoirAngle() const
+    {
+        if (!reservoirIsCalibrated)
+        {
+            return 0.0f;
+        }
+        // TODO, angle calculation
+    }
+
+    float Engineer17mmReservoirSubsystem::getReservoirDesiredAngle() const
+    {
+        return desiredReservoirAngle;
+    }
+
+    bool Engineer17mmReservoirSubsystem::reservoirCalibrateHere()
+    {
+        if (!reservoirMotor.isMotorOnline())
+        {
+            return false;
+        }
+        // TODO: determine calibration calculation
+        reservoirCalibrationAngle = ?;
+        reservoirIsCalibrated = true;
+        return true;
+    }
+
+    void Engineer17mmReservoirSubsystem::reservoirRunPositionPid()
+    {
+        if (!reservoirIsCalibrated)
+        {
+            reservoirPositionPid.reset();
+            return;
+        }
+        // TODO: determine calculation
+        reservoirPositionPid.update(desiredReservoirAngle - ?);
+        reservoirMotor.setDesiredOutput(reservoirPositionPid.getValue());
+    }
+
+    float Engineer17mmReservoirSubsystem::getUncalibratedReservoirAngle() const
+    {
+        // TODO:
+    }
 
 }  // namespace control
 
