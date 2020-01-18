caffcfe274cffce42b0cde37c34472fbb5a0787e
Ryan Shappell <rshapp@uw.edu>
2020-01-18 15:01:54 -0800

    outline most of the subsystem hpp

--- mcb-2019-2020-project/src/aruwsrc/control/engineer/engineer_17mm_reservoir_subsystem.hpp
+++ mcb-2019-2020-project/src/aruwsrc/control/engineer/engineer_17mm_reservoir_subsystem.hpp
@@ -1,13 +1,61 @@
 #ifndef __ENGINEER_17MM_RESERVOIR_SUBSYSTEM_HPP__
 #define __ENGINEER_17MM_RESERVOIR_SUBSYSTEM_HPP__
 
+#include <modm/math/filter/pid.hpp>
+#include "src/aruwlib/control/subsystem.hpp"
+#include "src/aruwlib/motor/dji_motor.hpp"
+
+using namespace aruwlib::control;
+
 namespace aruwsrc
 {
 
 namespace control
 {
 
-    
+class Engineer17mmReservoirSubsystem : public Subsystem
+{
+ public:
+    Engineer17mmReservoirSubsystem();
+
+    void refresh(void);
+
+    void setReservoirAngle(float newAngle);
+
+    float getReservoirAngle() const;
+
+    float getReservoirDesiredAngle() const;
+
+    bool reservoirCalibrateHere(void);
+ private:
+    static constexpr float RESERVOIR_GEAR_RATIO = 36.0f;
+
+    // TODO: update all engineer motor assignments
+    // might need another motor to dump the ammo
+    static const aruwlib::motor::MotorId RESERVOIR_MOTOR_ID = aruwlib::motor::MOTOR1;
+    const aruwlib::can::CanBus CAN_BUS_MOTORS = aruwlib::can::CanBus::CAN_BUS1;
+
+    aruwlib::motor::DjiMotor reservoirMotor;
+
+    // PID values
+    const float PID_P = 10.0f;
+    const float PID_I = 0.0f;
+    const float PID_D = 0.0f;
+    const float PID_MAX_ERROR_SUM = 0.0f;
+    const float PID_MAX_OUTPUT = 16000;
+
+    modm::Pid<float> reservoirPositionPid;
+
+    float desiredReservoirAngle;
+
+    float reservoirCalibrationAngle;
+
+    bool reservoirIsCalibrated;
+
+    void reservoirRunPositionPid(void);
+
+    float getUncalibratedReservoirAngle(void) const;
+};
 
 }  // namespace control
 
