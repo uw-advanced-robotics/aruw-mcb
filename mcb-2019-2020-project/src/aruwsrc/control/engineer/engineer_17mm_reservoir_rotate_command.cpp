f9b29cef3fad820adc487887709e6aa427a5ca41
Ryan Shappell <rshapp@uw.edu>
2020-01-21 19:54:34 -0800

    initial rotate command cpp

--- mcb-2019-2020-project/src/aruwsrc/control/engineer/engineer_17mm_reservoir_rotate_command.cpp
+++ mcb-2019-2020-project/src/aruwsrc/control/engineer/engineer_17mm_reservoir_rotate_command.cpp
@@ -1,11 +1,52 @@
-namespace aruwlib
+#include "engineer_17mm_reservoir_rotate_command.hpp"
+
+namespace aruwsrc
 {
 
 namespace control
 {
+    Engineer17mmReservoirRotateCommand::Engineer17mmReservoirRotateCommand(
+        Engineer17mmReservoirSubsystem* reservoir,
+            float reservoirAngleChange,
+            float reservoirRotateTime) :
+            reservoirTargetChange(reservoirAngleChange),
+            reservoirRotateSetpoint(
+                RESERVOIR_ROTATE_COMMAND_PERIOD * reservoirAngleChange / reservoirRotateTime,
+                RESERVOIR_ROTATE_COMMAND_PERIOD * reservoirAngleChange / reservoirRotateTime, 0),
+            reservoirDesiredRotateTime(reservoirRotateTime),
+            reservoirMinRotateTime(RESERVOIR_MIN_ROTATE_TIME)
+        {
+            this->addSubsystemRequirement(reinterpret_cast<aruwlib::control::Subsystem*>(reservoir));
+            connectedReservoir = reservoir;
+        }
 
+    void Engineer17mmReservoirRotateCommand::initialize()
+    {
+        reservoirRotateSetpoint.reset(connectedReservoir->getReservoirAngle());
+        reservoirRotateSetpoint.setTarget(connectedReservoir->getReservoirAngle() + reservoirTargetChange);
 
+        reservoirMinRotateTime.restart(RESERVOIR_MIN_ROTATE_TIME);
+    }
 
+    void Engineer17mmReservoirRotateCommand::execute()
+    {
+        reservoirRotateSetpoint.update();
+        connectedReservoir->setReservoirAngle(reservoirRotateSetpoint.getValue());
+    }
+
+    void Engineer17mmReservoirRotateCommand::end(bool interrupted)
+    {
+        // nothing yet??
+    }
+
+    bool Engineer17mmReservoirRotateCommand::isFinished() const
+    {
+        return fabs(static_cast<double>(connectedReservoir->getReservoirAngle()
+         - connectedReservoir->getReservoirDesiredAngle()))
+         < static_cast<double>(RESERVOIR_SETPOINT_TOLERANCE)
+         && reservoirRotateSetpoint.isTargetReached()
+         && reservoirMinRotateTime.isExpired();
+    }
 }  // namespace control
 
-}  // namespace aruwlib
+}  // namespace aruwsrc
