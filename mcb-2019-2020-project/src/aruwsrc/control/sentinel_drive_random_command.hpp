31a84db49ab5c07445bd1d47410b70ae7c2e27cc
Austin Jenchi <ajenchi9@gmail.com>
2020-01-21 19:36:04 -0800

    Basic random sentinel drive

---
+++ mcb-2019-2020-project/src/aruwsrc/control/sentinel_drive_random_command.hpp
@@ -0,0 +1,65 @@
+#ifndef __COMMAND_SENTINEL_DRIVE_RANDOM_HPP__
+#define __COMMAND_SENTINEL_DRIVE_RANDOM_HPP__
+
+#include "src/aruwlib/control/command.hpp"
+#include "modm/processing/timer.hpp"
+
+using namespace aruwlib::control;
+
+namespace aruwsrc
+{
+
+namespace control
+{
+
+class SentinelDriveSubsystem;
+
+class SentinelDriveRandomCommand : public Command
+{
+ public:
+    explicit SentinelDriveRandomCommand(SentinelDriveSubsystem* subsystem = nullptr);
+
+    /**
+      * The initial subroutine of a command.  Called once when the command is
+      * initially scheduled.
+      */
+    void initialize(void);
+
+    /**
+      * The main body of a command.  Called repeatedly while the command is
+      * scheduled.
+      */
+    void execute(void);
+
+    /**
+      * The action to take when the command ends.  Called when either the command
+      * finishes normally, or when it interrupted/canceled.
+      *
+      * @param interrupted whether the command was interrupted/canceled
+      */
+    void end(bool interrupted);
+
+    /**
+      * Whether the command has finished.  Once a command finishes, the scheduler
+      * will call its end() method and un-schedule it.
+      *
+      * @return whether the command has finished.
+      */
+    bool isFinished(void) const;
+
+    void interrupted(void);
+
+ private:
+    static const int16_t MAX_RPM = 1000;
+    static const float RAIL_BUFFER = 0.1 * SentinelDriveSubsystem::RAIL_LENGTH;
+
+    float currentRPM = 0;
+
+    SentinelDriveSubsystem* subsystemSentinelDrive;
+    modm::ShortTimeout sleepTimeout;
+};
+
+}  // namespace control
+
+}  // namespace aruwsrc
+#endif
---
+++ mcb-2019-2020-project/src/aruwsrc/control/sentinel_drive_subsystem.cpp
@@ -0,0 +1,68 @@
+#include "sentinel_drive_subsystem.hpp"
+
+namespace aruwsrc
+{
+
+namespace control
+{
+    const aruwlib::motor::MotorId SentinelDriveSubsystem::LEFT_MOTOR_ID = aruwlib::motor::MOTOR4;
+    const aruwlib::motor::MotorId SentinelDriveSubsystem::RIGHT_MOTOR_ID = aruwlib::motor::MOTOR5;
+
+    void SentinelDriveSubsystem::setDesiredRpm(float desRpm)
+    {
+        desiredRpm = desRpm;
+    }
+
+    void SentinelDriveSubsystem::refresh()
+    {
+        updateMotorRpmPid(
+            &velocityPidLeftWheel,
+            &leftWheel, desiredRpm
+        );
+        updateMotorRpmPid(
+            &velocityPidRightWheel,
+            &rightWheel, desiredRpm
+        );
+    }
+
+    void SentinelDriveSubsystem::updateMotorRpmPid(
+        modm::Pid<float>* pid,
+        aruwlib::motor::DjiMotor* motor,
+        float desiredRpm
+    ) {
+        pid->update(desiredRpm - motor->getShaftRPM());
+        motor->setDesiredOutput(pid->getValue());
+    }
+
+    // Returns the absolute position of the sentinel on the rail with respect to the first rail end that was hit
+    float SentinelDriveSubsystem::absolutePosition () 
+    {
+        return ((distanceFromEncoder(&this->leftWheel) - leftZeroRailOffset) +
+            (distanceFromEncoder(&this->rightWheel) - rightZeroRailOffset)) / 2;
+    }  
+
+    // Call when rail is hit to set the offset or the 
+    // Sets the offset field value every time a rails is hit
+    // Only sets the offset when the rail end that was first hit is hit again, or a rail end
+    // is hit for the first time
+    void SentinelDriveSubsystem::resetOffsetFromLimitterSwitch ()
+    {
+        if(Board::AnalogInPinS::read()){// DigitalPin where LimitterSwitch is placed 
+         leftZeroRailOffset = distanceFromEncoder(&this->leftWheel);
+         rightZeroRailOffset = distanceFromEncoder(&this->rightWheel);
+        }
+    }
+
+    // Returns the distance covered by the sentinel wheel on the rail 
+    // with respect to the encoders
+    // Equation used: Arc Length = Angle * numberOfRotations * radius
+    // Here we get the radius from the getEncoderUnwrapped function
+    float SentinelDriveSubsystem::distanceFromEncoder(aruwlib::motor::DjiMotor* motor){
+        float unwrappedAngle = motor->encStore.getEncoderUnwrapped();
+        float numberOfRotations = unwrappedAngle / 8192.0f;
+        return numberOfRotations * 2.0f * (atan(1)*4) * WHEEL_RADIUS;
+    }
+
+}  // namespace control
+
+}  // namespace aruwsrc
