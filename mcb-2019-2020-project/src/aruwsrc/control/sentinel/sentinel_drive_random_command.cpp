c67b010c89551d3e1a122396c2dca7b18d9f97c7
Austin Jenchi <ajenchi9@gmail.com>
2020-02-06 20:01:12 -0800

    don't lint the thread safe-ness of rand()

--- mcb-2019-2020-project/src/aruwsrc/control/sentinel/sentinel_drive_random_command.cpp
+++ mcb-2019-2020-project/src/aruwsrc/control/sentinel/sentinel_drive_random_command.cpp
@@ -21,7 +21,7 @@ namespace control
     {
         if (this->sleepTimeout.isExpired()) {
             this->sleepTimeout.restart(CHANGE_TIME_INTERVAL);
-            currentRPM = rand() % MAX_RPM + 1;
+            currentRPM = rand() % MAX_RPM + 1;  // NOLINT
             if (currentRPM < MAX_RPM / 2) {
                 currentRPM -= MAX_RPM;
             }
