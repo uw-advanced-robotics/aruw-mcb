ade11be7c2fe2ab17bd496ef6f5ae21750eec082
David Le <davidle0@uw.edu>
2020-02-17 17:46:20 -0800

    starting work on properties component

---
+++ mcb-2019-2020-project/src/serial_properties_handler.hpp
@@ -0,0 +1,16 @@
+#ifndef SERIAL_PROPERTIES_HANDLER_HPP
+#define SERIAL_PROPERTIES_HANDLER_HPP
+
+#include "src/serial_data_logger.hpp"
+
+namespace src
+{
+
+namespace prophandler
+{
+
+} // namespace prophandler
+
+} // namespace src
+
+#endif
