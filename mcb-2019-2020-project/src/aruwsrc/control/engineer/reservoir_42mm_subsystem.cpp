c7ea56d75527528fd6704adda3c5638b2bafb144
Ryan Shappell <rshapp@uw.edu>
2020-02-20 20:29:24 -0800

    subsystem complete

---
+++ mcb-2019-2020-project/src/aruwsrc/control/engineer/reservoir_42mm_subsystem.cpp
@@ -0,0 +1,77 @@
+#include "reservoir_42mm_subsystem.hpp"
+
+namespace aruwsrc
+{
+
+namespace engineer
+{
+    Reservoir42mmSubsystem::Reservoir42mmSubsystem() :
+        reservoirMotor(RESERVOIR_42MM_MOTOR_ID, CAN_BUS_MOTORS, false),
+        positionPid(PID_P, PID_I, PID_D, PID_MAX_ERROR_SUM, PID_MAX_OUTPUT),
+        desiredReservoirAngle(0.0f),
+        reservoirCalibratedAngle(0.0f),
+        reservoirIsCalibrated(false)
+    {}
+
+    void Reservoir42mmSubsystem::refresh()
+    {
+        if (reservoirIsCalibrated)
+        {
+            reservoirRunPositionPid();
+        }
+        else
+        {
+            reservoirCalibrateHere();
+        }
+    }
+
+    float Reservoir42mmSubsystem::getAngle() const
+    {
+        if (!reservoirIsCalibrated)
+        {
+            return 0.0f;
+        }
+        return getUncalibratedReservoirAngle() - reservoirCalibratedAngle;
+    }
+
+    float Reservoir42mmSubsystem::getDesiredAngle() const
+    {
+        return desiredReservoirAngle;
+    }
+
+    void Reservoir42mmSubsystem::setDesiredAngle(float newAngle)
+    {
+        desiredReservoirAngle = newAngle;
+    }
+
+    bool Reservoir42mmSubsystem::reservoirCalibrateHere()
+    {
+        if (!reservoirMotor.isMotorOnline())
+        {
+            return false;
+        }
+        reservoirCalibratedAngle = getUncalibratedReservoirAngle();
+        reservoirIsCalibrated = true;
+        return true;
+    }
+
+    void Reservoir42mmSubsystem::reservoirRunPositionPid()
+    {
+        if (!reservoirIsCalibrated)
+        {
+            positionPid.reset();
+            return;
+        }
+        positionPid.update(desiredReservoirAngle - getAngle());
+        reservoirMotor.setDesiredOutput(positionPid.getValue());
+    }
+
+    float Reservoir42mmSubsystem::getUncalibratedReservoirAngle() const
+    {
+        return (2.0f * aruwlib::algorithms::PI / static_cast<float>(ENC_RESOLUTION)) *
+            reservoirMotor.encStore.getEncoderUnwrapped() / RESERVOIR_42MM_GEAR_RATIO;
+
+    }
+}  // namespace engineer
+
+}  // namespace aruwsrc
---
+++ mcb-2019-2020-project/src/aruwsrc/control/engineer/reservoir_42mm_subsystem.hpp
@@ -0,0 +1,65 @@
+#ifndef __RESERVOIR_42MM_SUBSYSTEM_HPP__
+#define __RESERVOIR_42MM_SUBSYSTEM_HPP__
+
+#include <modm/math/filter/pid.hpp>
+#include "src/aruwlib/control/command_scheduler.hpp"
+#include "src/aruwlib/control/subsystem.hpp"
+#include "src/aruwlib/motor/dji_motor.hpp"
+#include "src/aruwlib/algorithms/math_user_utils.hpp"
+
+using namespace aruwlib::control;
+
+namespace aruwsrc
+{
+
+namespace engineer
+{
+
+class Reservoir42mmSubsystem : public aruwlib::control::Subsystem
+{
+ public:
+    Reservoir42mmSubsystem();
+
+    void refresh();
+
+    float getAngle() const;  
+
+    float getDesiredAngle() const;
+
+    void setDesiredAngle(float newAngle);
+
+    bool reservoirCalibrateHere();
+
+ private:
+    static constexpr float RESERVOIR_42MM_GEAR_RATIO = 36.0f;
+
+    static const aruwlib::motor::MotorId RESERVOIR_42MM_MOTOR_ID = aruwlib::motor::MOTOR5;
+    const aruwlib::can::CanBus CAN_BUS_MOTORS = aruwlib::can::CanBus::CAN_BUS1;
+
+    aruwlib::motor::DjiMotor reservoirMotor;
+
+    // PID values
+    const float PID_P = 80000.0f; // TODO
+    const float PID_I = 0.0f;
+    const float PID_D = 800000.0f;
+    const float PID_MAX_ERROR_SUM = 0.0f;
+    const float PID_MAX_OUTPUT = 16000;
+
+    modm::Pid<float> positionPid;
+
+    float desiredReservoirAngle;
+
+    float reservoirCalibratedAngle;
+
+    bool reservoirIsCalibrated;
+
+    void reservoirRunPositionPid(void);
+
+    float getUncalibratedReservoirAngle(void) const;
+};
+
+}  // namespace engineer
+
+}  // namespace aruwsrc
+
+#endif  // __RESERVOIR_42MM_SUBSYSTEM_HPP__
