ea802e1931726d4887f33da14324d19424f5fb0e
Matthew <mma35@uw.edu>
2020-03-09 11:12:28 -0700

    tested changes to edge limiting, works

--- mcb-2019-2020-project/src/aruwsrc/control/sentinel/sentinel_drive_random_command.hpp
+++ mcb-2019-2020-project/src/aruwsrc/control/sentinel/sentinel_drive_random_command.hpp
@@ -34,7 +34,7 @@ class SentinelDriveRandomCommand : public Command
     static const int16_t MIN_RPM = 5000;
     static const int16_t MAX_RPM = 7000;
     static const int16_t CHANGE_TIME_INTERVAL = 1000;
-    static constexpr float RAIL_BUFFER = 0.1f * SentinelDriveSubsystem::RAIL_LENGTH;
+    static constexpr float RAIL_BUFFER = 0.2f * SentinelDriveSubsystem::RAIL_LENGTH;
 
     float currentRPM = 0;
 
