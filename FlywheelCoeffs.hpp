9256e0f30860a78909ab2d98fbb3a08809e4c3cd
Matthew <mma35@uw.edu>
2020-03-13 00:12:54 -0700

    flywheel python

---
+++ FlywheelCoeffs.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <frc/controller/StateSpaceControllerCoeffs.h>
+#include <frc/controller/StateSpaceLoop.h>
+#include <frc/controller/StateSpaceObserverCoeffs.h>
+#include <frc/controller/StateSpacePlantCoeffs.h>
+
+frc::StateSpacePlantCoeffs<1, 1, 1> MakeFlywheelPlantCoeffs();
+frc::StateSpaceControllerCoeffs<1, 1, 1> MakeFlywheelControllerCoeffs();
+frc::StateSpaceObserverCoeffs<1, 1, 1> MakeFlywheelObserverCoeffs();
+frc::StateSpaceLoop<1, 1, 1> MakeFlywheelLoop();
