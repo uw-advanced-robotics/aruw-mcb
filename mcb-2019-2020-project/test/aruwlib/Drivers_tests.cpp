3a82c698ca1f9437c5e9d7677d0b19a069baf686
matthew <mma35@uw.edu>
2020-06-26 20:36:11 -0700

    Update setup and teardown functionality, use a TestEventListenerBase instead of what was used previously.

--- mcb-2019-2020-project/test/aruwlib/Drivers_tests.cpp
+++ mcb-2019-2020-project/test/aruwlib/Drivers_tests.cpp
@@ -3,45 +3,41 @@
 
 using namespace aruwlib;
 
-TEST_CASE("Drivers reset tests")
-{
-    // Setup after each section.
-    Drivers::reset();
+// Each reset test contains two test cases. Change the state
+// of the driver in the first test case, then insure the
+// state of the driver is reset in the second test case.
 
-    // Each reset test contains two sections. Change the state
-    // of the driver in the first section, then insure the
-    // state of the driver is reset in the second section.
+// todo
 
-    SECTION("Test can driver reset") {}
-    SECTION("Test can driver reset, 2") {}
-    SECTION("Test canRxHandler driver reset") {}
-    SECTION("Test canRxHandler driver reset, 2") {}
-    SECTION("Test analog driver reset") {}
-    SECTION("Test analog driver reset, 2") {}
-    SECTION("Test digital driver reset") {}
-    SECTION("Test digital driver reset, 2") {}
-    SECTION("Test leds driver reset") {}
-    SECTION("Test leds driver reset, 2") {}
-    SECTION("Test pwm driver reset") {}
-    SECTION("Test pwm driver reset, 2") {}
-    SECTION("Test remote driver reset") {}
-    SECTION("Test remote driver reset, 2") {}
-    SECTION("Test mpu6500 driver reset") {}
-    SECTION("Test mpu6500 driver reset, 2") {}
-    SECTION("Test uart driver reset") {}
-    SECTION("Test uart driver reset, 2") {}
-    SECTION("Test xavierSerial driver reset") {}
-    SECTION("Test xavierSerial driver reset, 2") {}
-    SECTION("Test refSerial driver reset") {}
-    SECTION("Test refSerial driver reset, 2") {}
-    SECTION("Test commandScheduler driver reset") {}
-    SECTION("Test commandScheduler driver reset, 2") {}
-    SECTION("Test controlOperatorInterface driver reset") {}
-    SECTION("Test controlOperatorInterface driver reset, 2") {}
-    SECTION("Test commandMapper driver reset") {}
-    SECTION("Test commandMapper driver reset, 2") {}
-    SECTION("Test errorController driver reset") {}
-    SECTION("Test errorController driver reset, 2") {}
-    SECTION("Test djiMotorTxHandler driver reset") {}
-    SECTION("Test djiMotorTxHandler driver reset, 2") {}
-}
+TEST_CASE("Test can driver reset") {}
+TEST_CASE("Test can driver reset, 2") {}
+TEST_CASE("Test canRxHandler driver reset") {}
+TEST_CASE("Test canRxHandler driver reset, 2") {}
+TEST_CASE("Test analog driver reset") {}
+TEST_CASE("Test analog driver reset, 2") {}
+TEST_CASE("Test digital driver reset") {}
+TEST_CASE("Test digital driver reset, 2") {}
+TEST_CASE("Test leds driver reset") {}
+TEST_CASE("Test leds driver reset, 2") {}
+TEST_CASE("Test pwm driver reset") {}
+TEST_CASE("Test pwm driver reset, 2") {}
+TEST_CASE("Test remote driver reset") {}
+TEST_CASE("Test remote driver reset, 2") {}
+TEST_CASE("Test mpu6500 driver reset") {}
+TEST_CASE("Test mpu6500 driver reset, 2") {}
+TEST_CASE("Test uart driver reset") {}
+TEST_CASE("Test uart driver reset, 2") {}
+TEST_CASE("Test xavierSerial driver reset") {}
+TEST_CASE("Test xavierSerial driver reset, 2") {}
+TEST_CASE("Test refSerial driver reset") {}
+TEST_CASE("Test refSerial driver reset, 2") {}
+TEST_CASE("Test commandScheduler driver reset") {}
+TEST_CASE("Test commandScheduler driver reset, 2") {}
+TEST_CASE("Test controlOperatorInterface driver reset") {}
+TEST_CASE("Test controlOperatorInterface driver reset, 2") {}
+TEST_CASE("Test commandMapper driver reset") {}
+TEST_CASE("Test commandMapper driver reset, 2") {}
+TEST_CASE("Test errorController driver reset") {}
+TEST_CASE("Test errorController driver reset, 2") {}
+TEST_CASE("Test djiMotorTxHandler driver reset") {}
+TEST_CASE("Test djiMotorTxHandler driver reset, 2") {}
--- mcb-2019-2020-project/test/test_main.cpp
+++ mcb-2019-2020-project/test/test_main.cpp
@@ -1,2 +1,25 @@
 #define CATCH_CONFIG_MAIN
+#include <iostream>
+
+#include <aruwlib/Drivers.hpp>
+
 #include "catch/catch.hpp"
+
+
+struct DriverInitListener : Catch::TestEventListenerBase
+{
+    using TestEventListenerBase::TestEventListenerBase;  // inherit constructor
+
+    void testCaseStarting(Catch::TestCaseInfo const& testInfo) override
+    {
+        // Perform some setup before a test case is run.
+        Drivers::reset();
+        std::cout << "HIEFISJKLSDFKSDF" << std::endl;
+    }
+
+    void testCaseEnded(Catch::TestCaseStats const& testCaseStats) override
+    {
+        // Tear-down after a test case is run.
+    }
+};
+CATCH_REGISTER_LISTENER(DriverInitListener);
