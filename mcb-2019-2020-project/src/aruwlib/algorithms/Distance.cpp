10f0d311c406a6292d0a83b5d8c593c4aadc998a
matthew <mma35@uw.edu>
2020-08-23 22:41:54 -0700

    Added comments and a few more operators to the Distance class.

--- mcb-2019-2020-project/src/aruwlib/algorithms/Distance.cpp
+++ mcb-2019-2020-project/src/aruwlib/algorithms/Distance.cpp
@@ -6,8 +6,7 @@ namespace aruwlib
 {
 namespace algorithms
 {
-
-Distance::Distance() : distance(0.0f), unit(CM) {}
+Distance::Distance() : distance(0.0f), unit(Units::CM) {}
 
 Distance::Distance(float distance, Units unit) : distance(distance), unit(unit) {}
 
@@ -17,62 +16,68 @@ float Distance::asMm() const
 {
     switch (unit)
     {
-        case MM: return distance;
-        case CM: return distance * MM_PER_CM;
-        case M: return distance * MM_PER_CM * CM_PER_M;
-        default: return 0.0f;
+        case Units::MM:
+            return distance;
+        case Units::CM:
+            return distance * MM_PER_CM;
+        case Units::M:
+            return distance * MM_PER_CM * CM_PER_M;
+        default:
+            return 0.0f;
     }
 }
 
 float Distance::asCm() const
 {
-    switch(unit)
+    switch (unit)
     {
-        case MM: return distance / MM_PER_CM;
-        case CM: return distance;
-        case M: return distance * CM_PER_M;
-        default: return 0.0f;
+        case Units::MM:
+            return distance / MM_PER_CM;
+        case Units::CM:
+            return distance;
+        case Units::M:
+            return distance * CM_PER_M;
+        default:
+            return 0.0f;
     }
 }
 
 float Distance::asM() const
 {
-    switch(unit)
+    switch (unit)
     {
-        case MM: return distance / MM_PER_CM / CM_PER_M;
-        case CM: return distance / CM_PER_M;
-        case M: return distance;
-        default: return 0.0f;
+        case Units::MM:
+            return distance / MM_PER_CM / CM_PER_M;
+        case Units::CM:
+            return distance / CM_PER_M;
+        case Units::M:
+            return distance;
+        default:
+            return 0.0f;
     }
 }
 
-Distance Distance::toMm() const
-{
-    return Distance(asMm(), MM);
-}
+Distance Distance::toMm() const { return Distance(asMm(), Units::MM); }
 
-Distance Distance::toCm() const
-{
-    return Distance(asCm(), CM);
-}
+Distance Distance::toCm() const { return Distance(asCm(), Units::CM); }
 
-Distance Distance::toM() const
-{
-    return Distance(asM(), M);
-}
-
+Distance Distance::toM() const { return Distance(asM(), Units::M); }
 
 Distance Distance::operator+(Distance other) const
 {
     float distM = asM();
     float otherDistM = other.asM();
-    Distance sum(distM + otherDistM, M);
-    switch(unit)
+    Distance sum(distM + otherDistM, Units::M);
+    switch (unit)
     {
-        case MM: return sum.toMm();
-        case CM: return sum.toCm();
-        case M: return sum.toM();
-        default: return sum;
+        case Units::MM:
+            return sum.toMm();
+        case Units::CM:
+            return sum.toCm();
+        case Units::M:
+            return sum.toM();
+        default:
+            return sum;
     }
     // return Distance(distM + other)
 }
@@ -83,65 +88,76 @@ Distance Distance::operator-(Distance other) const
     return *this + other;
 }
 
-Distance Distance::operator*(float val) const
-{
-    return Distance(distance * val, unit);
-}
+Distance Distance::operator*(float val) const { return Distance(distance * val, unit); }
 
-Distance Distance::operator/(float val) const
-{
-    return Distance(distance / val, unit);
-}
+Distance Distance::operator*(Distance other) const { return *this * other.distance; }
+
+Distance Distance::operator/(float val) const { return Distance(distance / val, unit); }
+
+Distance Distance::operator/(Distance other) const { return *this / other.distance; }
 
 Distance Distance::operator+=(Distance other)
 {
     *this = *this + other;
+    return *this;
 }
 
 Distance Distance::operator-=(Distance other)
 {
     *this = *this - other;
+    return *this;
 }
 
 Distance Distance::operator*=(float val)
 {
     *this = *this * val;
+    return *this;
+}
+
+Distance Distance::operator*=(Distance other)
+{
+    *this = *this * other;
+    return *this;
 }
 
 Distance Distance::operator/=(float val)
 {
     *this = *this / val;
+    return *this;
 }
 
-bool Distance::operator<(Distance other) const
+Distance Distance::operator/=(Distance other)
 {
-    return asM() - other.asM() < 0.0f;
+    *this = *this / other;
+    return *this;
 }
 
-bool Distance::operator>(Distance other) const
+Distance Distance::operator++()
 {
-    return asM() - other.asM() > 0.0f;
+    this->distance++;
+    return *this;
 }
 
-bool Distance::operator<=(Distance other) const
+Distance Distance::operator--()
 {
-    return !(*this > other);
+    this->distance--;
+    return *this;
 }
 
-bool Distance::operator>=(Distance other) const
-{
-    return !(*this < other);
-}
+bool Distance::operator<(Distance other) const { return asM() < other.asM(); }
+
+bool Distance::operator>(Distance other) const { return asM() > other.asM(); }
+
+bool Distance::operator<=(Distance other) const { return !(*this > other); }
+
+bool Distance::operator>=(Distance other) const { return !(*this < other); }
 
 bool Distance::operator==(Distance other) const
 {
     return compareFloatClose(asM(), other.asM(), EQUAL_EPSILON);
 }
 
-bool Distance::operator!=(Distance other) const
-{
-    return !(*this == other);
-}
+bool Distance::operator!=(Distance other) const { return !(*this == other); }
 
 }  // namespace algorithms
 }  // namespace aruwlib
--- mcb-2019-2020-project/src/aruwlib/algorithms/Distance.hpp
+++ mcb-2019-2020-project/src/aruwlib/algorithms/Distance.hpp
@@ -1,15 +1,16 @@
 #ifndef DISTANCE_HPP_
 #define DISTANCE_HPP_
 
+#include <cinttypes>
+
 namespace aruwlib
 {
 namespace algorithms
 {
-
 class Distance
 {
 public:
-    enum Units
+    enum class Units : uint8_t
     {
         MM,
         CM,
@@ -24,34 +25,168 @@ public:
 
     void set(float val);
 
+    /**
+     * @return The distance in mm.
+     */
     float asMm() const;
+
+    /**
+     * @return The distance in cm.
+     */
     float asCm() const;
+
+    /**
+     * @return The distance in m.
+     */
     float asM() const;
 
+    /**
+     * Creates an instance of a Distance class, converting `this`'s distance to
+     * millimeters and then returns the Distance.
+     *
+     * @note the current distance is not changed (hence the `const`).
+     */
     Distance toMm() const;
+
+    /**
+     * Creates an instance of a Distance class, converting `this`'s distance to
+     * centimeters and then returns the Distance.
+     *
+     * @note the current distance is not changed (hence the `const`).
+     */
     Distance toCm() const;
+
+    /**
+     * Creates an instance of a Distance class, converting `this`'s distance to
+     * meters and then returns the Distance.
+     *
+     * @note the current distance is not changed (hence the `const`).
+     */
     Distance toM() const;
 
+    // Note, addition and subtraction with distances without units doesn't make sense, which
+    // is why they are not provided.
+
+    /**
+     * Adds `this` and `other`'s distance, returning an instance of a Distance
+     * with the same units as `this`.
+     *
+     * @param[in] other The distance to add.
+     * @return The added distance.
+     */
     Distance operator+(Distance other) const;
+
+    /**
+     * Subtracts `other` from `this`, returning an instance of a Distance
+     * with the same units as `this`.
+     *
+     * @param[in]
+     * @return The subtracted distance.
+     */
     Distance operator-(Distance other) const;
+
+    /**
+     * Multiplies `this`'s distance by `val`, returning the multiplied distance using
+     * `this`'s units.
+     *
+     * @param[in] val The value to multiply `this`'s distance by.
+     * @return This multiplied distance.
+     */
     Distance operator*(float val) const;
+
+    /**
+     * @note The units returned are that of `this`'s.
+     * @see operator*(float val)
+     */
+    Distance operator*(Distance other) const;
+
+    /**
+     * Divides `this`'s distance by `val`, returning the divided distance using
+     * `this`'s units.
+     *
+     * @param[in] val The value to divide `this`'s distance by.
+     * @return The divided distance.
+     */
     Distance operator/(float val) const;
+
+    /**
+     * @note The units returned are that of `this`'s.
+     * @see operator/(float val)
+     */
+    Distance operator/(Distance other) const;
+
+    /**
+     * Adds on to `this`'s distance from `other`'s distance, returning the new distance
+     * using `this`'s units.
+     */
     Distance operator+=(Distance other);
+
+    /**
+     * Subtracts from `this`'s distance from `other`'s distance, returning the new distance
+     * using `this`'s units.
+     */
     Distance operator-=(Distance other);
+
+    /**
+     * Multiplies to `this`'s distance from `val`'s distance, returning the new distance
+     * using `this`'s units.
+     */
     Distance operator*=(float val);
+
+    /**
+     * Multiplies to `this`'s distance from `other`'s distance, returning the new distance
+     * using `this`'s units.
+     */
+    Distance operator*=(Distance other);
+
+    /**
+     * Divides `this`'s distance by `val`'s distance, returning the new distance
+     * using `this`'s units.
+     */
     Distance operator/=(float val);
 
+    /**
+     * Divides `this`'s distance by `other`'s distance, returning the new distance
+     * using `this`'s units.
+     */
+    Distance operator/=(Distance other);
+
+    /**
+     * Adds one unit to the `this`'s distance. The units are not changed, so if the units
+     * are in meters, one meter is added to `this`, whereas if the units are in millimeters,
+     * one millimeter is added to `this`.
+     */
+    Distance operator++();
+
+    /**
+     * Subtracts one unit to the `this`'s distance. The units are not changed, so if the units
+     * are in meters, one meter is subtracted from `this`, whereas if the units are in millimeters,
+     * one millimeter is subtracted from `this`.
+     */
+    Distance operator--();
+
+    /*
+     * These comparisons are as they seem. In particular, note that `other`'s distance is
+     * converted into the same units as `this` for comparison.
+     */
     bool operator<(Distance other) const;
     bool operator>(Distance other) const;
     bool operator<=(Distance other) const;
     bool operator>=(Distance other) const;
+    /**
+     * @note Equality is determined by using `aruwlib::algorithms::compareFloatClose`, with
+     *      an epsilon of EQUAL_EPSILON.
+     */
     bool operator==(Distance other) const;
+    /**
+     * @see `operator==` for now equality is determined.
+     */
     bool operator!=(Distance other) const;
 
 private:
     static constexpr float MM_PER_CM = 10.0f;
     static constexpr float CM_PER_M = 100.0f;
-    static constexpr float EQUAL_EPSILON = 0.0001f;
+    static constexpr float EQUAL_EPSILON = 0.000001f;
 
     float distance;
     Units unit;
