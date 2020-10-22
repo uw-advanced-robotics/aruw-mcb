e5599280a9a8cbbc70d18de45505edee6b6665d0
matthew <mma35@uw.edu>
2020-10-22 02:49:39 +0000

    Add warning to microsecond clock

--- aruw-mcb-project/src/aruwlib/architecture/clock.hpp
+++ aruw-mcb-project/src/aruwlib/architecture/clock.hpp
@@ -35,6 +35,9 @@ namespace clock
 {
 inline uint32_t getTimeMilliseconds() { return modm::Clock().now().time_since_epoch().count(); }
 
+/**
+ * @warning This clock time will wrap every 72 minutes. Do not use unless absolutely necessary.
+ */
 inline uint32_t getTimeMicroseconds()
 {
     return modm::PreciseClock().now().time_since_epoch().count();
