9f73060250d4809f6b672c02270f1530a3fc3bca
davidle0 <davidle0@cs.washington.edu>
2021-03-10 01:43:42 +0000

    started turret tests

---
+++ aruw-mcb-project/test/aruwsrc/control/turret/turret_subsystem_tests.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <aruwlib/Drivers.hpp>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "aruwsrc/control/turret/turret_subsystem.hpp"
+
+using aruwlib::Drivers;
+using aruwsrc::turret::TurretSubsystem;
+
+TEST(TurretSubsystem, turret_angles)
+{
+
+}
+
+TEST(TurretSubsystem, turret_online)
+{
+
+}
+
+TEST(TurretSubsystem, yaw_and_pitch_angle_center)
+{
+
+}
+
+
+TEST(TurretSubsystem, yaw_and_pitch_angle)
+{
+
+}
+
+TEST(TurretSubsystem, set_motor_output)
+{
+
+}
+
+TEST(TurretSubsystem, set_target)
+{
+
+}
+
+TEST(TurretSubsystem, get_target)
+{
+
+}
