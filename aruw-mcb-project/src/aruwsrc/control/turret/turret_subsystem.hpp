204ccc0cf4345ced46c09aa9331b21dd2a97fb79
Advanced Robotics at UW <robomstr@uw.edu>
2021-09-17 05:28:34 +0000

    CI Submodule Update

--- aruw-mcb-project/src/aruwsrc/control/turret/turret_subsystem.hpp
+++ aruw-mcb-project/src/aruwsrc/control/turret/turret_subsystem.hpp
@@ -21,7 +21,7 @@
 #define TURRET_SUBSYSTEM_HPP_
 
 #include "tap/algorithms/contiguous_float.hpp"
-#include "tap/algorithms/linear_interpolation.hpp"
+#include "tap/algorithms/linear_interpolation_predictor.hpp"
 #include "tap/control/turret/turret_subsystem_interface.hpp"
 
 #if defined(PLATFORM_HOSTED) && defined(ENV_UNIT_TESTS)
