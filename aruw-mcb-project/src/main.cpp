aae82e5f824b4784594f098bf3e170794ba2f708
matthew <mma35@uw.edu>
2021-10-03 00:18:41 -0700

    Add support for turning off laser and opening hopper cover based on flywheel motor speed

--- aruw-mcb-project/src/main.cpp
+++ aruw-mcb-project/src/main.cpp
@@ -36,21 +36,50 @@
 /* communication includes ---------------------------------------------------*/
 #include "tap/drivers_singleton.hpp"
 
-/* error handling includes --------------------------------------------------*/
-#include "tap/errors/create_errors.hpp"
-
 /* control includes ---------------------------------------------------------*/
 #include "tap/architecture/clock.hpp"
 #include "tap/architecture/endianness_wrappers.hpp"
 
-#include "aruwsrc/control/robot_control.hpp"
+#include "aruwsrc/control/hopper-cover/hopper_subsystem.hpp"
+#include "aruwsrc/control/hopper-cover/open_hopper_command_old.hpp"
 #include "modm/architecture/interface/can_message.hpp"
 
 using tap::Drivers;
 
-/* define timers here -------------------------------------------------------*/
-tap::arch::PeriodicMilliTimer sendMotorTimeout(2);
-tap::arch::PeriodicMilliTimer sendXavierTimeout(3);
+static constexpr int16_t MIN_FLYWHEEL_SPEED_FLYWHEELS_OFF = 100;
+
+static constexpr tap::motor::MotorId LEFT_MOTOR_ID = tap::motor::MOTOR2;
+static constexpr tap::motor::MotorId RIGHT_MOTOR_ID = tap::motor::MOTOR1;
+static constexpr tap::can::CanBus CAN_BUS_MOTORS = tap::can::CanBus::CAN_BUS1;
+
+tap::motor::DjiMotor flywheelLeft(
+    tap::DoNotUse_getDrivers(),
+    LEFT_MOTOR_ID,
+    CAN_BUS_MOTORS,
+    false,
+    "flywheel left");
+tap::motor::DjiMotor flywheelRight(
+    tap::DoNotUse_getDrivers(),
+    RIGHT_MOTOR_ID,
+    CAN_BUS_MOTORS,
+    false,
+    "flywheel right");
+
+aruwsrc::control::HopperSubsystem hopperSubsystem(
+    tap::DoNotUse_getDrivers(),
+    tap::gpio::Pwm::Pin::W,
+    aruwsrc::control::HopperSubsystem::SOLDIER_HOPPER_OPEN_PWM,
+    aruwsrc::control::HopperSubsystem::SOLDIER_HOPPER_CLOSE_PWM,
+    aruwsrc::control::HopperSubsystem::SOLDIER_PWM_RAMP_SPEED);
+
+aruwsrc::control::OpenHopperCommand openHopper(&hopperSubsystem);
+
+bool flywheelsOn = false;
+bool prevFlywheelsOn = true;
+
+int blinkCounter = 0;
+
+tap::arch::PeriodicMilliTimer mainLoopTimeout(2);
 
 // Place any sort of input/output initialization here. For example, place
 // serial init stuff here.
@@ -60,6 +89,11 @@ static void initializeIo(tap::Drivers *drivers);
 // very frequently. Use PeriodicMilliTimers if you don't want something to be
 // called as frequently.
 static void updateIo(tap::Drivers *drivers);
+
+static void sendIMUData(tap::Drivers *drivers);
+static void updateFlywheelState(tap::Drivers *drivers);
+static void updateHopperCover(tap::Drivers *drivers);
+static void updateLaser(tap::Drivers *drivers);
 
 static constexpr uint16_t IMU_MSG_CAN_ID = 0x203;
 
@@ -75,35 +109,26 @@ int main()
     Board::initialize();
     initializeIo(drivers);
 
-    int i = 0;
+    flywheelLeft.initialize();
+    flywheelRight.initialize();
 
-    drivers->digital.set(tap::gpio::Digital::Laser, true);
+    drivers->commandScheduler.registerSubsystem(&hopperSubsystem);
 
     while (1)
     {
         // do this as fast as you can
         PROFILE(drivers->profiler, updateIo, (drivers));
 
-        if (sendMotorTimeout.execute())
+        if (mainLoopTimeout.execute())
         {
             PROFILE(drivers->profiler, drivers->mpu6500.periodicIMUUpdate, ());
             PROFILE(drivers->profiler, drivers->terminalSerial.update, ());
+            PROFILE(drivers->profiler, drivers->commandScheduler.run, ());
 
-            float yaw = drivers->mpu6500.getYaw();
-            int16_t gzRaw =
-                drivers->mpu6500.getGz() * tap::sensors::Mpu6500::LSB_D_PER_S_TO_D_PER_S;
-
-            if (drivers->can.isReadyToSend(tap::can::CanBus::CAN_BUS1))
-            {
-                drivers->leds.set(tap::gpio::Leds::Green, i < 50);
-                i = (i + 1) % 100;
-
-                modm::can::Message msg(IMU_MSG_CAN_ID, 8);
-                msg.setExtended(false);
-                tap::arch::convertToLittleEndian(yaw, msg.data);
-                tap::arch::convertToLittleEndian(gzRaw, msg.data + 4);
-                drivers->can.sendMessage(tap::can::CanBus::CAN_BUS1, msg);
-            }
+            sendIMUData(drivers);
+            updateFlywheelState(drivers);
+            updateHopperCover(drivers);
+            updateLaser(drivers);
         }
         modm::delay_us(10);
     }
@@ -125,4 +150,53 @@ static void updateIo(tap::Drivers *drivers)
 {
     drivers->mpu6500.read();
     drivers->canRxHandler.pollCanData();
+}
+
+static void updateFlywheelState(tap::Drivers *drivers)
+{
+    prevFlywheelsOn = flywheelsOn;
+    flywheelsOn = abs(flywheelLeft.getShaftRPM()) > MIN_FLYWHEEL_SPEED_FLYWHEELS_OFF &&
+                  abs(flywheelRight.getShaftRPM()) > MIN_FLYWHEEL_SPEED_FLYWHEELS_OFF;
+}
+
+static void sendIMUData(tap::Drivers *drivers)
+{
+    float yaw = drivers->mpu6500.getYaw();
+    int16_t gzRaw = drivers->mpu6500.getGz() * tap::sensors::Mpu6500::LSB_D_PER_S_TO_D_PER_S;
+
+    if (drivers->can.isReadyToSend(tap::can::CanBus::CAN_BUS1))
+    {
+        drivers->leds.set(tap::gpio::Leds::Green, blinkCounter < 50);
+        blinkCounter = (blinkCounter + 1) % 100;
+
+        modm::can::Message msg(IMU_MSG_CAN_ID, 8);
+        msg.setExtended(false);
+        tap::arch::convertToLittleEndian(yaw, msg.data);
+        tap::arch::convertToLittleEndian(gzRaw, msg.data + 4);
+        drivers->can.sendMessage(tap::can::CanBus::CAN_BUS1, msg);
+    }
+}
+
+static void updateHopperCover(tap::Drivers *drivers)
+{
+    if (!flywheelsOn && prevFlywheelsOn)
+    {
+        drivers->commandScheduler.addCommand(&openHopper);
+    }
+    else if (flywheelsOn && !prevFlywheelsOn)
+    {
+        drivers->commandScheduler.removeCommand(&openHopper, false);
+    }
+}
+
+static void updateLaser(tap::Drivers *drivers)
+{
+    if (!flywheelsOn && prevFlywheelsOn)
+    {
+        drivers->digital.set(tap::gpio::Digital::OutputPin::Laser, false);
+    }
+    else if (flywheelsOn && !prevFlywheelsOn)
+    {
+        drivers->digital.set(tap::gpio::Digital::OutputPin::Laser, true);
+    }
 }
