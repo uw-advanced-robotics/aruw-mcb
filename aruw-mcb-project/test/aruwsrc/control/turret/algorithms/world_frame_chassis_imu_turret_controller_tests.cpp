9290873c8b26f4c717bbaa63d1939ce14096e79b
matthew <mma35@uw.edu>
2021-11-11 18:51:54 -0800

    \#249: Unit tests for turret subsystems/commands/algorithms

---
+++ aruw-mcb-project/test/aruwsrc/control/turret/algorithms/world_frame_chassis_imu_turret_controller_tests.cpp
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <gtest/gtest.h>
+
+#include "aruwsrc/control/turret/algorithms/world_frame_chassis_imu_turret_controller.hpp"
+#include "aruwsrc/drivers.hpp"
+#include "aruwsrc/mock/turret_subsystem_mock.hpp"
+
+using namespace testing;
+
+#define SETUP_SINGLE_PID_TEST()                                             \
+    aruwsrc::Drivers drivers;                                               \
+    tap::algorithms::ContiguousFloat worldFrameYawSetpoint(0, 0, 360);      \
+    testing::NiceMock<aruwsrc::mock::TurretSubsystemMock> turret(&drivers); \
+    tap::algorithms::SmoothPid positionPid(1, 0, 0, 0, 1, 1, 0, 1, 0);
+
+#define RUN_SINGLE_PID_YAW_CONTROLLER()                        \
+    WorldFrameChassisImuTurretController::runYawPidController( \
+        drivers,                                               \
+        1,                                                     \
+        yawSetpoint,                                           \
+        0,                                                     \
+        &worldFrameYawSetpoint,                                \
+        &positionPid,                                          \
+        &turret);
+
+using namespace aruwsrc::control::turret;
+
+TEST(WorldFrameChassisImuTurretController, runYawPidController__setpoint_actual_identical_output_0)
+{
+    // Setpoint used in gmock pointee
+    float yawSetpoint = 0.0f;
+    // Value used in gmock return reference
+    tap::algorithms::ContiguousFloat yawValue(0, 0, 360);
+
+    SETUP_SINGLE_PID_TEST();
+
+    yawSetpoint = 90.0f;
+    yawValue.setValue(90);
+    worldFrameYawSetpoint.setValue(yawSetpoint);
+    ON_CALL(turret, getCurrentYawValue).WillByDefault(ReturnRef(yawValue));
+    ON_CALL(drivers.mpu6500, getYaw).WillByDefault(Return(0));
+
+    EXPECT_CALL(turret, setYawMotorOutput(FloatEq(0)));
+
+    RUN_SINGLE_PID_YAW_CONTROLLER();
+}
+
+TEST(WorldFrameChassisImuTurretController, runYawPidController__setpoint_gt_actual_output_positive)
+{
+    // Setpoint used in gmock pointee
+    float yawSetpoint = 0.0f;
+    // Value used in gmock return reference
+    tap::algorithms::ContiguousFloat yawValue(0, 0, 360);
+    SETUP_SINGLE_PID_TEST();
+
+    // setpoint > value, output should be > 0
+    yawSetpoint = 90;
+    yawValue.setValue(80);
+    worldFrameYawSetpoint.setValue(yawSetpoint);
+    ON_CALL(turret, getCurrentYawValue).WillByDefault(ReturnRef(yawValue));
+    ON_CALL(drivers.mpu6500, getYaw).WillByDefault(Return(0));
+
+    EXPECT_CALL(turret, setYawMotorOutput(Gt(0)));
+
+    RUN_SINGLE_PID_YAW_CONTROLLER();
+}
+
+TEST(WorldFrameChassisImuTurretController, runYawPidController__setpoint_lt_actual_output_negative)
+{
+    // Setpoint used in gmock pointee
+    float yawSetpoint = 0.0f;
+    // Value used in gmock return reference
+    tap::algorithms::ContiguousFloat yawValue(0, 0, 360);
+
+    SETUP_SINGLE_PID_TEST();
+
+    // setpoint < value, output should be < 0
+    yawSetpoint = 90;
+    yawValue.setValue(100);
+    worldFrameYawSetpoint.setValue(yawSetpoint);
+    ON_CALL(turret, getCurrentYawValue).WillByDefault(ReturnRef(yawValue));
+    ON_CALL(drivers.mpu6500, getYaw).WillByDefault(Return(0));
+
+    EXPECT_CALL(turret, setYawMotorOutput(Lt(0)));
+
+    RUN_SINGLE_PID_YAW_CONTROLLER();
+}
+
+TEST(
+    WorldFrameChassisImuTurretController,
+    runYawPidController__chassis_frame_rotated_setpoint_actual_equal_0_output)
+{
+    // Setpoint used in gmock pointee
+    float yawSetpoint = 0.0f;
+    // Value used in gmock return reference
+    tap::algorithms::ContiguousFloat yawValue(0, 0, 360);
+
+    SETUP_SINGLE_PID_TEST();
+
+    yawSetpoint = 90;
+    yawValue.setValue(0);
+    worldFrameYawSetpoint.setValue(yawSetpoint);
+    ON_CALL(turret, getCurrentYawValue).WillByDefault(ReturnRef(yawValue));
+    ON_CALL(drivers.mpu6500, getYaw).WillByDefault(Return(90));
+
+    EXPECT_CALL(turret, setYawMotorOutput(FloatEq(0)));
+
+    RUN_SINGLE_PID_YAW_CONTROLLER();
+}
---
+++ aruw-mcb-project/test/aruwsrc/control/turret/world-relative/turret_world_relative_command_tests.cpp
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <gtest/gtest.h>
+
+#include "aruwsrc/control/turret/world-relative/turret_world_relative_command.hpp"
+#include "aruwsrc/drivers.hpp"
+#include "aruwsrc/mock/turret_subsystem_mock.hpp"
+
+using namespace aruwsrc::control::turret;
+using namespace aruwsrc;
+using namespace aruwsrc::mock;
+using namespace testing;
+
+TEST(TurretWorldRelativeCommand, isReady__true_if_turret_online)
+{
+    Drivers drivers;
+    NiceMock<TurretSubsystemMock> turret(&drivers);
+    TurretWorldRelativeCommand turretCmd(&drivers, &turret);
+
+    ON_CALL(turret, isOnline).WillByDefault(Return(true));
+    EXPECT_TRUE(turretCmd.isReady());
+
+    ON_CALL(turret, isOnline).WillByDefault(Return(false));
+    EXPECT_FALSE(turretCmd.isReady());
+}
+
+TEST(TurretWorldRelativeCommand, isFinished__true_if_turret_offline)
+{
+    Drivers drivers;
+    NiceMock<TurretSubsystemMock> turret(&drivers);
+    TurretWorldRelativeCommand turretCmd(&drivers, &turret);
+
+    ON_CALL(turret, isOnline).WillByDefault(Return(true));
+    EXPECT_FALSE(turretCmd.isFinished());
+
+    ON_CALL(turret, isOnline).WillByDefault(Return(false));
+    EXPECT_TRUE(turretCmd.isFinished());
+}
+
+TEST(TurretWorldRelativeCommand, execute__runs_turret_wr_turret_imu_cmd_when_turret_imu_online)
+{
+    Drivers drivers;
+    NiceMock<TurretSubsystemMock> turret(&drivers);
+    TurretWorldRelativeCommand turretCmd(&drivers, &turret);
+
+    tap::algorithms::ContiguousFloat currentYawValue(0, 0, 360);
+    tap::algorithms::ContiguousFloat currentPitchValue(0, 0, 360);
+    ON_CALL(turret, getCurrentYawValue).WillByDefault(ReturnRef(currentYawValue));
+    ON_CALL(turret, getCurrentPitchValue).WillByDefault(ReturnRef(currentYawValue));
+
+    ON_CALL(drivers.turretMCBCanComm, isConnected).WillByDefault(Return(true));
+    ON_CALL(turret, isOnline).WillByDefault(Return(true));
+
+    // The turret MCB comm will be queried if the turret IMU command is running
+    EXPECT_CALL(drivers.turretMCBCanComm, getYaw).Times(AtLeast(1));
+
+    turretCmd.initialize();
+    turretCmd.execute();
+}
+
+TEST(TurretWorldRelativeCommand, execute__runs_turret_wr_chassis_imu_cmd_when_turret_imu_offline)
+{
+    Drivers drivers;
+    NiceMock<TurretSubsystemMock> turret(&drivers);
+    TurretWorldRelativeCommand turretCmd(&drivers, &turret);
+
+    tap::algorithms::ContiguousFloat currentYawValue(0, 0, 360);
+    tap::algorithms::ContiguousFloat currentPitchValue(0, 0, 360);
+    ON_CALL(turret, getCurrentYawValue).WillByDefault(ReturnRef(currentYawValue));
+    ON_CALL(turret, getCurrentPitchValue).WillByDefault(ReturnRef(currentYawValue));
+
+    ON_CALL(drivers.turretMCBCanComm, isConnected).WillByDefault(Return(false));
+    ON_CALL(turret, isOnline).WillByDefault(Return(true));
+
+    // The turret MCB comm will be queried if the turret IMU command is running
+    EXPECT_CALL(drivers.turretMCBCanComm, getYaw).Times(0);
+
+    turretCmd.initialize();
+    turretCmd.execute();
+}
+
+TEST(TurretWorldRelativeCommand, end__doesnt_set_des_out_when_no_cmds_scheduled)
+{
+    Drivers drivers;
+    NiceMock<TurretSubsystemMock> turret(&drivers);
+    TurretWorldRelativeCommand turretCmd(&drivers, &turret);
+
+    ON_CALL(turret, isOnline).WillByDefault(Return(true));
+
+    EXPECT_CALL(turret, setPitchMotorOutput(0)).Times(0);
+    EXPECT_CALL(turret, setYawMotorOutput(0)).Times(0);
+
+    turretCmd.end(true);
+}
+
+TEST(TurretWorldRelativeCommand, end__set_des_out_0_when_either_turret_command_initially_scheduled)
+{
+    Drivers drivers;
+    NiceMock<TurretSubsystemMock> turret(&drivers);
+    TurretWorldRelativeCommand turretCmd(&drivers, &turret);
+
+    tap::algorithms::ContiguousFloat currentYawValue(0, 0, 360);
+    tap::algorithms::ContiguousFloat currentPitchValue(0, 0, 360);
+    ON_CALL(turret, getCurrentYawValue).WillByDefault(ReturnRef(currentYawValue));
+    ON_CALL(turret, getCurrentPitchValue).WillByDefault(ReturnRef(currentYawValue));
+    ON_CALL(turret, isOnline).WillByDefault(Return(true));
+
+    EXPECT_CALL(turret, setPitchMotorOutput(0)).Times(2);
+    EXPECT_CALL(turret, setYawMotorOutput(0)).Times(2);
+
+    ON_CALL(drivers.turretMCBCanComm, isConnected).WillByDefault(Return(false));
+
+    turretCmd.initialize();
+    turretCmd.end(true);
+
+    ON_CALL(drivers.turretMCBCanComm, isConnected).WillByDefault(Return(true));
+
+    turretCmd.initialize();
+    turretCmd.end(true);
+}
