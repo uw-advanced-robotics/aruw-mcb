e7f64404f0fdfb2fab080567fb53f5b50c7cd5e8
matthew <mma35@uw.edu>
2022-01-15 19:31:48 -0800

    15 m/s firing speed was slightly too fast, 2 parameters in shoot slow command should have been flipped

--- aruw-mcb-project/src/aruwsrc/control/launcher/friction_wheel_subsystem.hpp
+++ aruw-mcb-project/src/aruwsrc/control/launcher/friction_wheel_subsystem.hpp
@@ -67,7 +67,7 @@ public:
           9000.0f }};
 #else
     static constexpr modm::Pair<float, float> LAUNCH_SPEED_TO_FRICTION_WHEEL_RPM_LUT[] =
-        {{0.0f, 0.0f}, {15.0f, 4850.0f}, {18.0f, 5350.0f}, {30.0f, 7500.2f}, {32.0f, 8300.0f}};
+        {{0.0f, 0.0f}, {15.0f, 4650.0f}, {18.0f, 5350.0f}, {30.0f, 7500.2f}, {32.0f, 8300.0f}};
 #endif
 
     /**
