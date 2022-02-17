caacf0d297d6baa21af4c661efab8d3c546c5fda
matthew <mma35@uw.edu>
2022-02-17 13:29:44 -0800

    clang format

--- aruw-mcb-project/src/aruwsrc/control/turret/user/turret_quick_turn_command.hpp
+++ aruw-mcb-project/src/aruwsrc/control/turret/user/turret_quick_turn_command.hpp
@@ -21,6 +21,7 @@
 #define TURRET_QUICK_TURN_COMMAND_HPP_
 
 #include "tap/control/command.hpp"
+
 #include "../turret_subsystem.hpp"
 
 namespace tap
@@ -48,9 +49,7 @@ public:
      * @param[in] targetOffsetToTurn Offset angle, in degrees, that the turret setpoint
      *      will be updated to when this command is run.
      */
-    TurretQuickTurnCommand(
-        TurretSubsystem *turretSubsystem,
-        const float targetOffsetToTurn);
+    TurretQuickTurnCommand(TurretSubsystem *turretSubsystem, const float targetOffsetToTurn);
 
     bool isReady() override;
 
