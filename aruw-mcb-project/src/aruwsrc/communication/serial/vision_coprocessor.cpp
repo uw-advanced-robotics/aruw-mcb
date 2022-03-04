2c95b59c5de35bf11955a49c8f6c7536cf05a131
matthew <mma35@uw.edu>
2022-03-03 16:02:18 -0800

    Use 0.9 mbaud for hero vision coprocessor UART

--- aruw-mcb-project/src/aruwsrc/communication/serial/vision_coprocessor.cpp
+++ aruw-mcb-project/src/aruwsrc/communication/serial/vision_coprocessor.cpp
@@ -32,7 +32,7 @@ namespace aruwsrc
 namespace serial
 {
 VisionCoprocessor::VisionCoprocessor(aruwsrc::Drivers* drivers)
-    : DJISerial(drivers, VISION_COPROCESSOR_RX_PORT),
+    : DJISerial(drivers, VISION_COPROCESSOR_RX_UART_PORT),
       lastAimData(),
       odometryInterface(nullptr),
       turretOrientationInterface(nullptr)
@@ -42,8 +42,13 @@ VisionCoprocessor::VisionCoprocessor(aruwsrc::Drivers* drivers)
 void VisionCoprocessor::initializeCV()
 {
     cvOfflineTimeout.restart(TIME_OFFLINE_CV_AIM_DATA_MS);
+#if defined(TARGET_HERO)
+    drivers->uart.init<VISION_COPROCESSOR_TX_UART_PORT, 900'000>();
+    drivers->uart.init<VISION_COPROCESSOR_RX_UART_PORT, 900'000>();
+#else
     drivers->uart.init<VISION_COPROCESSOR_TX_UART_PORT, 1'000'000>();
-    initialize();
+    drivers->uart.init<VISION_COPROCESSOR_RX_UART_PORT, 1'000'000>();
+#endif
 }
 
 void VisionCoprocessor::messageReceiveCallback(const ReceivedSerialMessage& completeMessage)
