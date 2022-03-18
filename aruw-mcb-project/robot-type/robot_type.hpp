a2f2ae9b93e8122bf9393e1a2eb51a03cca5b3ae
tenzinhl <tenzinhl@uw.edu>
2022-03-18 13:56:31 -0700

    Add comments to robot_type.hpp
    Also remove unnecessary import in
    aruw-mcb-project/SConstruct

--- aruw-mcb-project/robot-type/robot_type.hpp
+++ aruw-mcb-project/robot-type/robot_type.hpp
@@ -1,3 +1,22 @@
+/**
+ * @file robot_type.hpp
+ *
+ * @brief Header for intellisense and default robot selection
+ *
+ * This file serves two main purposes:
+ *  1. To provide a default build target if one isn't provided when building.
+ *     This functionality is handled by the build script searching this file.
+ *  2. To help syntax highlighting. We setup the VSCode configuration to
+ *     "include" this file for intellisense
+ *
+ * `aruw-mcb` builds take a target option. This specifies which robot to build
+ * for as different robots will have different subsystems and constants etc., and
+ * this file helps with the above mentioned purposes.
+ *
+ * So as to not mess with the build system this file should **NOT** be included
+ * normally unless you're sure of what you're doing.
+ */
+
 #ifndef __ROBOT_TYPE_HPP__
 #define __ROBOT_TYPE_HPP__
 #define TARGET_SOLDIER_2022
