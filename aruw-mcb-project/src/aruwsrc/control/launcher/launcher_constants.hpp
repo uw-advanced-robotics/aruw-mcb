0100551c2b3acdd10ac8ada8d9120fd883f9aa63
manolipt <manolipt@uw.edu>
2022-04-01 19:56:25 -0700

    Removed [robot]_constants.hpp
    Discussed with Tenzin & Matthew: felt that "misc" constants files are
    unnecessary and misleading, so removed them and put their constants back
    where they were before.

--- aruw-mcb-project/src/aruwsrc/control/launcher/launcher_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/control/launcher/launcher_constants.hpp
@@ -60,6 +60,10 @@ static constexpr modm::Pair<float, float> LAUNCH_SPEED_TO_FRICTION_WHEEL_RPM_LUT
     {20.0f, 8500.0f}};
 #else
 static constexpr modm::Pair<float, float> LAUNCH_SPEED_TO_FRICTION_WHEEL_RPM_LUT[] =
-    {{0.0f, 0.0f}, {15.0f, 4600.0f}, {18.0f, 5000.0f}, {30.0f, 7200.2f}, {32.0f, 8300.0f}};
+    {{0.0f, 0.0f},
+    {15.0f, 4600.0f},
+    {18.0f, 5000.0f},
+    {30.0f, 7200.2f},
+    {32.0f, 8300.0f}};
 #endif
 }  // namespace aruwsrc::control::launcher
