3f7b97872b02d1b8cb1060faf1a4fad267ef922e
Deja Lewis <dlewis7@cs.washington.edu>
2022-04-23 15:10:33 -0700

    Initial Commit Created About Menu

---
+++ aruw-mcb-project/src/aruwsrc/display/about_menu.hpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef ABOUT_MENU_
+#define ABOUT_MENU_
+
+#include "tap/architecture/periodic_timer.hpp"
+#include "tap/display/dummy_allocator.hpp"
+#include "modm/ui/menu/abstract_menu.hpp"
+
+namespace aruwsrc
+{
+class Drivers;
+} // namespace aruwsrc
+
+namespace aruwsrc::display
+{
+
+
+/**
+ * Menu that allows user to see 
+ */
+class AboutMenu : public modm::AbstractMenu<tap::display::DummyAllocator<modm::IAbstractView> >
+{
+public:
+    #if defined(TARGET_SOLDIER_2021)
+        static constexpr char ROBOT_NAME[] = "TARGET_SOLDIER_2021";
+    #elif defined(TARGET_SOLDIER_2022)
+        static constexpr char ROBOT_NAME[] = "TARGET_SOLDIER_2022";
+    #elif defined(TARGET_DRONE)
+        static constexpr char ROBOT_NAME[] = "TARGET_DRONE";
+    #elif defined(TARGET_ENGINEER)
+        static constexpr char ROBOT_NAME[] = "TARGET_ENGINEER";
+    #elif defined(TARGET_SENTINEL)
+    static constexpr char ROBOT_NAME[] = "TARGET_SENTINEL";
+    #elif defined(TARGET_HERO)
+        static constexpr char ROBOT_NAME[] = "TARGET_HERO";
+    #endif
+    
+    /** Time between calls to `draw`, which will redraw the about menu. */
+    static constexpr uint32_t DISPLAY_DRAW_PERIOD = 500;
+
+    AboutMenu(
+        modm::ViewStack<tap::display::DummyAllocator<modm::IAbstractView> > *vs,
+        aruwsrc::Drivers *drivers);
+    void draw() override;
+
+    void update() override;
+
+    void shortButtonPress(modm::MenuButtons::Button button) override;
+
+    bool hasChanged() override;
+
+    static const char *getMenuName() { return "About Menu"; }
+
+private:
+    static constexpr int TURRET_MCB_MENU_ID = 7;
+
+    aruwsrc::Drivers *drivers;
+
+    tap::arch::PeriodicMilliTimer updatePeriodicTimer{DISPLAY_DRAW_PERIOD};
+
+};
+} // namespace aruw::display
+#endif // ABOUT_MENU_
--- aruw-mcb-project/src/aruwsrc/display/main_menu.hpp
+++ aruw-mcb-project/src/aruwsrc/display/main_menu.hpp
@@ -33,6 +33,7 @@
 #include "error_menu.hpp"
 #include "imu_calibrate_menu.hpp"
 #include "turret_mcb_menu.hpp"
+#include "about_menu.hpp"
 
 namespace aruwsrc
 {
@@ -71,6 +72,7 @@ private:
     tap::display::RefSerialMenu refSerialMenu;
     tap::communication::sensors::imu::ImuMenu imuMenu;
     TurretMCBMenu turretStatusMenu;
+    AboutMenu aboutMenu;
 
     void addImuCalibrateMenuCallback();
     void addCVMenuCallback();
@@ -82,6 +84,7 @@ private:
     void addRefSerialMenuCallback();
     void addImuMenuCallback();
     void addTurretMCBMenuCallback();
+    void addAboutMenuCallback();
 };  // class MainMenu
 }  // namespace display
 }  // namespace aruwsrc
