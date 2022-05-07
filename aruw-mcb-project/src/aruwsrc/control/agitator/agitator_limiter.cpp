34cb7a742d2f316e35ceeff31ec83fd4c38842f9
sazwu <szwu00@gmail.com>
2022-05-07 14:35:31 -0700

    Formatting

--- aruw-mcb-project/src/aruwsrc/control/agitator/agitator_limiter.cpp
+++ aruw-mcb-project/src/aruwsrc/control/agitator/agitator_limiter.cpp
@@ -1,20 +1,20 @@
 /*
- * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ * Copyright (c) 2022 Advanced Robotics at the University of Washington <robomstr@uw.edu>
  *
- * This file is part of Taproot.
+ * This file is part of aruw-mcb.
  *
- * Taproot is free software: you can redistribute it and/or modify
+ * aruw-mcb is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Taproot is distributed in the hope that it will be useful,
+ * aruw-mcb is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Taproot.  If not, see <https://www.gnu.org/licenses/>.
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "agitator_limiter.hpp"
@@ -67,7 +67,8 @@ void AgitatorLimiter::refresh()
         calibrateHere();
     }
 
-    if (frictionWheelSubsystem->getCurrentLaunchSpeed() >= frictionWheelSubsystem->getDesiredLaunchSpeed() - 2)
+    if (frictionWheelSubsystem->getCurrentLaunchSpeed() >=
+        frictionWheelSubsystem->getDesiredLaunchSpeed() - 2)
     {
         agitatorRunPositionPid();
     }
