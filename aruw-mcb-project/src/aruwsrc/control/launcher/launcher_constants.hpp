e89c8a753468ebd76182a5d679d8497036efbddd
matthew <mma35@uw.edu>
2022-05-07 23:36:26 -0700

    update launcher speed constants

--- aruw-mcb-project/src/aruwsrc/control/launcher/launcher_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/control/launcher/launcher_constants.hpp
@@ -56,19 +56,35 @@ static constexpr float LAUNCHER_PID_MAX_OUTPUT = 16'000.0f;
  * Lookup table that maps launch speed to flywheel speed. In between points in the lookup table,
  * linear interpolation is used.
  */
-#ifdef TARGET_HERO
+#if defined(TARGET_HERO)
 static constexpr modm::Pair<float, float> LAUNCH_SPEED_TO_FRICTION_WHEEL_RPM_LUT[] = {
     {0.0f, 0.0f},
     {10, 3900.0f},
     {16.0f, 6700.0f},
     {20.0f, 8500.0f},
 };
-#else
+#elif defined(TARGET_SOLDIERMK4_2022)
 static constexpr modm::Pair<float, float> LAUNCH_SPEED_TO_FRICTION_WHEEL_RPM_LUT[] = {
     {0.0f, 0.0f},
     {15.0f, 4375.0f},
     {18.0f, 4750.0f},
-    {30.0f, 7000.2f},
+    {30.0f, 7000.0f},
+    {32.0f, 8400.0f},
+};
+#elif defined(TARGET_SOLDIER_2021) || defined(TARGET_SOLDIER_2022) || defined(TARGET_SENTINEL_2022)
+static constexpr modm::Pair<float, float> LAUNCH_SPEED_TO_FRICTION_WHEEL_RPM_LUT[] = {
+    {0.0f, 0.0f},
+    {15.0f, 4400.0f},
+    {18.0f, 4850.0f},
+    {30.0f, 7100.0f},
+    {32.0f, 8400.0f},
+};
+#else  // TARGET_SENTINEL_2021, TARGET_DRONE, TARGET_ENGINEER
+static constexpr modm::Pair<float, float> LAUNCH_SPEED_TO_FRICTION_WHEEL_RPM_LUT[] = {
+    {0.0f, 0.0f},
+    {15.0f, 4400.0f},
+    {18.0f, 4850.0f},
+    {30.0f, 7100.0f},
     {32.0f, 8400.0f},
 };
 #endif
