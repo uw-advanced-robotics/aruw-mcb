0fb9f37d66550a1d3593dd3ed10452f21376ce48
matthew <mma35@uw.edu>
2022-05-18 21:01:40 -0700

    I'm dumb

--- aruw-mcb-project/taproot/src/tap/control/conditionally_executed_command.hpp
+++ aruw-mcb-project/taproot/src/tap/control/conditionally_executed_command.hpp
@@ -80,7 +80,7 @@ public:
         return std::any_of(
                    commandGovernorList.begin(),
                    commandGovernorList.end(),
-                   [](auto governor) { return governor->isReady(); }) ||
+                   [](auto governor) { return governor->isFinished(); }) ||
                command.isFinished();
     }
 
