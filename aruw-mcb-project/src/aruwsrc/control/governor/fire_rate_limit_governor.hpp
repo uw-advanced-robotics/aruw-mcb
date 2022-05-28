002d8b34b0a547a1a7b948877424850a15fc6b81
manolipt <manolipt@uw.edu>
2022-05-27 22:40:03 -0700

    format

--- aruw-mcb-project/src/aruwsrc/control/governor/fire_rate_limit_governor.hpp
+++ aruw-mcb-project/src/aruwsrc/control/governor/fire_rate_limit_governor.hpp
@@ -20,26 +20,26 @@
 #ifndef FIRE_RATE_LIMIT_GOVERNOR_HPP_
 #define FIRE_RATE_LIMIT_GOVERNOR_HPP_
 
+#include "tap/architecture/periodic_timer.hpp"
 #include "tap/control/governor/command_governor_interface.hpp"
-#include "modm/processing/timer/periodic_timer.hpp"
+#include "tap/errors/create_errors.hpp"
 
 #include "aruwsrc/communication/serial/vision_coprocessor.hpp"
 #include "aruwsrc/drivers.hpp"
-#include "tap/errors/create_errors.hpp"
-#include "tap/architecture/periodic_timer.hpp"
+#include "modm/processing/timer/periodic_timer.hpp"
 
 namespace aruwsrc::control::governor
 {
 /**
- * A governor that allows a Command to run based on an internal timer and information from the vision
- * processor that dictates firerate
+ * A governor that allows a Command to run based on an internal timer and information from the
+ * vision processor that dictates firerate
  */
 class FireRateLimitGovernor : public tap::control::governor::CommandGovernorInterface
 {
 public:
     FireRateLimitGovernor(
-        aruwsrc::Drivers &drivers, 
-        aruwsrc::serial::VisionCoprocessor &visionCoprocessor, 
+        aruwsrc::Drivers &drivers,
+        aruwsrc::serial::VisionCoprocessor &visionCoprocessor,
         uint8_t turretID)
         : drivers(drivers),
           visionCoprocessor(visionCoprocessor),
@@ -47,15 +47,9 @@ public:
     {
     }
 
-    void initialize() final
-    {
-        restartTimer();
-    }
+    void initialize() final { restartTimer(); }
 
-    bool isReady() final
-    {
-        return isTimerFinished();
-    }
+    bool isReady() final { return isTimerFinished(); }
 
     bool isFinished() final { return false; }
 
@@ -69,24 +63,27 @@ private:
     static constexpr float MID_RPS = 10;
     static constexpr float HIGH_RPS = 20;
 
-    bool isTimerFinished() {
-        return 
-            drivers.refSerial.getRefSerialReceivingData() || 
-            (visionCoprocessor.getLastAimData(turretID).firerate != aruwsrc::serial::VisionCoprocessor::FireRate::ZERO  
-            && timer.isExpired());
+    bool isTimerFinished()
+    {
+        return drivers.refSerial.getRefSerialReceivingData() ||
+               (visionCoprocessor.getLastAimData(turretID).firerate !=
+                    aruwsrc::serial::VisionCoprocessor::FireRate::ZERO &&
+                timer.isExpired());
     }
 
-    void restartTimer() {
-        switch(visionCoprocessor.getLastAimData(turretID).firerate) {
-            case aruwsrc::serial::VisionCoprocessor::FireRate::ZERO: // don't fire
+    void restartTimer()
+    {
+        switch (visionCoprocessor.getLastAimData(turretID).firerate)
+        {
+            case aruwsrc::serial::VisionCoprocessor::FireRate::ZERO:  // don't fire
                 break;
-            case aruwsrc::serial::VisionCoprocessor::FireRate::LOW: // low fire rate
+            case aruwsrc::serial::VisionCoprocessor::FireRate::LOW:  // low fire rate
                 timer.restart(rpsToPeriodMS(LOW_RPS));
                 break;
-            case aruwsrc::serial::VisionCoprocessor::FireRate::MEDIUM: // medium fire rate
+            case aruwsrc::serial::VisionCoprocessor::FireRate::MEDIUM:  // medium fire rate
                 timer.restart(rpsToPeriodMS(MID_RPS));
                 break;
-            case aruwsrc::serial::VisionCoprocessor::FireRate::HIGH: // high fire rate
+            case aruwsrc::serial::VisionCoprocessor::FireRate::HIGH:  // high fire rate
                 timer.restart(rpsToPeriodMS(HIGH_RPS));
                 break;
             default:
@@ -96,7 +93,6 @@ private:
     }
 
     static inline constexpr uint32_t rpsToPeriodMS(float rps) { return (1000.0f / rps); }
-
 };
 }  // namespace aruwsrc::control::governor
 
