62d38b441cf5b73253c9700205a900d6893e18cc
Kaelin Laundry <wasabifan@outlook.com>
2022-06-07 23:07:49 +0000

    Use ballistics goal for CV on-target computation instead of setpoint
    Co-authored-by: Matthew <mma35@uw.edu>
    Co-authored-by: manolipt <manolipt@uw.edu>

--- aruw-mcb-project/src/aruwsrc/control/turret/cv/sentinel_turret_cv_command.cpp
+++ aruw-mcb-project/src/aruwsrc/control/turret/cv/sentinel_turret_cv_command.cpp
@@ -104,9 +104,13 @@ void SentinelTurretCVCommand::execute()
         yawSetpoint = turretSubsystem->yawMotor.unwrapTargetAngle(yawSetpoint);
         pitchSetpoint = turretSubsystem->pitchMotor.unwrapTargetAngle(pitchSetpoint);
 
+        auto differenceWrapped = [](float measurement, float setpoint) {
+            return tap::algorithms::ContiguousFloat(measurement, 0, M_TWOPI).difference(setpoint);
+        };
+
         withinAimingTolerance = aruwsrc::algorithms::OttoBallisticsSolver::withinAimingTolerance(
-            turretSubsystem->yawMotor.getValidChassisMeasurementErrorWrapped(),
-            turretSubsystem->pitchMotor.getValidChassisMeasurementErrorWrapped(),
+            differenceWrapped(yawController->getMeasurement(), yawSetpoint),
+            differenceWrapped(pitchController->getMeasurement(), pitchSetpoint),
             ballisticsSolution->distance);
     }
     else
