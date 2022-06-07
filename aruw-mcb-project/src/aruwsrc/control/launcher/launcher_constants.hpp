c7a537a2992bd7dce31314dc9633c9668f60705a
manolipt <manolipt@uw.edu>
2022-06-07 13:26:48 -0700

    tune constant

--- aruw-mcb-project/src/aruwsrc/control/launcher/launcher_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/control/launcher/launcher_constants.hpp
@@ -104,7 +104,7 @@ static constexpr uint32_t AGITATOR_TYPICAL_DELAY_MICROSECONDS = 80'000;
 #endif
 
 #if defined(ALL_SENTINELS)
-static constexpr uint32_t AGITATOR_PAUSE_PROJECTILE_LAUNCHING_TIME = 3'000;
+static constexpr uint32_t AGITATOR_PAUSE_PROJECTILE_LAUNCHING_TIME = 8'000;
 #endif
 
 }  // namespace aruwsrc::control::launcher
