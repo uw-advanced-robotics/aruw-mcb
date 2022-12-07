c65ae1612634ae959831b50a72dc82dad9e67287
manolipt <manoliptram@gmail.com>
2022-12-07 22:37:40 +0000

    Change getWheel() to getChannel(Remote::Channel::WHEEL)

--- aruw-mcb-project/src/aruwsrc/control/control_operator_interface.cpp
+++ aruw-mcb-project/src/aruwsrc/control/control_operator_interface.cpp
@@ -227,7 +227,8 @@ float ControlOperatorInterface::getTurretPitchInput(uint8_t turretID)
 
 float ControlOperatorInterface::getSentrySpeedInput()
 {
-    return (-drivers->remote.getWheel() / 660.0f) * USER_STICK_SENTRY_DRIVE_SCALAR;
+    return (-drivers->remote.getChannel(Remote::Channel::WHEEL) / 660.0f) *
+           USER_STICK_SENTRY_DRIVE_SCALAR;
 }
 }  // namespace control
 
