74f2c642ec44d7392ca5c4ce9c0d85902125c8af
SumedhP <43325361+SumedhP@users.noreply.github.com>
2023-02-24 15:55:20 -0800

    Standard drivers

--- aruw-mcb-project/src/aruwsrc/robot/standard/standard_drivers_singleton.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/standard/standard_drivers_singleton.hpp
@@ -17,32 +17,26 @@
  * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef ROBOT_CONTROL_HPP_
-#define ROBOT_CONTROL_HPP_
+#ifndef STANDARD_DRIVERS_SINGLETON_HPP_
+#define STANDARD_DRIVERS_SINGLETON_HPP_
 
-#include "aruwsrc/drivers.hpp"
-#include "aruwsrc/robot/drone/drone_drivers.hpp"
-#include "aruwsrc/robot/engineer/engineer_drivers.hpp"
-#include "aruwsrc/robot/hero/hero_drivers.hpp"
+#ifndef ENV_UNIT_TESTS
+
 #include "aruwsrc/robot/standard/standard_drivers.hpp"
 
 namespace aruwsrc
 {
-namespace control
-{
-#ifdef TARGET_HERO_CYCLONE
-void initSubsystemCommands(aruwsrc::HeroDrivers *drivers);
-#elif defined(TARGET_ENGINEER)
-void initSubsystemCommands(aruwsrc::EngineerDrivers *drivers);
-#elif defined(TARGET_DRONE)
-void initSubsystemCommands(aruwsrc::DroneDrivers *drivers);
-#elif defined(ALL_STANDARDS)
-void initSubsystemCommands(aruwsrc::StandardDrivers *drivers);
-#else
-void initSubsystemCommands(aruwsrc::Drivers *drivers);
-#endif
-}  // namespace control
-
+/**
+ * @return The singleton instance of the Drivers class. This is the only instance of the
+ *      Drivers class that should be created anywhere in the non-unit test framework.
+ * @note It is likely that you will never have to use this. There are only two files you
+ *      should be calling this function from -- `main.cpp` and `*_control.cpp`, either to
+ *      run I/O stuff and to add a Drivers pointer to an instance of a Subsystem or Command.
+ */
+aruwsrc::StandardDrivers *DoNotUse_getStandardDrivers();
+using standardDriversFunc = aruwsrc::StandardDrivers *(*)();
 }  // namespace aruwsrc
 
-#endif  // ROBOT_CONTROL_HPP_
+#endif
+
+#endif  // STANDARD_DRIVERS_SINGLETON_HPP_
