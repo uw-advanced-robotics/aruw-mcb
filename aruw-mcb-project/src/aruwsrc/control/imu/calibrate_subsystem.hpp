1ffc76fea3f6bbe6830b0abb9e738bf6472a6c16
Nikolas McNamee <nikolasmcnamee@gmail.com>
2023-03-02 17:58:12 -0800

    clang formatizing

--- aruw-mcb-project/src/aruwsrc/control/imu/calibrate_subsystem.hpp
+++ aruw-mcb-project/src/aruwsrc/control/imu/calibrate_subsystem.hpp
@@ -20,16 +20,18 @@
 #ifndef CALIBRATE_SUBSYSTEM_HPP_
 #define CALIBRATE_SUBSYSTEM_HPP_
 
-#include "aruwsrc/control/imu/imu_calibrate_command.hpp"
 #include "tap/control/subsystem.hpp"
+
 #include "aruwsrc/control/control_operator_interface.hpp"
+#include "aruwsrc/control/imu/imu_calibrate_command.hpp"
 
 namespace aruwsrc::control::imu
 {
 class CalibrateSubsystem : public tap::control::Subsystem
 {
 public:
-    CalibrateSubsystem(tap::Drivers* drivers,
+    CalibrateSubsystem(
+        tap::Drivers* drivers,
         ImuCalibrateCommand& imuCalibrateCommand,
         ControlOperatorInterface& controlOperatorInterface);
 
@@ -38,9 +40,10 @@ public:
     void refresh() override;
 
     const char* getName() override { return "Calibrate"; }
+
 private:
     ImuCalibrateCommand& imuCalibrateCommand;
     ControlOperatorInterface& controlOperatorInterface;
 };
-} // namespace aruwsrc::control::imu
+}  // namespace aruwsrc::control::imu
 #endif
