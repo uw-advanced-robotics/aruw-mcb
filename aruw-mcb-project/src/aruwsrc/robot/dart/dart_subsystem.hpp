7bd06584943b2c78fee4b8dd83049f89cbe567c8
SumedhP <43325361+SumedhP@users.noreply.github.com>
2023-03-02 19:23:29 -0800

    Made dart subsystem

---
+++ aruw-mcb-project/src/aruwsrc/robot/dart/dart_subsystem.hpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef DART_SUBSYSTEM_HPP_
+#define DART_SUBSYSTEM_HPP_
+
+#include "tap/control/subsystem.hpp"
+#include "tap/motor/dji_motor.hpp"
+
+namespace aruwsrc::control::dart
+{
+class DartSubsystem : public tap::control::Subsystem
+{
+public:
+    DartSubsystem(tap::Drivers* drivers, tap::motor::DjiMotor* motor);
+
+    const char* getName() override { return "Dart"; }
+
+    void windUp();
+
+	void stop();
+
+	private:
+	tap::motor::DjiMotor* motor;
+
+};
+
+}  // namespace aruwsrc::control::dart
+
+#endif  // DART_SUBSYSTEM_HPP_
