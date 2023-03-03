1265c9fc7ad498279f2a4dde72e2991c3539a312
by1459 <by1459@cs.washington.edu>
2023-03-02 18:31:07 -0800

    moew

---
+++ aruw-mcb-project/src/aruwsrc/robot/dart/dart_constants.hpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022-2023 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see https://www.gnu.org/licenses/.
+ */
+#ifndef DART_CONSTANTSHPP
+#define DART_CONSTANTSHPP
+
+#include "tap/algorithms/smooth_pid.hpp"
+#include "tap/motor/dji_motor.hpp"
+
+#include "aruwsrc/control/turret/turret_motor_config.hpp"
+#include "modm/math/geometry/angle.hpp"
+
+namespace aruwsrc::control::turret
+{
+
+static constexpr tap::motor::MotorId PULL_MOTOR_ID = tap::motor::MOTOR6; // TODO: update correct motor
+static constexpr tap::motor::MotorId DEAD_MOTOR1 = tap::motor::MOTOR5;
+static constexpr tap::motor::MotorId DEAD_MOTOR2 = tap::motor::MOTOR4;
+static constexpr tap::can::CanBus CAN_BUS_MOTORS = tap::can::CanBus::CAN_BUS1;
+
+}
+#endif
