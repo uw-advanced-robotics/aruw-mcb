5556d4cfa04e456e06817e67c7f84a9525c15aa3
SumedhP <43325361+SumedhP@users.noreply.github.com>
2023-03-07 16:54:25 -0800

    Removed emoji

--- aruw-mcb-project/src/aruwsrc/robot/dart/dart_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/dart/dart_constants.hpp
@@ -14,8 +14,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with aruw-mcb.  If not, see https://www.gnu.org/licenses/.
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 #ifndef DART_CONSTANTS_HPP_
 #define DART_CONSTANTS_HPP_
 
--- aruw-mcb-project/src/aruwsrc/robot/dart/dart_turret_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/dart/dart_turret_constants.hpp
@@ -27,7 +27,7 @@
 
 namespace aruwsrc::control::turret
 {
-// These are needed to not break vision coprocessor 🤷‍♂️, temp fix to issue
+// These are needed to not break vision coprocessor , temp fix to issue
 static constexpr uint8_t NUM_TURRETS = 1;
 static constexpr float TURRET_CG_X = 0;
 static constexpr float TURRET_CG_Z = 0;
