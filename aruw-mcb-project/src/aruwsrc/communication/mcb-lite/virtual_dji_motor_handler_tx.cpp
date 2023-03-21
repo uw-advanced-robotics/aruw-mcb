c459b507d704ccf7ddc4385bcd68b459c1e5d907
SumedhP <43325361+SumedhP@users.noreply.github.com>
2023-03-21 12:23:33 -0700

    Implmented dji motor tx handler for MCB

---
+++ aruw-mcb-project/src/aruwsrc/communication/mcb-lite/virtual_dji_motor_handler_tx.cpp
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "tap/communication/can/can.hpp"
+#include "tap/communication/can/can_bus.hpp"
+#include "tap/communication/serial/uart.hpp"
+#include "tap/drivers.hpp"
+#include "tap/errors/create_errors.hpp"
+
+#include "virtual_dji_motor_tx_handler.hpp"
+
+namespace aruwsrc::virtualMCB
+{
+
+VirtualDJIMotorTxHandler::VirtualDJIMotorTxHandler(tap::Drivers* drivers, CanBus canbus)
+    : DjiMotorTxHandler(drivers),
+      canbus(canbus)
+{
+}
+
+void VirtualDJIMotorTxHandler::encodeAndSendCanData()
+{
+    // set up new can messages to be sent via CAN bus 1 and 2
+    modm::can::Message can1MessageLow(
+        CAN_DJI_LOW_IDENTIFIER,
+        CAN_DJI_MESSAGE_SEND_LENGTH,
+        0,
+        false);
+    modm::can::Message can1MessageHigh(
+        CAN_DJI_HIGH_IDENTIFIER,
+        CAN_DJI_MESSAGE_SEND_LENGTH,
+        0,
+        false);
+    modm::can::Message can2MessageLow(
+        CAN_DJI_LOW_IDENTIFIER,
+        CAN_DJI_MESSAGE_SEND_LENGTH,
+        0,
+        false);
+    modm::can::Message can2MessageHigh(
+        CAN_DJI_HIGH_IDENTIFIER,
+        CAN_DJI_MESSAGE_SEND_LENGTH,
+        0,
+        false);
+
+    bool can1ValidMotorMessageLow = false;
+    bool can1ValidMotorMessageHigh = false;
+    bool can2ValidMotorMessageLow = false;
+    bool can2ValidMotorMessageHigh = false;
+
+    DjiMotorTxHandler::serializeMotorStoreSendData(
+        can1MotorStore,
+        &can1MessageLow,
+        &can1MessageHigh,
+        &can1ValidMotorMessageLow,
+        &can1ValidMotorMessageHigh);
+
+    DjiMotorTxHandler::serializeMotorStoreSendData(
+        can2MotorStore,
+        &can2MessageLow,
+        &can2MessageHigh,
+        &can2ValidMotorMessageLow,
+        &can2ValidMotorMessageHigh);
+
+    bool messageSuccess = true;
+
+    if (canbus.isReadyToSend(tap::can::CanBus::CAN_BUS1))
+    {
+        if (can1ValidMotorMessageLow)
+        {
+            messageSuccess &= canbus.sendMessage(tap::can::CanBus::CAN_BUS1, can1MessageLow);
+        }
+        if (can1ValidMotorMessageHigh)
+        {
+            messageSuccess &= canbus.sendMessage(tap::can::CanBus::CAN_BUS1, can1MessageHigh);
+        }
+    }
+    if (canbus.isReadyToSend(tap::can::CanBus::CAN_BUS2))
+    {
+        if (can2ValidMotorMessageLow)
+        {
+            messageSuccess &= canbus.sendMessage(tap::can::CanBus::CAN_BUS2, can2MessageLow);
+        }
+        if (can2ValidMotorMessageHigh)
+        {
+            messageSuccess &= canbus.sendMessage(tap::can::CanBus::CAN_BUS2, can2MessageHigh);
+        }
+    }
+
+    if (!messageSuccess)
+    {
+        RAISE_ERROR(drivers, "sendMessage failure");
+    }
+}
+
+}  // namespace aruwsrc::virtualMCB
--- aruw-mcb-project/src/aruwsrc/communication/mcb-lite/virutal_can_bus.cpp
+++ aruw-mcb-project/src/aruwsrc/communication/mcb-lite/virutal_can_bus.cpp
@@ -26,84 +26,45 @@
 
 namespace aruwsrc::can
 {
-VirtualCanBus::VirtualCanBus(tap::Drivers* drivers, tap::can::CanBus* port)
-    : drivers(drivers),
-      canPort(port)
-{
-    usingCanPort = true;
-}
 
 VirtualCanBus::VirtualCanBus(tap::Drivers* drivers, tap::communication::serial::Uart::UartPort port)
     : drivers(drivers),
-      uartPort(port)
+      port(port)
 {
-    usingCanPort = false;
 }
 
 void VirtualCanBus::initialize()
 {
-    if (usingCanPort)
-    {
-        // This is technically done in main TODO: ask if anything will go wrong if done twice maybe
-        drivers->can.initialize();
-    }
-    else
-    {
-        // drivers->uart.init<tap::communication::serial::Uart::UartPort::Uart3, 1000000>();
-        // drivers->uart.init<uartPort, 1000000>();
-    }
+    // drivers->uart.init<tap::communication::serial::Uart::Port::Uart3, 1000000>();
+    // drivers->uart.init<port, 1000000>();
 }
 
 bool VirtualCanBus::isMessageAvailable(tap::can::CanBus canbus)
 {
-    if (usingCanPort)
-    {
-        return drivers->can.isMessageAvailable(canbus);
-    }
     // Uuuuuuuuh this doesn't really exist on the uart port?? Also TODO: Where is this used???
     //  drivers->uart.i
 }
 
 bool VirtualCanBus::getMessage(tap::can::CanBus canbus, modm::can::Message* message)
 {
-    if (usingCanPort)
-    {
-        drivers->can.getMessage(canbus, message);
-    }
-    else
-    {
-        uint32_t* messageCast;
-        // This is read at 32 for can message??
-        drivers->uart.read(uartPort, reinterpret_cast<uint8_t*>(messageCast), sizeof(message));
-        memcpy(&message, &messageCast, sizeof(message));
-        // TODO: Figure out if this is correct
-    }
+    uint32_t* messageCast;
+    // This is read at 32 for can message??
+    drivers->uart.read(port, reinterpret_cast<uint8_t*>(messageCast), sizeof(message));
+    memcpy(&message, &messageCast, sizeof(message));
+    // TODO: Figure out if this is correct
 }
 
 bool VirtualCanBus::isReadyToSend(tap::can::CanBus canbus)
 {
-    if (usingCanPort)
-    {
-        drivers->can.isReadyToSend(canbus);
-    }
-    else
-    {
-        drivers->uart.isWriteFinished(uartPort);
-    }
+    drivers->uart.isWriteFinished(port);
 }
 
 bool VirtualCanBus::sendMessage(tap::can::CanBus canbus, const modm::can::Message& message)
 {
-    if (usingCanPort)
-    {
-        drivers->can.sendMessage(canbus, message);
-    }
-    else
-    {
-        uint32_t* messageCast;
-        memcpy(&messageCast, &message, sizeof(message));
-        drivers->uart.write(uartPort, reinterpret_cast<uint8_t*>(messageCast), sizeof(message));
-    }
+    // Copy of what's done in the can send message
+    uint32_t* messageCast;
+    memcpy(&messageCast, &message, sizeof(message));
+    drivers->uart.write(port, reinterpret_cast<uint8_t*>(messageCast), sizeof(message));
 }
 
 }  // namespace aruwsrc::can
