c039a85c58f05d24fddc71490fc4c42feee093c0
spoonk <cornwlucas@gmail.com>
2023-03-26 16:34:19 -0700

    begin refactor

--- aruw-mcb-project/src/aruwsrc/algorithms/transforms/standard_transformer.hpp
+++ aruw-mcb-project/src/aruwsrc/algorithms/transforms/standard_transformer.hpp
@@ -1,21 +1,21 @@
-// /*
-//  * Copyright (c) 2021-2022 Advanced Robotics at the University of Washington <robomstr@uw.edu>
-//  *
-//  * This file is part of aruw-mcb.
-//  *
-//  * aruw-mcb is free software: you can redistribute it and/or modify
-//  * it under the terms of the GNU General Public License as published by
-//  * the Free Software Foundation, either version 3 of the License, or
-//  * (at your option) any later version.
-//  * 
-//  * aruw-mcb is distributed in the hope that it will be useful,
-//  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  * GNU General Public License for more details.
-//  *
-//  * You should have received a copy of the GNU General Public License
-//  * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
-//  */
+/*
+ * Copyright (c) 2021-2022 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #ifndef STANDARD_TRANSFORMER_HPP_
 #define STANDARD_TRANSFORMER_HPP_
@@ -54,10 +54,8 @@ namespace aruwsrc::algorithms::transforms
          * robot. 
         */
         StandardTransformer
-        (      
-        tap::communication::sensors::imu::mpu6500::Mpu6500& chassisImu,
-        aruwsrc::can::TurretMCBCanComm& turretMCB
-        );
+        (tap::communication::sensors::imu::mpu6500::Mpu6500& chassisImu,
+        aruwsrc::can::TurretMCBCanComm& turretMCB);
 
         /**
          * Update each transform with most recent encoder and IMU odometry data. This method
