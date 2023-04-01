1a937ab0a8b9365c7cbefdfa65f7a15709724e92
SumedhP <43325361+SumedhP@users.noreply.github.com>
2023-04-01 13:09:44 -0700

    formatting

--- aruw-mcb-project/src/aruwsrc/communication/mcb-lite/listeners/virtual_can_rx_listener.cpp
+++ aruw-mcb-project/src/aruwsrc/communication/mcb-lite/listeners/virtual_can_rx_listener.cpp
@@ -8,7 +8,7 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * aruw-mcb is distributed in the hope that it will be useful,virtual_can_rx_listener
+ * aruw-mcb is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
