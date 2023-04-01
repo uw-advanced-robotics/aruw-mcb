227373aece3a51790fc8e9ead0fd7075c13e30a2
spoonk <cornwlucas@gmail.com>
2023-03-31 17:51:45 -0700

    fix xdrive hpp

--- aruw-mcb-project/src/aruwsrc/control/chassis/x_drive_chassis_subsystem.hpp
+++ aruw-mcb-project/src/aruwsrc/control/chassis/x_drive_chassis_subsystem.hpp
@@ -44,10 +44,10 @@ class XDriveChassisSubsystem : public Holonomic4MotorChassisSubsystem
 public:
     XDriveChassisSubsystem(
         tap::Drivers* drivers,
-        tap::motor::DjiMotor&,
-        tap::motor::DjiMotor&,
-        tap::motor::DjiMotor&,
-        tap::motor::DjiMotor&,
+        tap::motor::MotorId leftFrontMotorId = LEFT_FRONT_MOTOR_ID,
+        tap::motor::MotorId leftBackMotorId = LEFT_BACK_MOTOR_ID,
+        tap::motor::MotorId rightFrontMotorId = RIGHT_FRONT_MOTOR_ID,
+        tap::motor::MotorId rightBackMotorId = RIGHT_BACK_MOTOR_ID,
         tap::gpio::Analog::Pin currentPin = CURRENT_SENSOR_PIN);
 };
 
