82573e366c53b3b18e990ae73bfe1a8a46ea48cc
ProfJAT <JoshuaT22@lakesideschool.org>
2023-04-11 19:53:55 -0700

    Added turret command

---
+++ aruw-mcb-project/src/aruwsrc/control/turret/new_sentry/turret_major_sentry_control_command.hpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2020-2022 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef TURRET_MAJOR_SENTRY_CONTROL_COMMAND_HPP_
+#define TURRET_MAJOR_SENTRY_CONTROL_COMMAND_HPP_
+
+namespace aruwsrc
+{
+class Drivers;
+
+namespace control
+{
+class ControlOperatorInterface;
+}
+}  // namespace aruwsrc
+
+namespace aruwsrc::control::turret::sentry
+{
+/**
+ * Command that takes user input from the 'SentryControlOperatorInterface' to control
+ * axis of some turret using some passed in yaw and pitch controller upon constr
+*/
+class TurretSentryControlCommand : public tap::control::Command
+// TODO: Finish
+}
+
+#endif
