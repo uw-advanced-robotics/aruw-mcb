9fecc901d2dd3b0e49079844bde906e847c5da3b
Nikolas McNamee <nikolasmcnamee@gmail.com>
2023-04-11 17:23:49 -0700

    cpp and hpp

---
+++ aruw-mcb-project/src/aruwsrc/control/barrel_switcher_subsystem.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022-2023 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "barrel_switcher_subsystem.hpp"
+
+#include "tap/drivers.hpp"
+#include "tap/motor/dji_motor.hpp"
+
+namespace aruwsrc::control
+{
+BarrelSwitcherSubsystem::BarrelSwitcherSubsystem(tap::Drivers* drivers, tap::motor::MotorId motorid) : 
+    HomeableSubsystemInterface(drivers), 
+    motor(drivers, motorid, tap::can::CanBus::CAN_BUS2, false, "barrel switching motor") //canbus tbd
+    {}
+    
+void BarrelSwitcherSubsystem::initialize() {
+    motor.initialize();
+}
+
+void BarrelSwitcherSubsystem::setMotorOutput(int32_t desiredOutput) {
+    motor.setDesiredOutput(desiredOutput);
+}
+
+bool BarrelSwitcherSubsystem::isStalled() const {
+    
+}
+
+void BarrelSwitcherSubsystem::setLowerBound() {
+    //motor.resetEncoder();
+}
+
+void BarrelSwitcherSubsystem::setUpperBound() {
+    motorUpperBound = motor.getEncoderUnwrapped();
+}
+};
