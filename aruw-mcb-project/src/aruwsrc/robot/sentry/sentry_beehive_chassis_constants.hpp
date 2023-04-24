de60ce10c2cd37e567c2de59e713e73a8c281b0d
pie474 <chinmaymurthy474@gmail.com>
2023-04-23 21:54:25 -0700

    added comment explaining forward mat calculation

--- aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_beehive_chassis_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_beehive_chassis_constants.hpp
@@ -9,13 +9,27 @@ static constexpr float CENTER_TO_WHEELBASE_RADIUS = 0.205;
 static constexpr float WHEELBASE_COORD = CENTER_TO_WHEELBASE_RADIUS / 1.41421356237;
 static constexpr tap::can::CanBus CAN_BUS_MOTORS = tap::can::CanBus::CAN_BUS2;
 
+/**
+ * Calculated by solving for the pseudo-inverse of the following matrix
+ * 
+ * 1, 0, -LF_Y
+ * 0, 1,  LF_X
+ * 1, 0, -RF_Y
+ * 0, 1,  RF_X
+ * 1, 0, -LB_Y
+ * 0, 1,  LB_X
+ * 1, 0, -RB_Y
+ * 0, 1,  RB_X
+ * 
+*/
 constexpr float SWERVE_FORWARD_MATRIX[24] {
     0.25, 0.0, 0.25, 0.0, 0.25, 0., 0.25, 0.0, 
-    0.0, 0.25, 0., 0.25, 0., 0.25, 0., 0.25, 
+    0.0, 0.25, 0.0, 0.25, 0.0, 0.25, 0.0, 0.25, 
     -0.862325, -0.862325, -0.862325, 0.862325, 0.862325, -0.862325, 0.862325, 0.862325
 };
 
 // todo: hopefullly these can live as constants here soon :)
+// also todo: these positions use a +y-forward coord system, should use +x-forward
 aruwsrc::chassis::SwerveModuleConfig leftFrontSwerveConfig = {
     .azimuthZeroOffset = 2313,
     .positionWithinChassisX = -WHEELBASE_COORD,
