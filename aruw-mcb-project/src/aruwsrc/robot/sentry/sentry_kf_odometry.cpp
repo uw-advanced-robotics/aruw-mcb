9c103ca20e8a2455a64a737e3c8528567099bb1d
spoonk <cornwlucas@gmail.com>
2023-04-28 21:21:46 -0700

    use virtual imu instead of mpu6500 for chassis yaw

--- aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_kf_odometry.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_kf_odometry.cpp
@@ -29,14 +29,14 @@ namespace aruwsrc::algorithms::odometry
 {
 SentryKFOdometry::SentryKFOdometry(
     aruwsrc::serial::VisionCoprocessor& visionCoprocessor,
-    tap::communication::sensors::imu::mpu6500::Mpu6500& mpu,
+    aruwsrc::virtualMCB::VirtualIMUInterface chassisIMU,
     const aruwsrc::chassis::HolonomicChassisSubsystem& chassis,
     const aruwsrc::control::turret::SentryTurretMajorSubsystem& turretMajor,
     const aruwsrc::control::turret::SentryTurretMinorSubsystem& turretMinorLeft,
     const aruwsrc::control::turret::SentryTurretMinorSubsystem& turretMinorRight)
     : 
         visionCoprocessor(visionCoprocessor),
-        mpu(mpu),
+        chassisIMU(chassisIMU),
         chassis(chassis),
         turretMajor(turretMajor),
         turretMinorLeft(turretMinorLeft),
@@ -46,7 +46,7 @@ SentryKFOdometry::SentryKFOdometry(
     kf.init({0, 0, 0, 0, 0, 0});
 }
 
-float SentryKFOdometry::getChassisYaw() { return mpu.getYaw(); }
+float SentryKFOdometry::getChassisYaw() { return chassisIMU.getYaw(); }
 
 float SentryKFOdometry::getMajorYaw() { 
     return turretMajor.getWorldYaw() + this->turretMajorYawError; 
@@ -77,9 +77,9 @@ void SentryKFOdometry::update()
     // Get chassis positional values
     modm::Matrix<float, 3, 1> chassisPos = chassis.getActualVelocityChassisRelative();
     // use the chassis imu to get yaw
-    chassis.getVelocityWorldRelative(chassisPos, mpu.getYaw());
+    chassis.getVelocityWorldRelative(chassisPos, chassisIMU.getYaw());
 
-    float accData[3] = {mpu.getAx(), mpu.getAy(), mpu.getAz()};
+    float accData[3] = {chassisIMU.getAx(), chassisIMU.getAy(), chassisIMU.getAz()};
     modm::Matrix<float, 3, 1> chassisAcc(accData);
 
     float newOdomInput[int(SentryKFOdometry::OdomInput::NUM_INPUTS)] = {
@@ -110,79 +110,3 @@ void SentryKFOdometry::update()
 // }
 
 }  // namespace aruwsrc::algorithms::odometry
-
-// /*
-//  * Copyright (c) 2023 Advanced Robotics at the University of Washington <robomstr@uw.edu>
-//  *
-//  * This file is part of aruw-mcb.
-//  *
-//  * aruw-mcb is free software: you can redistribute it and/or modify
-//  * it under the terms of the GNU General Public License as published by
-//  * the Free Software Foundation, either version 3 of the License, or
-//  * (at your option) any later version.
-//  *
-//  * aruw-mcb is distributed in the hope that it will be useful,
-//  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  * GNU General Public License for more details.
-//  *
-//  * You should have received a copy of the GNU General Public License
-//  * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
-//  */
-
-// #include "aruwsrc/robot/sentry/sentry_kf_odometry.hpp"
-
-// #include "tap/drivers.hpp"
-
-// #include "aruwsrc/control/chassis/holonomic_chassis_subsystem.hpp"
-
-// namespace aruwsrc::algorithms::odometry
-// {
-// SentryKFOdometry::SentryKFOdometry(
-//     tap::Drivers& drivers,
-//     const aruwsrc::chassis::HolonomicChassisSubsystem& chassis,
-//     const aruwsrc::control::turret::SentryTurretMajorSubsystem& turretMajor,
-//     const aruwsrc::control::turret::SentryTurretMinorSubsystem& turretMinorLeft,
-//     const aruwsrc::control::turret::SentryTurretMinorSubsystem& turretMinorRight)
-//     : drivers(drivers),
-//       chassis(chassis),
-//       turretMajor(turretMajor),
-//       turretMinorLeft(turretMinorLeft),
-//       turretMinorRight(turretMinorRight),
-//       kf(KF_A, KF_C, KF_Q, KF_R, KF_P0)
-// {
-// }
-
-// float SentryKFOdometry::getChassisYaw() { return drivers.mpu6500.getYaw(); }
-
-// float SentryKFOdometry::getMajorYaw() { return turretMajor.getWorldYaw(); }
-
-// float SentryKFOdometry::getLeftMinorYaw() { return turretMinorLeft.getWorldYaw(); }
-
-// float SentryKFOdometry::getLeftMinorPitch() { return turretMinorLeft.getWorldPitch(); }
-
-// float SentryKFOdometry::getRightMinorYaw() { return turretMinorRight.getWorldYaw(); }
-
-// float SentryKFOdometry::getRightMinorPitch() { return turretMinorRight.getWorldPitch(); }
-
-// void SentryKFOdometry::update()
-// {
-//     // Get chassis positional values
-//     modm::Matrix<float, 3, 1> chassisPos = chassis.getActualVelocityChassisRelative();
-//     // use the chassis imu to get yaw
-//     chassis.getVelocityWorldRelative(chassisPos, drivers.mpu6500.getYaw());
-
-//     float accData[3] = {drivers.mpu6500.getAx(), drivers.mpu6500.getAy(), drivers.mpu6500.getAz()};
-//     modm::Matrix<float, 3, 1> chassisAcc(accData);
-
-//     float newOdomInput[int(SentryKFOdometry::OdomInput::NUM_INPUTS)] = {
-//         chassisPos[0][0],
-//         chassisAcc[0][0],
-//         chassisPos[1][0],
-//         chassisAcc[1][0],
-//     };
-
-//     kf.performUpdate(newOdomInput);
-// }
-
-// }  // namespace aruwsrc::algorithms::odometry
