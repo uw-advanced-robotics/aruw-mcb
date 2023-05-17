38b38f00d9152fe83141f674271cd21200c78986
Lucas <cornwluc@cs.washington.edu>
2023-05-16 19:23:36 -0700

    getting inter robot communication in place

---
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_motion_strategy_messages.hpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2022 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SENTRY_MOTION_STRATEGY_MESSAGE_TYPES_HPP_
+#define SENTRY_MOTION_STRATEGY_MESSAGE_TYPES_HPP_
+
+#include <cinttypes>
+
+namespace aruwsrc::communication::serial
+{
+
+// @todo: check
+static constexpr uint16_t SENTRY_REQUEST_ROBOT_ID = 0x200;
+
+static constexpr uint16_t SENTRY_RESPONSE_MESSAGE_ID = 0x201;
+
+enum class SentryMotionStrategyMessages : uint8_t
+{
+    NONE = 0,
+    GO_TO_FRIENDLY_BASE,
+    GO_TO_ENEMY_BASE,
+    GO_TO_SUPPLIER_ZONE,
+    NUM_MESSAGE_TYPES,
+};
+}  // namespace aruwsrc::communication::serial
+
+#endif  //  SENTRY_MOTION_STRATEGY_MESSAGE_TYPES_HPP_
