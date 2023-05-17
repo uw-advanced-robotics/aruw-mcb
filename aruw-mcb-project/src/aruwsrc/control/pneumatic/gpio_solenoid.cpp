e1c4a49b4f3ef01e76d30c6514c0f3acfd8674ba
SumedhP <43325361+SumedhP@users.noreply.github.com>
2023-05-16 19:35:53 -0700

    Wrote solenoid code

---
+++ aruw-mcb-project/src/aruwsrc/control/pneumatic/gpio_solenoid.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2023 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "gpio_solenoid.hpp"
+
+namespace aruwsrc::control
+{
+GpioSolenoid::GpioSolenoid(
+    tap::Drivers* drivers,
+    tap::gpio::Digital::OutputPin extendPin,
+    tap::gpio::Digital::OutputPin retractPin)
+    : drivers(drivers),
+      extendPin(extendPin),
+      retractPin(retractPin)
+{
+}
+
+void GpioSolenoid::extend()
+{
+    drivers->digital.set(extendPin, true);
+    drivers->digital.set(retractPin, false);
+}
+
+void GpioSolenoid::retract()
+{
+    drivers->digital.set(extendPin, false);
+    drivers->digital.set(retractPin, true);
+}
+
+void GpioSolenoid::off()
+{
+    drivers->digital.set(extendPin, false);
+    drivers->digital.set(retractPin, false);
+}
+
+}  // namespace aruwsrc::control
