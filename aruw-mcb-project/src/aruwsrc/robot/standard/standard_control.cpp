bf49210f46790b641ca9fe7e63fe09c4062a3ac2
Zyzxx <derek123456789101112131415@gmail.com>
2023-07-02 12:47:31 -0700

    clonk doormatter

--- aruw-mcb-project/src/aruwsrc/robot/standard/standard_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/standard/standard_control.cpp
@@ -83,8 +83,8 @@
 #include "aruwsrc/control/barrel-switcher/barrel_switcher_subsystem.hpp"
 #include "aruwsrc/control/governor/dual_barrel_heat_limit_governor.hpp"
 #include "aruwsrc/control/governor/dual_barrel_ref_system_projectile_launched_governor.hpp"
-#include "aruwsrc/control/launcher/dual_barrel_referee_feedback_friction_wheel_subsystem.hpp"
 #include "aruwsrc/control/launcher/dual_barrel_friction_wheel_spin_ref_limited_command.hpp"
+#include "aruwsrc/control/launcher/dual_barrel_referee_feedback_friction_wheel_subsystem.hpp"
 #endif
 
 #ifdef PLATFORM_HOSTED
