e4572dd45813743893fd519b1f9beeb4d893ce63
snowshoes <sliben@uw.edu>
2023-09-14 04:27:02 -0700

    smarter commenting out

--- aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_control.cpp
@@ -1,23 +1,23 @@
-// /*
-//  * Copyright (c) 2022 Advanced Robotics at the University of Washington <robomstr@uw.edu>
-//  *
-//  * This file is part of aruw-mcb.
-//  *
-//  * aruw-mcb is free software: you can redistribute it and/or modify
-//  * it under the terms of the GNU General Public License as published by
-//  * the Free Software Foundation, either version 3 of the License, or
-//  * (at your option) any later version.
-//  *
-//  * aruw-mcb is distributed in the hope that it will be useful,
-//  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  * GNU General Public License for more details.
-//  *
-//  * You should have received a copy of the GNU General Public License
-//  * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
-//  */
+/*
+ * Copyright (c) 2022 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-// #if defined(TARGET_SENTRY_BEEHIVE)
+#if defined(TARGET_SENTRY_BEEHIVE)
 
 // #include "tap/control/command_mapper.hpp"
 // #include "tap/control/governor/governor_limited_command.hpp"
@@ -82,16 +82,16 @@
 // using namespace tap::communication::serial;
 // using namespace aruwsrc::sentry;
 
-// /*
-//  * NOTE: We are using the DoNotUse_getDrivers() function here
-//  *      because this file defines all subsystems and command
-//  *      and thus we must pass in the single statically allocated
-//  *      Drivers class to all of these objects.
-//  */
-// driversFunc drivers = DoNotUse_getDrivers;
+/*
+ * NOTE: We are using the DoNotUse_getDrivers() function here
+ *      because this file defines all subsystems and command
+ *      and thus we must pass in the single statically allocated
+ *      Drivers class to all of these objects.
+ */
+driversFunc drivers = DoNotUse_getDrivers;
 
-// namespace sentry_control
-// {
+namespace sentry_control
+{
 // static constexpr Digital::InputPin LEFT_LIMIT_SWITCH = Digital::InputPin::B;
 // static constexpr Digital::InputPin RIGHT_LIMIT_SWITCH = Digital::InputPin::C;
 
@@ -393,96 +393,96 @@
 //     {&sentryDriveManual2},
 //     RemoteMapState(Remote::Switch::LEFT_SWITCH, Remote::SwitchState::MID));
 
-// /* initialize subsystems ----------------------------------------------------*/
-// void initializeSubsystems()
-// {
-//     sentryDrive.initialize();
-//     turretZero.agitator.initialize();
-//     turretZero.frictionWheels.initialize();
-//     turretZero.turretSubsystem.initialize();
-//     turretOne.agitator.initialize();
-//     turretOne.frictionWheels.initialize();
-//     turretOne.turretSubsystem.initialize();
-//     odometrySubsystem.initialize();
-//     sentryResponseSubsystem.initialize();
-//     buzzer.initialize();
-// }
+/* initialize subsystems ----------------------------------------------------*/
+void initializeSubsystems()
+{
+    // sentryDrive.initialize();
+    // turretZero.agitator.initialize();
+    // turretZero.frictionWheels.initialize();
+    // turretZero.turretSubsystem.initialize();
+    // turretOne.agitator.initialize();
+    // turretOne.frictionWheels.initialize();
+    // turretOne.turretSubsystem.initialize();
+    // odometrySubsystem.initialize();
+    // sentryResponseSubsystem.initialize();
+    // buzzer.initialize();
+}
 
-// RemoteSafeDisconnectFunction remoteSafeDisconnectFunction(drivers());
+RemoteSafeDisconnectFunction remoteSafeDisconnectFunction(drivers());
 
-// /* register subsystems here -------------------------------------------------*/
-// void registerSentrySubsystems(Drivers *drivers)
-// {
-//     drivers->commandScheduler.registerSubsystem(&sentryDrive);
-//     drivers->commandScheduler.registerSubsystem(&turretZero.agitator);
-//     drivers->commandScheduler.registerSubsystem(&turretZero.frictionWheels);
-//     drivers->commandScheduler.registerSubsystem(&turretZero.turretSubsystem);
-//     drivers->commandScheduler.registerSubsystem(&turretOne.agitator);
-//     drivers->commandScheduler.registerSubsystem(&turretOne.frictionWheels);
-//     drivers->commandScheduler.registerSubsystem(&turretOne.turretSubsystem);
-//     drivers->commandScheduler.registerSubsystem(&odometrySubsystem);
-//     drivers->commandScheduler.registerSubsystem(&sentryResponseSubsystem);
-//     drivers->visionCoprocessor.attachOdometryInterface(&odometrySubsystem);
-//     drivers->visionCoprocessor.attachTurretOrientationInterface(&turretZero.turretSubsystem, 0);
-//     drivers->visionCoprocessor.attachTurretOrientationInterface(&turretOne.turretSubsystem, 1);
-//     drivers->commandScheduler.registerSubsystem(&buzzer);
-// }
+/* register subsystems here -------------------------------------------------*/
+void registerSentrySubsystems(Drivers *drivers)
+{
+    // drivers->commandScheduler.registerSubsystem(&sentryDrive);
+    // drivers->commandScheduler.registerSubsystem(&turretZero.agitator);
+    // drivers->commandScheduler.registerSubsystem(&turretZero.frictionWheels);
+    // drivers->commandScheduler.registerSubsystem(&turretZero.turretSubsystem);
+    // drivers->commandScheduler.registerSubsystem(&turretOne.agitator);
+    // drivers->commandScheduler.registerSubsystem(&turretOne.frictionWheels);
+    // drivers->commandScheduler.registerSubsystem(&turretOne.turretSubsystem);
+    // drivers->commandScheduler.registerSubsystem(&odometrySubsystem);
+    // drivers->commandScheduler.registerSubsystem(&sentryResponseSubsystem);
+    // drivers->visionCoprocessor.attachOdometryInterface(&odometrySubsystem);
+    // drivers->visionCoprocessor.attachTurretOrientationInterface(&turretZero.turretSubsystem, 0);
+    // drivers->visionCoprocessor.attachTurretOrientationInterface(&turretOne.turretSubsystem, 1);
+    // drivers->commandScheduler.registerSubsystem(&buzzer);
+}
 
-// /* set any default commands to subsystems here ------------------------------*/
-// void setDefaultSentryCommands(Drivers *)
-// {
-//     sentryDrive.setDefaultCommand(&sentryAutoDrive);
-//     turretZero.frictionWheels.setDefaultCommand(&turretZero.spinFrictionWheels);
-//     turretOne.frictionWheels.setDefaultCommand(&turretOne.spinFrictionWheels);
-//     turretZero.turretSubsystem.setDefaultCommand(&turretZero.turretCVCommand);
-//     turretOne.turretSubsystem.setDefaultCommand(&turretOne.turretCVCommand);
-//     turretZero.agitator.setDefaultCommand(
-//         &turretZero.rotateAndUnjamAgitatorWithHeatAndCvLimitingWhenCvOnline);
-//     turretOne.agitator.setDefaultCommand(
-//         &turretOne.rotateAndUnjamAgitatorWithHeatAndCvLimitingWhenCvOnline);
-// }
+/* set any default commands to subsystems here ------------------------------*/
+void setDefaultSentryCommands(Drivers *)
+{
+    // sentryDrive.setDefaultCommand(&sentryAutoDrive);
+    // turretZero.frictionWheels.setDefaultCommand(&turretZero.spinFrictionWheels);
+    // turretOne.frictionWheels.setDefaultCommand(&turretOne.spinFrictionWheels);
+    // turretZero.turretSubsystem.setDefaultCommand(&turretZero.turretCVCommand);
+    // turretOne.turretSubsystem.setDefaultCommand(&turretOne.turretCVCommand);
+    // turretZero.agitator.setDefaultCommand(
+    //     &turretZero.rotateAndUnjamAgitatorWithHeatAndCvLimitingWhenCvOnline);
+    // turretOne.agitator.setDefaultCommand(
+    //     &turretOne.rotateAndUnjamAgitatorWithHeatAndCvLimitingWhenCvOnline);
+}
 
-// /* add any starting commands to the scheduler here --------------------------*/
-// void startSentryCommands(Drivers *drivers)
-// {
-//     drivers->commandScheduler.addCommand(&imuCalibrateCommand);
+/* add any starting commands to the scheduler here --------------------------*/
+void startSentryCommands(Drivers *drivers)
+{
+    // drivers->commandScheduler.addCommand(&imuCalibrateCommand);
 
-//     sentryRequestHandler.attachPauseProjectileLaunchingMessageHandler(
-//         pauseProjectileLaunchMessageHandler);
-//     sentryRequestHandler.attachSelectNewRobotMessageHandler(selectNewRobotMessageHandler);
-//     sentryRequestHandler.attachTargetNewQuadrantMessageHandler(targetNewQuadrantMessageHandler);
-//     sentryRequestHandler.attachToggleDriveMovementMessageHandler(toggleDriveMovementMessageHandler);
-//     drivers->refSerial.attachRobotToRobotMessageHandler(
-//         aruwsrc::communication::serial::SENTRY_REQUEST_ROBOT_ID,
-//         &sentryRequestHandler);
-// }
+    // sentryRequestHandler.attachPauseProjectileLaunchingMessageHandler(
+    //     pauseProjectileLaunchMessageHandler);
+    // sentryRequestHandler.attachSelectNewRobotMessageHandler(selectNewRobotMessageHandler);
+    // sentryRequestHandler.attachTargetNewQuadrantMessageHandler(targetNewQuadrantMessageHandler);
+    // sentryRequestHandler.attachToggleDriveMovementMessageHandler(toggleDriveMovementMessageHandler);
+    // drivers->refSerial.attachRobotToRobotMessageHandler(
+    //     aruwsrc::communication::serial::SENTRY_REQUEST_ROBOT_ID,
+    //     &sentryRequestHandler);
+}
 
-// /* register io mappings here ------------------------------------------------*/
-// void registerSentryIoMappings(Drivers *drivers)
-// {
-//     drivers->commandMapper.addMap(&rightSwitchDown);
-//     drivers->commandMapper.addMap(&rightSwitchUp);
-//     drivers->commandMapper.addMap(&leftSwitchDown);
-//     drivers->commandMapper.addMap(&leftSwitchMid);
-// }
-// }  // namespace sentry_control
+/* register io mappings here ------------------------------------------------*/
+void registerSentryIoMappings(Drivers *drivers)
+{
+    // drivers->commandMapper.addMap(&rightSwitchDown);
+    // drivers->commandMapper.addMap(&rightSwitchUp);
+    // drivers->commandMapper.addMap(&leftSwitchDown);
+    // drivers->commandMapper.addMap(&leftSwitchMid);
+}
+}  // namespace sentry_control
 
-// namespace aruwsrc::sentry
-// {
-// void initSubsystemCommands(aruwsrc::sentry::Drivers *drivers)
-// {
-//     drivers->commandScheduler.setSafeDisconnectFunction(
-//         &sentry_control::remoteSafeDisconnectFunction);
-//     sentry_control::initializeSubsystems();
-//     sentry_control::registerSentrySubsystems(drivers);
-//     sentry_control::setDefaultSentryCommands(drivers);
-//     sentry_control::startSentryCommands(drivers);
-//     sentry_control::registerSentryIoMappings(drivers);
-// }
-// }  // namespace aruwsrc::sentry
+namespace aruwsrc::sentry
+{
+void initSubsystemCommands(aruwsrc::sentry::Drivers *drivers)
+{
+    // drivers->commandScheduler.setSafeDisconnectFunction(
+    //     &sentry_control::remoteSafeDisconnectFunction);
+    // sentry_control::initializeSubsystems();
+    // sentry_control::registerSentrySubsystems(drivers);
+    // sentry_control::setDefaultSentryCommands(drivers);
+    // sentry_control::startSentryCommands(drivers);
+    // sentry_control::registerSentryIoMappings(drivers);
+}
+}  // namespace aruwsrc::sentry
 
 // #ifndef PLATFORM_HOSTED
 // imu::ImuCalibrateCommand *getImuCalibrateCommand() { return &sentry_control::imuCalibrateCommand; }
 // #endif
 
-// #endif
+#endif
