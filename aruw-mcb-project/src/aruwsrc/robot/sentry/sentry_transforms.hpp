738ff0a1b03ecf5f479e6302f9c6ea888e776dcb
spoonk <cornwlucas@gmail.com>
2023-10-12 08:45:18 -0700

    blocked by subsystem implementations

---
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_transforms.hpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef SENTRY_TRANSFORMS_HPP_
+#define SENTRY_TRANSFORMS_HPP_
+
+#include "tap/algorithms/transforms/transform.hpp"
+
+using namespace tap::algorithms::transforms;
+
+namespace aruwsrc::algorithms::transforms
+{
+
+class SentryTransforms
+{
+public:
+    // @todo: rename turret minors
+    // @note: blocked by subsystem implementations
+    // SentryTransforms(
+    //     const tap::algorithms::odometry::Odometry2DInterface& chassisOdometry,
+    //     const SentryTurretMajorSubsystem& turretMajor,
+    //     const SentryTurretMinorSubsystem& turretMinorGirlboss,
+    //     const SentryTurretMinorSubsystem& turretMinorMalewife);
+};
+}  // namespace aruwsrc::algorithms::transforms
+
+#endif  // SENTRY_TRANSFORMS_HPP_
