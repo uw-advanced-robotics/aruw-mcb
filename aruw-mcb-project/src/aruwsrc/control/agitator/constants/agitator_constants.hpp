d94a2292c3dda935955e2f583940293f20772196
SumedhP <43325361+SumedhP@users.noreply.github.com>
2023-10-19 17:59:25 -0700

    Mmmmmm I did shit

--- aruw-mcb-project/src/aruwsrc/control/agitator/constants/agitator_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/control/agitator/constants/agitator_constants.hpp
@@ -26,6 +26,8 @@
 #include "aruwsrc/robot/sentry/sentry_beehive_agitator_constants.hpp"
 #elif defined(TARGET_HERO_CYCLONE)
 #include "aruwsrc/robot/hero/hero_agitator_constants.hpp"
+#elif defined(TARGET_TESTBED)
+#include "aruwsrc/robot/testbed/testbed_agitator_constants.hpp"
 #endif
 
 #endif  // AGITATOR_CONSTANTS_HPP_
--- aruw-mcb-project/src/aruwsrc/robot/testbed/testbed_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/testbed/testbed_control.cpp
@@ -19,10 +19,28 @@
 
 #if defined(TARGET_TESTBED)
 
+#include "tap/control/setpoint/commands/move_integral_command.hpp"
+#include "tap/motor/dji_motor.hpp"
+
+#include "aruwsrc/control/agitator/constants/agitator_constants.hpp"
+#include "aruwsrc/control/agitator/velocity_agitator_subsystem.hpp"
 #include "aruwsrc/control/safe_disconnect.hpp"
 #include "aruwsrc/drivers_singleton.hpp"
+#include "aruwsrc/robot/robot_control.hpp"
 #include "aruwsrc/robot/testbed/testbed_drivers.hpp"
-#include "tap/motor/dji_motor.hpp"
+
+#include "aruwsrc/control/agitator/manual_fire_rate_reselection_manager.hpp"
+#include "aruwsrc/control/governor/fire_rate_limit_governor.hpp"
+#include "tap/control/governor/governor_limited_command.hpp"
+
+
+using namespace aruwsrc::agitator;
+using namespace aruwsrc::control::agitator;
+using namespace tap::control::setpoint;
+using namespace aruwsrc::control::governor;
+using namespace tap::control::governor;
+
+
 
 using namespace aruwsrc::testbed;
 
@@ -36,17 +54,72 @@ driversFunc drivers = DoNotUse_getDrivers;
 
 namespace testbed_control
 {
-	// Uhhhh just a motor
-	tap::motor::DjiMotor motor(drivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "Testbed motor");
 
-	
+// Motor you just want to spin real fast
+tap::motor::MotorId spinMotorID = tap::motor::MotorId::MOTOR2;
+
+tap::motor::DjiMotor spinMotor(
+    drivers(),
+    spinMotorID,
+    tap::can::CanBus::CAN_BUS1,
+    false,
+    "Testbed spinny motor");
+
+// Agitator motor
+VelocityAgitatorSubsystem agitator(
+    drivers(),
+    constants::AGITATOR_PID_CONFIG,
+    constants::AGITATOR_CONFIG);
+
+// An agitator??
+MoveIntegralCommand rotateAgitator(agitator, constants::AGITATOR_ROTATE_CONFIG);
+
+ManualFireRateReselectionManager manualFireRateReselectionManager;
+FireRateLimitGovernor fireRateLimitGovernor(manualFireRateReselectionManager);
+
+GovernorLimitedCommand<1> rotateAndUnjamAgitatorButLimited(
+    {&agitator},
+    rotateAndUnjamAgitator,
+    {&fireRateLimitGovernor});
 
 
-	// An agitator??
+void initializeSubsystems()
+{
+    spinMotor.initialize();
+    agitator.initialize();
 
-	aruwsrc::control::RemoteSafeDisconnectFunction remoteSafeDisconnectFunction(drivers());
+	manualFireRateReselectionManager.setFireRate(30);
+}
 
+void registerTestbedSubsystems(Drivers *drivers)
+{
+    drivers->commandScheduler.registerSubsystem(&agitator);
+}
+
+void setDefaultTestbedCommands(Drivers *drivers)
+{
+	agitator.setDefaultCommand(&rotateAndUnjamAgitatorButLimited);
+}
+
+void startTestbedCommands(Drivers *drivers)
+{
+    drivers->commandScheduler.addCommand(&rotateAndUnjamAgitatorButLimited);
+
+	spinMotor.setDesiredOutput(10'000);
 
 }
+
+}  // namespace testbed_control
+
+namespace aruwsrc::testbed
+{
+void initSubsystemCommands(aruwsrc::testbed::Drivers *drivers)
+{
+    testbed_control::initializeSubsystems();
+    testbed_control::registerTestbedSubsystems(drivers);
+    testbed_control::setDefaultTestbedCommands(drivers);
+    testbed_control::startTestbedCommands(drivers);
+}
+}  // namespace aruwsrc::testbed
 
 #endif
