8285acc9681c83ea923347b448f2c5d270211516
Nikolas McNamee <nikolasmcnamee@gmail.com>
2024-01-30 18:27:52 -0800

    removed vision_coprocessor ifndef for hero

--- aruw-mcb-project/src/aruwsrc/communication/serial/vision_coprocessor.cpp
+++ aruw-mcb-project/src/aruwsrc/communication/serial/vision_coprocessor.cpp
@@ -80,13 +80,8 @@ void VisionCoprocessor::initializeCV()
 #endif
 
     cvOfflineTimeout.restart(TIME_OFFLINE_CV_AIM_DATA_MS);
-#if defined(TARGET_HERO_CYCLONE)
-    drivers->uart.init<VISION_COPROCESSOR_TX_UART_PORT, 900'000>();
-    drivers->uart.init<VISION_COPROCESSOR_RX_UART_PORT, 900'000>();
-#else
     drivers->uart.init<VISION_COPROCESSOR_TX_UART_PORT, 1'000'000>();
     drivers->uart.init<VISION_COPROCESSOR_RX_UART_PORT, 1'000'000>();
-#endif
 }
 
 void VisionCoprocessor::messageReceiveCallback(const ReceivedSerialMessage& completeMessage)
