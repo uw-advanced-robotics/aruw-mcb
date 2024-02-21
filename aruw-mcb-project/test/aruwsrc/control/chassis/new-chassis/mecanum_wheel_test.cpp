79d8939b4b8418294649f7e5fa96e6c8e4311ac8
Tong Lin <tlin01@uw.edu>
2024-02-20 18:39:43 -0800

    fixed formatting actual

--- aruw-mcb-project/test/aruwsrc/control/chassis/new-chassis/mecanum_wheel_test.cpp
+++ aruw-mcb-project/test/aruwsrc/control/chassis/new-chassis/mecanum_wheel_test.cpp
@@ -17,20 +17,18 @@
  * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include "aruwsrc/control/chassis/new-chassis/mecanum_wheel.hpp"
+
 #include <gtest/gtest.h>
 
-
-#include "tap/drivers.hpp"
-#include "aruwsrc/control/chassis/new-chassis/wheel.hpp"
-#include "aruwsrc/control/chassis/new-chassis/mecanum_wheel.hpp"
 #include "tap/algorithms/cmsis_mat.hpp"
-
 #include "tap/algorithms/math_user_utils.hpp"
+#include "tap/drivers.hpp"
 
 #include "aruwsrc/communication/sensors/current/acs712_current_sensor_config.hpp"
+#include "aruwsrc/control/chassis/new-chassis/wheel.hpp"
 #include "aruwsrc/control/chassis/swerve_chassis_subsystem.hpp"
 #include "aruwsrc/util_macros.hpp"
-
 
 using namespace aruwsrc::chassis;
 using namespace testing;
@@ -40,34 +38,31 @@ class MecanumWheelTest : public Test
 {
 protected:
     MecanumWheelTest()
-    : mockMotor(&drivers, tap::motor::MOTOR1, CAN_BUS_MOTORS, false, "mock motor"),
-    mecWheel(mockMotor, testConfig, 1)
+        : mockMotor(&drivers, tap::motor::MOTOR1, CAN_BUS_MOTORS, false, "mock motor"),
+          mecWheel(mockMotor, testConfig, 1)
     {
     }
 
-    void SetUp() override {
+    void SetUp() override
+    {
         ON_CALL(drivers.refSerial, getRefSerialReceivingData).WillByDefault(testing::Return(false));
         ON_CALL(drivers.refSerial, getRobotData).WillByDefault(testing::ReturnRef(robotData));
     }
 
-    SmoothPidConfig pidConfig = {0,
-    0,
-    0,
-    0,
-    0};
+    SmoothPidConfig pidConfig = {0, 0, 0, 0, 0};
 
     WheelConfig testConfig = {
-    .wheelPositionChassisRelativeX = 1.0f,
-    .wheelPositionChassisRelativeY = 1.0f,
-    .wheelOrientationChassisRelative = M_PI_2,
-    .distFromCenterToWheel = sqrt(2.0f),
-    .diameter = 2.0f,
-    .gearRatio = 10.0f,
-    .motorGearRatio = 100.0f,
-    .velocityPidConfig = pidConfig,
-    .isPowered = true,
-    .maxWheelRPM = 1000,
-    .inverted = false,
+        .wheelPositionChassisRelativeX = 1.0f,
+        .wheelPositionChassisRelativeY = 1.0f,
+        .wheelOrientationChassisRelative = M_PI_2,
+        .distFromCenterToWheel = sqrt(2.0f),
+        .diameter = 2.0f,
+        .gearRatio = 10.0f,
+        .motorGearRatio = 100.0f,
+        .velocityPidConfig = pidConfig,
+        .isPowered = true,
+        .maxWheelRPM = 1000,
+        .inverted = false,
     };
 
     tap::Drivers drivers;
@@ -78,30 +73,23 @@ protected:
 };
 
 TEST_F(MecanumWheelTest, calculate_desired_wheel_velocity_returns_desired_velocity)
-    {
-        modm::Pair<float, float> desiredWheelVel = mecWheel.calculateDesiredWheelVelocity(0, 0, 0);
-        ASSERT_EQ(0.0f, desiredWheelVel.first);
-        ASSERT_EQ(0.0f, desiredWheelVel.second);
-
-        modm::Pair<float, float> desiredWheelVel2 = mecWheel.calculateDesiredWheelVelocity(1, 0, 0);
-        ASSERT_EQ(0.0f, desiredWheelVel2.first);
-        ASSERT_EQ(-1.0f, desiredWheelVel2.second);
-    }
-
-TEST_F(MecanumWheelTest, numMotorsInitializedCorrectly)
 {
-    EXPECT_EQ(1, mecWheel.getNumMotors());  
+    modm::Pair<float, float> desiredWheelVel = mecWheel.calculateDesiredWheelVelocity(0, 0, 0);
+    ASSERT_EQ(0.0f, desiredWheelVel.first);
+    ASSERT_EQ(0.0f, desiredWheelVel.second);
+
+    modm::Pair<float, float> desiredWheelVel2 = mecWheel.calculateDesiredWheelVelocity(1, 0, 0);
+    ASSERT_EQ(0.0f, desiredWheelVel2.first);
+    ASSERT_EQ(-1.0f, desiredWheelVel2.second);
 }
 
+TEST_F(MecanumWheelTest, numMotorsInitializedCorrectly) { EXPECT_EQ(1, mecWheel.getNumMotors()); }
+
 TEST_F(MecanumWheelTest, execute_wheel_velocity_calculates_correct_setpoint)
-    {
-        mecWheel.executeWheelVelocity(0.0f, 0.0f);
-        ASSERT_EQ(0, mecWheel.getSetpoint());
+{
+    mecWheel.executeWheelVelocity(0.0f, 0.0f);
+    ASSERT_EQ(0, mecWheel.getSetpoint());
 
-        mecWheel.executeWheelVelocity(0.0f, -1.0f);
-        EXPECT_EQ(-1.0f, mecWheel.getSetpoint());
-    }
-
-
-
-
+    mecWheel.executeWheelVelocity(0.0f, -1.0f);
+    EXPECT_EQ(-1.0f, mecWheel.getSetpoint());
+}
