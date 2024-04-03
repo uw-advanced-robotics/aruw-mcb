b1ba3c6362aca2d540788bdc81bc41744b0b9ca0
Nikolas McNamee <nikolasmcnamee@gmail.com>
2024-04-02 19:53:29 -0700

    cv working

--- aruw-mcb-project/src/aruwsrc/communication/serial/vision_coprocessor.cpp
+++ aruw-mcb-project/src/aruwsrc/communication/serial/vision_coprocessor.cpp
@@ -173,6 +173,7 @@ void VisionCoprocessor::sendRebootMessage()
         sizeof(rebootMessage));
 }
 
+
 void VisionCoprocessor::sendOdometryData()
 {
     assert(odometryInterface != nullptr);
@@ -213,7 +214,11 @@ void VisionCoprocessor::sendOdometryData()
         assert(turretOrientationInterfaces[i] != nullptr);
         odometryData->turretOdometry[i].timestamp =
             turretOrientationInterfaces[i]->getLastMeasurementTimeMicros();
+        #ifdef TARGET_HERO_CYCLONE
+        odometryData->turretOdometry[i].pitch = -turretOrientationInterfaces[i]->getWorldPitch();
+        #else
         odometryData->turretOdometry[i].pitch = turretOrientationInterfaces[i]->getWorldPitch();
+        #endif
         odometryData->turretOdometry[i].yaw = turretOrientationInterfaces[i]->getWorldYaw();
     }
 
