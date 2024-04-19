d58ecaa9133782b94e260cd22f3e20d10abad3a1
pie474 <chinmaymurthy474@gmail.com>
2024-04-18 17:44:20 -0700

    copy over and concat minor world frame controllers no edits

---
+++ aruw-mcb-project/src/aruwsrc/control/turret/algorithms/minor_world_frame_turret_controller.cpp
@@ -0,0 +1,240 @@
+/*
+ * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "tap/algorithms/contiguous_float.hpp"
+
+#include "../constants/turret_constants.hpp"
+#include "../turret_subsystem.hpp"
+#include "aruwsrc/communication/can/turret_mcb_can_comm.hpp"
+
+#include "minor_world_frame_turret_yaw_controller.hpp"
+
+using namespace tap::algorithms;
+
+namespace aruwsrc::control::turret::algorithms
+{
+
+/**
+ * Runs a world frame cascade (position -> velocity) PID controller.
+ *
+ * @param[in] worldFrameAngleSetpoint World frame angle setpoint, not required to be normalized, in
+ * radians.
+ * @param[in] worldFrameAngleMeasurement World frame angle measurement, not required to be
+ * normalized, in radians.
+ * @param[in] worldFrameVelocityMeasured World frame angular velocity measurement, in
+ * radians/second.
+ * @param[in] dt Time change since this function was last called, in ms.
+ * @param[in] turretMotor TurretMotor associated with the angles being measured.
+ * @param[out] positionPid Position PID controller.
+ * @param[out] velocityPid Velocity PID controller.
+ * @return desired PID output from running the position -> velocity cascade controller
+ */
+
+WorldFrameTurretYawCascadePIDControllerMinor::WorldFrameTurretYawCascadePIDControllerMinor(
+    const tap::algorithms::transforms::Transform<
+        aruwsrc::sentry::WorldFrame,
+        aruwsrc::sentry::TurretMajorFrame> &worldToBaseTransform,
+    TurretMotor &yawMotor,
+    tap::algorithms::SmoothPid &positionPid,
+    tap::algorithms::SmoothPid &velocityPid,
+    const aruwsrc::can::TurretMCBCanComm &turretMCB)
+    : TurretYawControllerInterface(yawMotor),
+      worldToBaseTransform(worldToBaseTransform),
+      positionPid(positionPid),
+      velocityPid(velocityPid),
+      worldFrameSetpoint(0),
+      yawMotor(yawMotor),
+      turretMCB(turretMCB)
+{
+}
+
+void WorldFrameTurretYawCascadePIDControllerMinor::initialize()
+{
+    if (yawMotor.getTurretController() != this)
+    {
+        positionPid.reset();
+        velocityPid.reset();
+
+        worldFrameSetpoint = yawMotor.getChassisFrameSetpoint() + worldToBaseTransform.getYaw();
+
+        yawMotor.attachTurretController(this);
+    }
+}
+
+float localSetpoint = 0;
+
+// @todo implement separate controller with limiting or refactor elsewhere
+//       rationale: it is not at all intuitive or expected for angle limiting to occur here; makes
+//       code difficult to trace, follow, and maintain
+void WorldFrameTurretYawCascadePIDControllerMinor::runController(
+    const uint32_t dt,
+    const float desiredSetpoint)
+{
+    // float localAngle = turretMCB.getYawUnwrapped() - worldToBaseTransform.getYaw();
+
+    localSetpoint = desiredSetpoint + yawMotor.getChassisFrameUnwrappedMeasuredAngle() -
+                    turretMCB.getYawUnwrapped();
+
+    yawMotor.setChassisFrameSetpoint(localSetpoint);
+
+    localSetpoint = yawMotor.getChassisFrameSetpoint();
+
+    worldFrameSetpoint = localSetpoint - yawMotor.getChassisFrameUnwrappedMeasuredAngle() +
+                         turretMCB.getYawUnwrapped();
+
+    const float positionControllerError =
+        turretMotor.getValidMinError(worldFrameSetpoint, turretMCB.getYawUnwrapped());
+
+    positionPidOutput =
+        positionPid.runController(positionControllerError, turretMCB.getYawVelocity(), dt);
+
+    const float velocityPidOutput =
+        velocityPid.runControllerDerivateError(positionPidOutput - turretMCB.getYawVelocity(), dt);
+
+    yawMotor.setMotorOutput(velocityPidOutput);
+}
+
+// @todo what's the point of this; overridden by runController anyways?
+void WorldFrameTurretYawCascadePIDControllerMinor::setSetpoint(float desiredSetpoint)
+{
+    float localSetpoint =
+        turretMotor.getSetpointWithinTurretRange(desiredSetpoint - worldToBaseTransform.getYaw());
+
+    worldFrameSetpoint = localSetpoint + worldToBaseTransform.getYaw();
+}
+
+float WorldFrameTurretYawCascadePIDControllerMinor::getSetpoint() const
+{
+    return worldFrameSetpoint;
+}
+
+float WorldFrameTurretYawCascadePIDControllerMinor::getMeasurement() const
+{
+    return yawMotor.getChassisFrameMeasuredAngle().getValue() + worldToBaseTransform.getYaw();
+}
+
+// @todo ask benjamin about this (benjamin go look at your tracking sheet)
+bool WorldFrameTurretYawCascadePIDControllerMinor::isOnline() const { return yawMotor.isOnline(); }
+
+/**
+ * Runs a world frame cascade (position -> velocity) PID controller.
+ *
+ * @param[in] worldFrameAngleSetpoint World frame angle setpoint, not required to be normalized, in
+ * radians.
+ * @param[in] worldFrameAngleMeasurement World frame angle measurement, not required to be
+ * normalized, in radians.
+ * @param[in] worldFrameVelocityMeasured World frame angular velocity measurement, in
+ * radians/second.
+ * @param[in] dt Time change since this function was last called, in ms.
+ * @param[in] turretMotor TurretMotor associated with the angles being measured.
+ * @param[out] positionPid Position PID controller.
+ * @param[out] velocityPid Velocity PID controller.
+ * @return desired PID output from running the position -> velocity cascade controller
+ */
+
+WorldFrameTurretPitchCascadePIDControllerMinor::WorldFrameTurretPitchCascadePIDControllerMinor(
+    const tap::algorithms::transforms::Transform<
+        aruwsrc::sentry::WorldFrame,
+        aruwsrc::sentry::TurretMajorFrame> &worldToBaseTransform,
+    TurretMotor &pitchMotor,
+    tap::algorithms::SmoothPid &positionPid,
+    tap::algorithms::SmoothPid &velocityPid,
+    const aruwsrc::can::TurretMCBCanComm &turretMCB)
+    : TurretPitchControllerInterface(pitchMotor),
+      worldToBaseTransform(worldToBaseTransform),
+      positionPid(positionPid),
+      velocityPid(velocityPid),
+      worldFrameSetpoint(0),
+      pitchMotor(pitchMotor),
+      turretMCB(turretMCB)
+{
+}
+
+void WorldFrameTurretPitchCascadePIDControllerMinor::initialize()
+{
+    if (pitchMotor.getTurretController() != this)
+    {
+        positionPid.reset();
+        velocityPid.reset();
+
+        worldFrameSetpoint = pitchMotor.getChassisFrameSetpoint() + worldToBaseTransform.getPitch();
+
+        pitchMotor.attachTurretController(this);
+    }
+}
+
+float velocityPidOutput = 0.0f;
+
+// @todo implement separate controller with limiting or refactor elsewhere
+//       rationale: it is not at all intuitive or expected for angle limiting to occur here; makes
+//       code difficult to trace, follow, and maintain
+void WorldFrameTurretPitchCascadePIDControllerMinor::runController(
+    const uint32_t dt,
+    const float desiredSetpoint)
+{
+    float localAngle = turretMCB.getPitch() - worldToBaseTransform.getPitch();
+
+    const float worldVelocity = turretMCB.getPitchVelocity();
+
+    float localSetpoint =
+        turretMotor.getSetpointWithinTurretRange(desiredSetpoint - worldToBaseTransform.getPitch());
+
+    pitchMotor.setChassisFrameSetpoint(localSetpoint);
+
+    localSetpoint = pitchMotor.getChassisFrameSetpoint();
+
+    worldFrameSetpoint = localSetpoint + worldToBaseTransform.getPitch();
+
+    const float positionControllerError = turretMotor.getValidMinError(localSetpoint, localAngle);
+
+    positionPidOutput =
+        positionPid.runController(positionControllerError, turretMCB.getPitchVelocity(), dt);
+
+    float velocityPidOutput =
+        velocityPid.runControllerDerivateError(positionPidOutput - worldVelocity, dt);
+
+    velocityPidOutput +=
+        computeGravitationalForceOffset(CG_X, CG_Z, turretMCB.getPitch(), G_COMPENSATION_SCALAR);
+
+    pitchMotor.setMotorOutput(velocityPidOutput);
+}
+
+// @todo what's the point of this; overridden by runController anyways?
+void WorldFrameTurretPitchCascadePIDControllerMinor::setSetpoint(float desiredSetpoint)
+{
+    worldFrameSetpoint = desiredSetpoint;
+}
+
+float WorldFrameTurretPitchCascadePIDControllerMinor::getSetpoint() const
+{
+    return worldFrameSetpoint;
+}
+
+float WorldFrameTurretPitchCascadePIDControllerMinor::getMeasurement() const
+{
+    return pitchMotor.getChassisFrameUnwrappedMeasuredAngle() + worldToBaseTransform.getPitch();
+}
+
+// @todo ask benjamin about this (benjamin go look at your tracking sheet)
+bool WorldFrameTurretPitchCascadePIDControllerMinor::isOnline() const
+{
+    return pitchMotor.isOnline();
+}
+
+}  // namespace aruwsrc::control::turret::algorithms
---
+++ aruw-mcb-project/src/aruwsrc/control/turret/algorithms/minor_world_frame_turret_controller.hpp
@@ -0,0 +1,203 @@
+/*
+ * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef MINOR_WORLD_FRAME_TURRET_CONTROLLER_HPP_
+#define MINOR_WORLD_FRAME_TURRET_CONTROLLER_HPP_
+
+#include <cstdint>
+
+#include "tap/algorithms/contiguous_float.hpp"
+#include "tap/algorithms/fuzzy_pd.hpp"
+
+#include "aruwsrc/communication/can/turret_mcb_can_comm.hpp"
+
+#include "turret_controller_interface.hpp"
+
+// @todo primarily here to avoid namespace pain
+#include "tap/algorithms/transforms/transform.hpp"
+
+#include "aruwsrc/control/chassis/holonomic_chassis_subsystem.hpp"
+#include "aruwsrc/robot/sentry/sentry_transforms.hpp"
+#include "aruwsrc/robot/sentry/sentry_turret_minor_subsystem.hpp"
+
+namespace aruwsrc::control::turret
+{
+class TurretMotor;
+}
+
+namespace aruwsrc::control::turret::algorithms
+{
+/**
+ * World frame turret yaw controller. Requires that a development board be mounted rigidly on the
+ * turret and connected via the `TurretMCBCanComm` class. The development board's IMU is used to
+ * determine the turret's world frame coordinates directly, making this controller better than the
+ * `WorldFrameChassisImuTurretController`.
+ *
+ * Runs a cascade PID controller (position PID output feeds into velocity PID controller, velocity
+ * PID controller is desired motor output) to control the turret yaw.
+ *
+ * Implements TurretControllerInterface interface, see parent class comment for details.
+ */
+class WorldFrameTurretYawCascadePIDControllerMinor final : public TurretYawControllerInterface
+{
+public:
+    /**
+     * @param[in] turretMCBCanComm A TurretMCBCanComm object that will be queried for IMU
+     * information.
+     * @param[in] yawMotor A `TurretMotor` object accessible for children objects to use.
+     * @param[in] positionPid Position PID controller.
+     * @param[in] velocityPid Velocity PID controller.
+     */
+    WorldFrameTurretYawCascadePIDControllerMinor(
+        const tap::algorithms::transforms::Transform<
+            aruwsrc::sentry::WorldFrame,
+            aruwsrc::sentry::TurretMajorFrame>& worldToBaseTransform,
+        TurretMotor& yawMotor,
+        tap::algorithms::SmoothPid& positionPid,
+        tap::algorithms::SmoothPid& velocityPid,
+        const aruwsrc::can::TurretMCBCanComm& turretMCB);
+
+    void initialize() final;
+
+    /**
+     * @see TurretControllerInterface for more details.
+     * @param[in] desiredSetpoint The yaw desired setpoint in the world frame.
+     */
+    void runController(const uint32_t dt, const float desiredSetpoint) final;
+
+    /// Sets the world frame yaw angle setpoint, refer to top level documentation for more details.
+    void setSetpoint(float desiredSetpoint) final;
+
+    /// @return World frame yaw angle setpoint, refer to top level documentation for more details.
+    float getSetpoint() const final;
+
+    /// @return World frame yaw angle measurement, refer to top level documentation for more
+    /// details.
+    float getMeasurement() const final;
+
+    bool isOnline() const final;
+
+    // @todo see todo in interface class
+    float convertControllerAngleToChassisFrame(float controllerFrameAngle) const final
+    {
+        return 0.0;
+    };
+
+    float convertChassisAngleToControllerFrame(float chassisFrameAngle) const final { return 0.0; };
+
+    // void overWrite
+
+private:
+    const tap::algorithms::transforms::Transform<
+        aruwsrc::sentry::WorldFrame,
+        aruwsrc::sentry::TurretMajorFrame>& worldToBaseTransform;
+
+    tap::algorithms::SmoothPid& positionPid;
+    tap::algorithms::SmoothPid& velocityPid;
+
+    float worldFrameSetpoint;
+
+    float positionPidOutput;
+
+    TurretMotor& yawMotor;
+
+    const aruwsrc::can::TurretMCBCanComm& turretMCB;
+};
+
+/**
+ * World frame turret pitch controller. Requires that a development board be mounted rigidly on the
+ * turret and connected via the `TurretMCBCanComm` class. The development board's IMU is used to
+ * determine the turret's world frame coordinates directly, making this controller better than the
+ * `WorldFrameChassisImuTurretController`.
+ *
+ * Runs a cascade PID controller (position PID output feeds into velocity PID controller, velocity
+ * PID controller is desired motor output) to control the turret pitch.
+ *
+ * Implements TurretControllerInterface interface, see parent class comment for details.
+ */
+class WorldFrameTurretPitchCascadePIDControllerMinor final : public TurretPitchControllerInterface
+{
+public:
+    /**
+     * @param[in] turretMCBCanComm A TurretMCBCanComm object that will be queried for IMU
+     * information.
+     * @param[in] pitchMotor A `TurretMotor` object accessible for children objects to use.
+     * @param[in] positionPid Position PID controller.
+     * @param[in] velocityPid Velocity PID controller.
+     */
+    WorldFrameTurretPitchCascadePIDControllerMinor(
+        const tap::algorithms::transforms::Transform<
+            aruwsrc::sentry::WorldFrame,
+            aruwsrc::sentry::TurretMajorFrame>& worldToBaseTransform,
+        TurretMotor& pitchMotor,
+        tap::algorithms::SmoothPid& positionPid,
+        tap::algorithms::SmoothPid& velocityPid,
+        const aruwsrc::can::TurretMCBCanComm& turretMCB);
+
+    void initialize() final;
+
+    /**
+     * @see TurretControllerInterface for more details.
+     * @param[in] desiredSetpoint The pitch desired setpoint in the world frame.
+     */
+    void runController(const uint32_t dt, const float desiredSetpoint) final;
+
+    /// Sets the world frame pitch angle setpoint, refer to top level documentation for more
+    /// details.
+    void setSetpoint(float desiredSetpoint) final;
+
+    /// @return World frame pitch angle setpoint, refer to top level documentation for more details.
+    float getSetpoint() const final;
+
+    /// @return World frame pitch angle measurement, refer to top level documentation for more
+    /// details.
+    float getMeasurement() const final;
+
+    bool isOnline() const final;
+
+    // @todo see todo in interface class
+    float convertControllerAngleToChassisFrame(float controllerFrameAngle) const final
+    {
+        return 0.0;
+    };
+
+    float convertChassisAngleToControllerFrame(float chassisFrameAngle) const final { return 0.0; };
+
+    // void overWrite
+
+private:
+    const tap::algorithms::transforms::Transform<
+        aruwsrc::sentry::WorldFrame,
+        aruwsrc::sentry::TurretMajorFrame>& worldToBaseTransform;
+
+    tap::algorithms::SmoothPid& positionPid;
+    tap::algorithms::SmoothPid& velocityPid;
+
+    float worldFrameSetpoint;
+
+    float positionPidOutput;
+
+    TurretMotor& pitchMotor;
+
+    const aruwsrc::can::TurretMCBCanComm& turretMCB;
+};
+
+}  // namespace aruwsrc::control::turret::algorithms
+
+#endif  //  MINOR_WORLD_FRAME_TURRET_CONTROLLER_HPP_
