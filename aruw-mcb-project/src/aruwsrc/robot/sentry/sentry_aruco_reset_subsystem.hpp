3db08fcbc13242e624ec3cd4c1ea60ce428871f9
spoonk <cornwlucas@gmail.com>
2024-04-22 20:33:39 -0700

    policy checks

--- aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_aruco_reset_subsystem.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_aruco_reset_subsystem.hpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2023-2024 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef SENTRY_ARUCO_RESET_SUBSYSTEM_HPP_
 #define SENTRY_ARUCO_RESET_SUBSYSTEM_HPP_
 
@@ -10,7 +29,6 @@
 
 namespace aruwsrc::sentry
 {
-
 class SentryArucoResetSubsystem : public tap::control::Subsystem
 {
 public:
@@ -21,7 +39,7 @@ public:
         aruwsrc::sentry::SentryKFOdometry2DSubsystem& odometrySubsystem,
         SentryTransforms& transforms);
 
-    void initialize() override {};
+    void initialize() override{};
 
     void refresh() override;
 
