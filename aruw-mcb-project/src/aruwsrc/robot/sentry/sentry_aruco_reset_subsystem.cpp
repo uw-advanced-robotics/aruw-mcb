f41baa716f46548b1c2f19d390d090c318bb1d53
spoonk <cornwlucas@gmail.com>
2024-04-22 20:05:56 -0700

    aruco reset subysytem:

---
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_aruco_reset_subsystem.cpp
@@ -0,0 +1,72 @@
+#include "sentry_aruco_reset_subsystem.hpp"
+
+#include "tap/algorithms/math_user_utils.hpp"
+
+// #include "aruwsrc/control/turret/algorithms/world_frame_turret_yaw_controller.hpp"
+#include "aruwsrc/control/turret/constants/turret_constants.hpp"
+#include "modm/math/geometry/quaternion.hpp"
+#include "modm/math/geometry/vector2.hpp"
+#include "modm/math/geometry/vector3.hpp"
+
+using namespace tap::algorithms::transforms;
+using namespace aruwsrc::sentry;
+
+SentryArucoResetSubsystem::SentryArucoResetSubsystem(
+    tap::Drivers& drivers,
+    aruwsrc::serial::VisionCoprocessor& vision,
+    aruwsrc::sentry::SentryChassisWorldYawObserver& yawObserver,
+    aruwsrc::sentry::SentryKFOdometry2DSubsystem& odometrySubsystem,
+    SentryTransforms& transforms,
+    aruwsrc::control::turret::algorithms::TurretMajorWorldFrameController& majorController)
+    : tap::control::Subsystem(&drivers),
+      vision(vision),
+      yawObserver(yawObserver),
+      odometrySubsystem(odometrySubsystem),
+      transforms(transforms),
+      majorController(majorController)
+{
+}
+
+void SentryArucoResetSubsystem::refresh()
+{
+    const aruwsrc::serial::VisionCoprocessor::ArucoResetData& resetData =
+        vision.getLastArucoResetData();
+
+    if (!resetData.updated) return;
+
+    modm::Quaternion<float> q(
+        resetData.data.quatW,
+        resetData.data.quatX,
+        resetData.data.quatY,
+        resetData.data.quatZ);
+
+    const Transform& majorToMinor = transforms.getWorldToTurretMajor().getInverse().compose(
+        transforms.getWorldToTurret(resetData.data.turretId));
+
+    const Transform& chassisToMajor = transforms.getWorldToChassis().getInverse().compose(
+        transforms.getWorldToTurretMajor().getInverse());
+
+    float newYaw = tap::algorithms::eulerAnglesFromQuaternion(q).z - majorToMinor.getYaw() -
+                   chassisToMajor.getYaw();
+
+    float oldYaw;
+    yawObserver.getChassisWorldYaw(&oldYaw);
+
+    float chassisX = resetData.data.x -
+                     transforms.getWorldToTurret(resetData.data.turretId).getX() +
+                     transforms.getWorldToChassis().getX();
+    float chassisY = resetData.data.y -
+                     transforms.getWorldToTurret(resetData.data.turretId).getY() +
+                     transforms.getWorldToChassis().getY();
+}
+
+void SentryArucoResetSubsystem::setOrientation(float newYaw, float oldYaw)
+{
+    odometrySubsystem.overrideOdometryOrientation(newYaw - oldYaw);
+    yawObserver.overrideChassisYaw(newYaw);
+}
+
+void SentryArucoResetSubsystem::setPosition(const float x, const float y)
+{
+    odometrySubsystem.overrideOdometryPosition(modm::Vector2f(x, y));
+}
---
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_aruco_reset_subsystem.hpp
@@ -0,0 +1,49 @@
+#ifndef SENTRY_ARUCO_RESET_SUBSYSTEM_HPP_
+#define SENTRY_ARUCO_RESET_SUBSYSTEM_HPP_
+
+#include "tap/control/subsystem.hpp"
+
+#include "aruwsrc/communication/serial/vision_coprocessor.hpp"
+#include "aruwsrc/robot/sentry/sentry_chassis_world_yaw_observer.hpp"
+#include "aruwsrc/robot/sentry/sentry_kf_odometry_2d_subsystem.hpp"
+#include "aruwsrc/robot/sentry/sentry_transforms.hpp"
+#include "aruwsrc/robot/sentry/sentry_turret_major_world_relative_yaw_controller.hpp"
+#include "modm/math/geometry/quaternion.hpp"
+#include "modm/math/geometry/vector3.hpp"
+
+namespace aruwsrc::sentry
+{
+
+class SentryArucoResetSubsystem : public tap::control::Subsystem
+{
+public:
+    SentryArucoResetSubsystem(
+        tap::Drivers& drivers,
+        aruwsrc::serial::VisionCoprocessor& vision,
+        aruwsrc::sentry::SentryChassisWorldYawObserver& yawObserver,
+        aruwsrc::sentry::SentryKFOdometry2DSubsystem& odometrySubsystem,
+        SentryTransforms& transforms,
+        aruwsrc::control::turret::algorithms::TurretMajorWorldFrameController& majorController);
+
+    void initialize() override {};
+
+    void refresh() override;
+
+    const char* getName() override { return "Sentry Aruco Reset Subsystem"; }
+
+    mockable inline bool isOnline() const { return true; }
+
+private:
+    const aruwsrc::serial::VisionCoprocessor& vision;
+    SentryChassisWorldYawObserver& yawObserver;
+    SentryKFOdometry2DSubsystem& odometrySubsystem;
+    const SentryTransforms& transforms;
+    aruwsrc::control::turret::algorithms::TurretMajorWorldFrameController& majorController;
+
+    void setOrientation(float newYaw, float oldYaw);
+    void setPosition(float x, float y);
+};
+
+}  // namespace aruwsrc::sentry
+
+#endif  // SENTRY_ARUCO_RESET_SUBSYSTEM_HPP_
