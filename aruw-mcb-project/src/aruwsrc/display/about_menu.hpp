1bc4ae40ae4047fefa12843f4da99a57c3b85873
Benjamin Li <sliben@uw.edu>
2024-05-09 23:46:59 +0000

    Standard Orion
    Co-authored-by: SumedhP <43325361+SumedhP@users.noreply.github.com>

--- aruw-mcb-project/src/aruwsrc/display/about_menu.hpp
+++ aruw-mcb-project/src/aruwsrc/display/about_menu.hpp
@@ -54,8 +54,12 @@ public:
     static constexpr char ROBOT_NAME[] = "TARGET_SENTRY_BEEHIVE";
 #elif defined(TARGET_HERO_CYCLONE)
     static constexpr char ROBOT_NAME[] = "TARGET_HERO_CYCLONE";
+#elif defined(TARGET_STANDARD_SPIDER)
+    static constexpr char ROBOT_NAME[] = "TARGET_STANDARD_SPIDER";
+#elif defined(TARGET_STANDARD_ORION)
+    static constexpr char ROBOT_NAME[] = "TARGET_STANDARD_ORION";
 #else
-    static constexpr char ROBOT_NAME[] = "UNKNOWN";
+    static constexpr char ROBOT_NAME[] = "TARGET_UNKNOWN";
 #endif
 
     static constexpr char LAST_USER[] = STRINGIFYMACRO(BUILD_USERNAME);
