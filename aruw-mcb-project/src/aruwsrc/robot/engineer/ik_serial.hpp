635098b46b49b6fb7782a41dc06b2300b21e066b
SumedhP <43325361+SumedhP@users.noreply.github.com>
2024-09-07 10:32:49 -0700

    Serial processor done lol

---
+++ aruw-mcb-project/src/aruwsrc/robot/engineer/ik_serial.hpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2024-2024 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef IK_SERIAL_HPP_
+#define IK_SERIAL_HPP_
+
+#include "tap/communication/serial/dji_serial.hpp"
+
+namespace aruwsrc::engineer
+{
+struct IKSolution
+{
+    float liftSetpoint;
+    float xAxisSetpoint;
+    float yawSetpoint;
+    float pitchSetpoint;
+    float rollSetpoint;
+    bool solutionFound;
+} modm_packed;
+
+struct SlotDelta
+{
+    float x;
+    float y;
+    float z;
+    float yaw;
+    float pitch;
+    float roll;
+} modm_packed;
+
+class IKSerial : public tap::communication::serial::DJISerial
+{
+public:
+    IKSerial(tap::Drivers *drivers, tap::communication::serial::Uart::UartPort port)
+        : DJISerial(drivers, port)
+    {
+    }
+
+    void messageReceiveCallback(const ReceivedSerialMessage &completeMessage) override
+    {
+        if (completeMessage.messageType == IK_SOLUTION_MESSAGE_ID)
+        {
+            memcpy(&ikSolution, completeMessage.data, sizeof(IKSolution));
+        }
+        else if (completeMessage.messageType == SLOT_DELTA_MESSAGE_ID)
+        {
+            memcpy(&slotDelta, completeMessage.data, sizeof(SlotDelta));
+        }
+    }
+
+    IKSolution getIKSolution() const { return ikSolution; }
+
+    SlotDelta getSlotDelta() const { return slotDelta; }
+
+    static constexpr uint8_t IK_SOLUTION_MESSAGE_ID = 0x01;
+    static constexpr uint8_t SLOT_DELTA_MESSAGE_ID = 0x02;
+
+private:
+    IKSolution ikSolution;
+    SlotDelta slotDelta;
+};
+}  // namespace aruwsrc::engineer
+#endif  // IK_SERIAL_HPP_
