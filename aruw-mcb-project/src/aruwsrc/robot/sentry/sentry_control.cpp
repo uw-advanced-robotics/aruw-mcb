df80a38d84ea81a534155d79c9a2a9693c2041e0
Clementine Allen <callen42@uw.edu>
2024-10-24 19:47:03 -0700

    ??? why was that there

--- aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_control.cpp
@@ -881,7 +881,6 @@ void setDefaultSentryCommands(Drivers *)
 void startSentryCommands(Drivers *drivers)
 {
     drivers->commandScheduler.addCommand(&imuCalibrateCommand);
-    drivers->plateHitTracker.attachTransformer(&transformAdapter);
 }
 
 /* register io mappings here ------------------------------------------------*/
