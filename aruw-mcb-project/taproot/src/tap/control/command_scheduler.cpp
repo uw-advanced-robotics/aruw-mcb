d19d5863768367b9d4e3bc9d0da18dda05bcd0a8
Eli Orona <eliorona@live.com>
2024-10-24 17:55:02 -0700

    Fix tap hardware test menu

--- aruw-mcb-project/taproot/src/tap/control/command_scheduler.cpp
+++ aruw-mcb-project/taproot/src/tap/control/command_scheduler.cpp
@@ -198,6 +198,19 @@ void CommandScheduler::run()
         // Refresh subsystems in the registeredSubsystemBitmap
         for (auto it = subMapBegin(); it != subMapEnd(); it++)
         {
+            Command *testCommand;
+            if (!safeDisconnected() &&
+                !(subsystemsAssociatedWithCommandBitmap &
+                  (LSB_ONE_HOT_SUBSYSTEM_BITMAP << (*it)->getGlobalIdentifier())) &&
+                (testCommand = (*it)->getTestCommand()) != nullptr)
+            {
+                if (testCommand->isFinished())
+                {
+                    this->subsystemsPassingHardwareTests |=
+                        (LSB_ONE_HOT_SUBSYSTEM_BITMAP << (*it)->getGlobalIdentifier());
+                }
+            }
+
             // Call appropriate refresh function for each of the subsystems
             if (safeDisconnected())
             {
@@ -355,6 +368,8 @@ void CommandScheduler::runHardwareTest(const Subsystem *subsystem)
     Command *testCommand = subsystem->getTestCommand();
     if (testCommand != nullptr)
     {
+        this->subsystemsPassingHardwareTests &=
+            ~(LSB_ONE_HOT_SUBSYSTEM_BITMAP << subsystem->getGlobalIdentifier());
         this->addCommand(testCommand);
     }
 }
@@ -387,7 +402,7 @@ int CommandScheduler::countRunningHardwareTests()
     int total = 0;
     for (auto it = subMapBegin(); it != subMapEnd(); it++)
     {
-        if (this->runningTest(*it))
+        if (this->isRunningTest(*it))
         {
             total += 1;
         }
@@ -396,9 +411,15 @@ int CommandScheduler::countRunningHardwareTests()
     return total;
 }
 
-bool CommandScheduler::runningTest(const Subsystem *subsystem)
+bool CommandScheduler::isRunningTest(const Subsystem *subsystem)
 {
     return this->isCommandScheduled(subsystem->getTestCommand());
+}
+
+bool CommandScheduler::hasPassedTest(const Subsystem *subsystem)
+{
+    return (this->subsystemsPassingHardwareTests &
+            (LSB_ONE_HOT_SUBSYSTEM_BITMAP << subsystem->getGlobalIdentifier())) != 0;
 }
 
 int CommandScheduler::subsystemListSize() const
