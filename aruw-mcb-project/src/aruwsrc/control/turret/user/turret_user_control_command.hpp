c20f0c58d65a7022b03ef96d143275ef744b9316
SumedhP <43325361+SumedhP@users.noreply.github.com>
2024-11-05 16:56:43 -0800

    clang

--- aruw-mcb-project/src/aruwsrc/control/turret/user/turret_user_control_command.hpp
+++ aruw-mcb-project/src/aruwsrc/control/turret/user/turret_user_control_command.hpp
@@ -20,8 +20,8 @@
 #ifndef TURRET_USER_CONTROL_COMMAND_HPP_
 #define TURRET_USER_CONTROL_COMMAND_HPP_
 
-#include "tap/control/command.hpp"
 #include "tap/algorithms/wrapped_float.hpp"
+#include "tap/control/command.hpp"
 
 #include "../algorithms/turret_controller_interface.hpp"
 #include "../turret_subsystem.hpp"
