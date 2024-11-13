503e196169a437011cf9d530abcadc780937a643
SumedhP <43325361+SumedhP@users.noreply.github.com>
2024-11-12 18:42:11 -0800

    I hate coding

---
+++ aruw-mcb-project/src/aruwsrc/control/client-display/wall_hack.cpp
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2024-2024 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "wall_hack.hpp"
+
+using namespace tap::communication::serial;
+
+namespace aruwsrc::control::client_display
+{
+WallHack::WallHack(
+    aruwsrc::serial::VisionCoprocessor &visionCoprocessor,
+    RefSerialTransmitter &refSerialTransmitter,
+    TransformerInterface *transformer)
+    : HudIndicator(refSerialTransmitter),
+      visionCoprocessor(visionCoprocessor),
+      transformer(transformer)
+{
+}
+
+modm::ResumableResult<bool> WallHack::update()
+{
+    auto robotPos = transformer->getWorldToTurret(0);
+    robotPositionVector.x = robotPos.getX();
+    robotPositionVector.y = robotPos.getY();
+    robotPositionVector.z = robotPos.getZ();
+
+    auto aimData = visionCoprocessor.getLastAimData(0);
+
+    Position enemyPosition = Position(aimData.pva.xPos, aimData.pva.yPos, aimData.pva.zPos);
+    enemyPosition = transformer->getWorldToTurret(0).apply(enemyPosition);
+
+    // Swap axes
+    enemyPosition = swapAxesMatrix * enemyPosition.coordinates();
+    enemyPositionVector.x = enemyPosition.x();
+    enemyPositionVector.y = enemyPosition.y();
+    enemyPositionVector.z = enemyPosition.z();
+
+    enemyPositionTransformed = convertVectorByProjectionMatrix(enemyPositionVector);
+
+    computedScreenX = std::min(
+        (uint32_t)(SCREEN_WIDTH - 1),
+        (uint32_t)((enemyPositionTransformed.x + 1) * 0.5 * SCREEN_WIDTH));
+    computedScreenY = std::min(
+        (uint32_t)(SCREEN_HEIGHT - 1),
+        (uint32_t)((1 - enemyPositionTransformed.y) * 0.5 * SCREEN_HEIGHT));
+
+    RF_BEGIN(0);
+
+    if (abs(enemyPositionTransformed.x) > 1 || abs(enemyPositionTransformed.y) > 1)
+    {
+        RF_RETURN(false);
+    }
+
+    visionTargetGraphic.graphicData.operation =
+        visionTargetGraphic.graphicData.operation == Tx::GRAPHIC_DELETE ? Tx::GRAPHIC_MODIFY
+                                                                        : Tx::GRAPHIC_ADD;
+
+    RefSerialTransmitter::configRectangle(
+        WALL_HACK_THICKNESS,
+        computedScreenX,
+        computedScreenY,
+        computedScreenX + SQUARE_SIZE,
+        computedScreenY + SQUARE_SIZE,
+        &visionTargetGraphic.graphicData);
+
+    RF_CALL(refSerialTransmitter.sendGraphic(&visionTargetGraphic));
+
+    RF_END();
+}
+
+modm::ResumableResult<bool> WallHack::sendInitialGraphics()
+{
+    RF_BEGIN(1);
+    RF_END();
+}
+
+void WallHack::initialize()
+{
+    uint8_t indicatorName[3];
+
+    getUnusedGraphicName(indicatorName);
+    RefSerialTransmitter::configGraphicGenerics(
+        &visionTargetGraphic.graphicData,
+        indicatorName,
+        Tx::GRAPHIC_DELETE,
+        DEFAULT_GRAPHIC_LAYER,
+        Tx::GraphicColor::PURPLISH_RED);
+}
+
+void WallHack::setProjectionMatrix()
+{
+    float horizontalScale = 1 / tanf(horizontalFOV * 0.5 * M_PI / 180);
+    float verticalScale = 1 / tanf(verticalFOV * 0.5 * M_PI / 180);
+
+    projectionMatrix.data[0] = horizontalScale;
+    projectionMatrix.data[4 * 1 + 1] = verticalScale;
+
+    projectionMatrix.data[4 * 2 + 2] = -far / (far - near);
+    projectionMatrix.data[4 * 2 + 3] = -1;
+
+    projectionMatrix.data[4 * 3 + 2] = -far * near / (far - near);
+    projectionMatrix.data[4 * 3 + 3] = 0;
+}
+
+modm::Vector3f WallHack::convertVectorByProjectionMatrix(modm::Vector3f &vector)
+{
+    CMSISMat<4, 1> vec;
+    vec.data[0] = vector.x;
+    vec.data[1] = vector.y;
+    vec.data[2] = vector.z;
+    vec.data[3] = 1;
+
+    CMSISMat<4, 1> result = projectionMatrix * vec;
+
+    modm::Vector3f resultVec;
+    resultVec.x = result.data[0] / result.data[3];
+    resultVec.y = result.data[1] / result.data[3];
+    resultVec.z = result.data[2] / result.data[3];
+
+    return resultVec;
+}
+
+}  // namespace aruwsrc::control::client_display
---
+++ aruw-mcb-project/src/aruwsrc/control/client-display/wall_hack.hpp
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2024-2024 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef WALL_HACK_HPP_
+#define WALL_HACK_HPP_
+
+#include "tap/algorithms/cmsis_mat.hpp"
+#include "tap/architecture/timeout.hpp"
+#include "tap/communication/referee/state_hud_indicator.hpp"
+#include "tap/communication/serial/ref_serial.hpp"
+
+#include "aruwsrc/algorithms/odometry/transformer_interface.hpp"
+#include "aruwsrc/communication/serial/vision_coprocessor.hpp"
+#include "modm/processing/resumable.hpp"
+
+#include "hud_indicator.hpp"
+
+using namespace aruwsrc::algorithms::transforms;
+using tap::algorithms::CMSISMat;
+
+
+namespace aruwsrc::control::client_display
+{
+
+/**
+ * Draws a square where the enemy robot is.
+ */
+class WallHack : public HudIndicator, protected modm::Resumable<2>
+{
+public:
+    /**
+     * Construct a WallHack object.
+     *
+     * @param[in] visionCoprocessor VisionCoprocessor instance.
+     * @param[in] refSerialTransmitter RefSerialTransmitter instance.
+     * @param[in] transformer TransformerInterface instance.
+     */
+    WallHack(
+        aruwsrc::serial::VisionCoprocessor &visionCoprocessor,
+        tap::communication::serial::RefSerialTransmitter &refSerialTransmitter,
+        TransformerInterface *transformer);
+
+    void initialize() override final;
+
+    modm::ResumableResult<bool> sendInitialGraphics() override final;
+
+    modm::ResumableResult<bool> update() override final;
+
+private:
+    aruwsrc::serial::VisionCoprocessor &visionCoprocessor;
+    TransformerInterface *transformer;
+
+    modm::Vector3f enemyPositionVector, robotPositionVector, enemyPositionTransformed;
+    CMSISMat<3,1> enemyPosition, robotPosition;
+
+    Tx::Graphic1Message visionTargetGraphic;
+
+    static constexpr uint16_t WALL_HACK_THICKNESS = 3;
+    static constexpr Tx::GraphicColor COLOR = Tx::GraphicColor::YELLOW;
+
+    CMSISMat<4,4> projectionMatrix;
+
+    int SQUARE_SIZE = 30;
+
+    float horizontalFOV = 135;
+    float verticalFOV = 90;
+
+    float near = 0.1, far = 100;
+
+    void setProjectionMatrix();
+
+    modm::Vector3f convertVectorByProjectionMatrix(modm::Vector3f &vector);
+
+    /**
+     * Swaps:
+     * -y -> x
+     *  z -> y
+     * -x -> z
+     */
+    CMSISMat<3,3> swapAxesMatrix = {{
+         0, -1, 0,
+         0,  0, 1,
+        -1,  0, 0
+    }};
+
+    uint32_t computedScreenX, computedScreenY;
+
+};
+
+}  // namespace aruwsrc::control::client_display
+
+#endif  // WALL_HACK_HPP_
