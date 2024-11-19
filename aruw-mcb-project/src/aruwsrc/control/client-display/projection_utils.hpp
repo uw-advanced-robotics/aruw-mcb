fc3dbe65f687f7f95094f6e5068c7ef1f5e26e65
SumedhP <43325361+SumedhP@users.noreply.github.com>
2024-11-18 17:07:19 -0800

    yuh

--- aruw-mcb-project/src/aruwsrc/control/client-display/projection_utils.hpp
+++ aruw-mcb-project/src/aruwsrc/control/client-display/projection_utils.hpp
@@ -56,6 +56,7 @@ struct ProjectedResult
 // This in theory is the offset between the realsense and the VTM
 #if defined(TARGET_STANDARD_CYGNUS)
 static Position VTM_OFFSET_FRAME = Position(0, -0.02, -0.085);
+#else if defined(TARGET_STANDARD_ORION)
 #else
 static Position VTM_OFFSET_FRAME = Position(0, 0, 0);
 #endif
