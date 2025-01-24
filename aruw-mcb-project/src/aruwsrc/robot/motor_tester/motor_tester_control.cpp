b1b7619e852a40cff1bb1e01651ac842f2fabb55
pie474 <chinmaymurthy474@gmail.com>
2025-01-23 21:46:29 -0800

    max value up to 60000

--- aruw-mcb-project/src/aruwsrc/robot/motor_tester/motor_tester_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/motor_tester/motor_tester_control.cpp
@@ -123,7 +123,7 @@ StickRpmCommand leftManual(
     &leftMotorSubsystem,
     &drivers()->remote,
     tap::communication::serial::Remote::Channel::LEFT_VERTICAL,
-    4096.0f);
+    60000.0f);
 
 // StickRpmCommand rightManual(
 //     &rightMotorSubsystem,
