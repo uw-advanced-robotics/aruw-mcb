b1abe089659dd2068d2c35ad568cd994af2b19ed
fallgold <acaciah@uw.edu>
2025-01-25 12:30:20 -0800

    deleting more stuff until it works

--- aruw-mcb-project/test/aruwsrc/mock/engineer/mock_constructors.cpp
+++ aruw-mcb-project/test/aruwsrc/mock/engineer/mock_constructors.cpp
@@ -25,33 +25,4 @@
 // so in a source file allows for faster compilation than defining constructors
 // in the headers
 namespace aruwsrc::mock
-{
-GrabberSubsystemMock::GrabberSubsystemMock(tap::Drivers *drivers, tap::gpio::Digital::OutputPin pin)
-    : engineer::GrabberSubsystem(drivers, pin)
-{
-}
-GrabberSubsystemMock::~GrabberSubsystemMock() {}
-
-TowSubsystemMock::TowSubsystemMock(
-    tap::Drivers *drivers,
-    tap::gpio::Digital::OutputPin leftTowPin,
-    tap::gpio::Digital::OutputPin rightTowPin,
-    tap::gpio::Digital::InputPin leftTowLimitSwitchPin,
-    tap::gpio::Digital::InputPin rightTowLimitSwitchPin)
-    : aruwsrc::engineer::TowSubsystem(
-          drivers,
-          leftTowPin,
-          rightTowPin,
-          leftTowLimitSwitchPin,
-          rightTowLimitSwitchPin)
-{
-}
-TowSubsystemMock::~TowSubsystemMock() {}
-
-XAxisSubsystemMock::XAxisSubsystemMock(tap::Drivers *drivers, tap::gpio::Digital::OutputPin pin)
-    : engineer::XAxisSubsystem(drivers, pin)
-{
-}
-XAxisSubsystemMock::~XAxisSubsystemMock() {}
-
-}  // namespace aruwsrc::mock
+{}  // namespace aruwsrc::mock
