7148de70cd407da3f30c047c5d46c7a829de227e
swaras <swaras@uw.edu>
2025-02-01 12:16:09 -0800

    level 1??

--- aruw-mcb-project/src/aruwsrc/robot/engineer/engineer_lift_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/engineer/engineer_lift_constants.hpp
@@ -7,7 +7,7 @@ namespace aruwsrc::robot::engineer
 static constexpr tap::motor::MotorId CUBE_LIFT_MOTOR_ID = tap::motor::MOTOR1;  //TODO: UPDATE W CORRECT VALUE
  
 
-static constexpr tap::can::CanBus LAUNCHER_CAN_BUS = tap::can::CanBus::CAN_BUS2;
+static constexpr tap::can::CanBus LIFT_MOTOR_CAN_BUS = tap::can::CanBus::CAN_BUS2; // update
 
 static constexpr tap::gpio::Digital::InputPin LIMITSWITCH_PORT = tap::gpio::Digital::InputPin::D; //TODO: UPDATE W CORRECT VALUE
 
