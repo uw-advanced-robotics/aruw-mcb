7765ef56d9888d7a8b5c17c79b54d122ba06a2a2
swaras <swaras@uw.edu>
2025-02-01 12:34:15 -0800

    why isnt it committing

--- aruw-mcb-project/src/aruwsrc/robot/engineer/engineer_lift_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/engineer/engineer_lift_constants.hpp
@@ -7,7 +7,7 @@ namespace aruwsrc::robot::engineer
 static constexpr tap::motor::MotorId CUBE_LIFT_MOTOR_ID = tap::motor::MOTOR1;  //TODO: UPDATE W CORRECT VALUE
  
 
-static constexpr tap::can::CanBus LIFT_MOTOR_CAN_BUS = tap::can::CanBus::CAN_BUS2; // update
+static constexpr tap::can::CanBus LIFT_MOTOR_CAN_BUS = tap::can::CanBus::CAN_BUS2; // TODO: UPDATE W CORRECT VALUE
 
 static constexpr tap::gpio::Digital::InputPin LIMITSWITCH_PORT = tap::gpio::Digital::InputPin::D; //TODO: UPDATE W CORRECT VALUE
 
