85db6feb58c5582205813acd131d610347d3a59b
AidenP <aprevey@uw.edu>
2025-02-26 15:24:30 -0800

    Eli can const expr everything

--- aruw-mcb-project/taproot/src/tap/algorithms/butterworth.cpp
+++ aruw-mcb-project/taproot/src/tap/algorithms/butterworth.cpp
@@ -1,120 +1,170 @@
-#include <complex>
-#include <vector>
-#include <cmath>
-#include <numeric>
-#include "modm/math/geometry/angle.hpp"
-#include "butterworth.hpp"
 
-namespace tap
-{
-namespace algorithms
-{
-    Butterworth::Butterworth(float wc, int n, float Ts) : 
-    naturalResponseCoefficients(n, 0.0f),
-    forcedResponseCoefficients(n, 0.0f),
-    naturalResponse(n, 0.0f),
-    naturalResponseResult(n, 0.0f),
-    forcedResponse(n, 0.0f),
-    forcedResponseResult(n, 0.0f),
-    sum(n, 0.0f)
-    {
-        //Warp frequency for bilinear transform
-        wc = (2/Ts)*std::atan(wc*(Ts/2));
+/*
+ * Copyright (c) 2024-2024 Advanced Robotics at the University of Washington
+ * <robomstr@uw.edu>
+ *
+ * This file is part of Taproot.
+ *
+ * Taproot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Taproot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Taproot.  If not, see <https://www.gnu.org/licenses/>;.
+ */
 
-        //generate poles for butterworth filter
-        std::vector<std::complex<float>> poles;
-        for (int k = 0; k < n; ++k) {
-            float theta = M_PI * (2.0 * k + 1) / (2.0 * n) + M_PI/2;
-            std::complex<float> pole = std::polar(wc, theta);
-            poles.push_back(pole);
-        }
+ #include <array>
+ #include <cmath>
+ #include <complex>
+ #include <cstdint>
+ #include <iostream>
+ #include <iomanip>
+ #include "modm/math/geometry/angle.hpp" // for M_PI 
 
-        //Preform the z transform for each pole
-        std::vector<std::complex<float>> zPoles(n);
-        for (int i = 0; i < n; ++i) {
-            zPoles[i] = s2z(poles[i], Ts);
-        }
-
-        // zeros calculation, for a butterworth all zeros in the Z domain are -1
-        std::complex<float> zZero(-1,0);
-        std::vector<std::complex<float>> zZeros(n, zZero);
-
-        // Calculate polynomial coefficients
-        auto forcedResponseCoefficients = expandPolynomial(zZeros);
-        auto naturalResponseCoefficients = expandPolynomial(zPoles);
-
-        // Calculate and apply the scalar so the DC gain at 1hz is 1
-        auto scale_factor = calculateScalar(forcedResponseCoefficients, naturalResponseCoefficients);
-
-        std::transform(forcedResponseCoefficients.begin(), forcedResponseCoefficients.end(), 
-        forcedResponseCoefficients.begin(), [&scale_factor](auto& c){return c*scale_factor;});
-
-
-
-    }
-    
-    float Butterworth::filterData(float dat){
-        
-        addData(forcedResponse, dat);
-        std::transform(forcedResponse.begin()+1, forcedResponse.end(),
-                forcedResponseCoefficients.begin()+1, forcedResponseResult.begin(),
-                std::multiplies<float>() );
-
-        std::transform( naturalResponse.begin()+1, naturalResponse.end(),
-                naturalResponseCoefficients.begin()+1, naturalResponseResult.begin(),
-                std::multiplies<float>() );
-
-        std::transform(naturalResponseResult.begin(), naturalResponseResult.end(), forcedResponseResult.begin(),
-               sum.begin(), std::plus<double>());
-    
-        addData(naturalResponse, std::reduce(sum.begin(),sum.end()));
-
-        return naturalResponse[0];
-    }
-
-    float Butterworth::getLastFiltered(){
-        return naturalResponse[0];
-    }
-
-    void Butterworth::addData(std::vector<float> &Response, float newData){
-        Response.insert(Response.begin(), newData);
-        Response.pop_back();
-        return;
-    }
-
-    std::complex<float> s2z(std::complex<float> s, float Ts){
-        return (1.0f + (Ts / 2) * s) / (1.0f - (Ts / 2) * s);
-    }
-
-    std::vector<float> Butterworth::expandPolynomial(std::vector<std::complex<float>> zeros){
-
-        std::vector<std::complex<float>> coefficients(1, std::complex<float>(1.0, 0.0));  // Start with 1
-        for (const auto& zero : zeros) {
-            // Multiply current polynomial by (x - zero)
-            std::vector<std::complex<float>> newCoefficients(coefficients.size() + 1, std::complex<float>(0.0, 0.0));
-            for (size_t i = 0; i < coefficients.size(); ++i) {
-                newCoefficients[i] -= coefficients[i] * zero;      // Multiply current coefficient by zero
-                newCoefficients[i + 1] += coefficients[i];         // Shift current coefficient
-            }
-            coefficients = newCoefficients;  // Update coefficients
-        }
-        std::vector<float> stripped_coefficients(coefficients.size(), 0.0);
-        // Extract the real part of each coefficient, the imaginary appears to be an artifact of float
-        for (size_t i = 0; i < coefficients.size(); ++i) {
-            stripped_coefficients[i] = coefficients[i].real();
-        }
-
-        return stripped_coefficients;
-    }
-
-    float Butterworth::calculateScalar(std::vector<float> numeratior, std::vector<float> denominator){
-        auto denResult = std::reduce(denominator.begin(), denominator.end());
-        auto numResult = std::reduce(numeratior.begin(), numeratior.end());
-        //den is over num in the result as it is in the form 1/(num/den) to apply a scalar for the DC gain
-        return denResult/numResult;
-    }
-
-
-}// namespace algorithms
-
-}// namespace tap
+ namespace tap {
+ namespace algorithms {
+ 
+ /**
+  * used to transform poles from the laplauce domain to the
+  * Z domain for descrete time using the bilinear transform
+  *
+  * @param [in] s a pole or zero from the laplauce domain
+  * @param [in] Ts the sample time
+  * @return a complex number corresponding to the Z domain location
+  */
+ constexpr std::complex<double> s2z(std::complex<double> s, double Ts) {
+   return (1.0 + (Ts / 2) * s) / (1.0 - (Ts / 2) * s);
+ }
+ 
+ /**
+  * used to multiply out a series of zeros to obtain a list of coefficents
+  *
+  * @param [in] zeros a vector of complex poles or zeros to multiply out, works
+  * for any polynomial
+  */
+ template <uint8_t ORDER>
+ constexpr std::array<double, ORDER + 1>
+ expandPolynomial(std::array<std::complex<double>, ORDER> zeros) {
+ 
+   std::array<std::complex<double>, ORDER + 1> coefficients{
+       std::complex<double>(1.0, 0.0)}; // Start with 1
+ 
+   for (size_t i = 0; i < ORDER; i++) {
+     // Multiply current polynomial by (x - zero)
+     std::array<std::complex<double>, ORDER + 1> newCoefficients{
+         std::complex<double>(0.0, 0.0)};
+     for (size_t j = 0; j < i + 1; j++) {
+       newCoefficients[j] -=
+           coefficients[j] * zeros[i]; // Multiply current coefficient by zero
+       newCoefficients[j + 1] += coefficients[j]; // Shift current coefficient
+     }
+     coefficients.swap(newCoefficients);
+   }
+   std::array<double, ORDER + 1> stripped_coefficients{};
+   // Extract the real part of each coefficient, the imaginary appears to be an
+   // artifact of double
+   for (size_t i = 0; i < ORDER + 1; i++) {
+     stripped_coefficients[i] = coefficients[i].real();
+   }
+ 
+   return stripped_coefficients;
+ }
+ 
+ template <uint8_t ORDER>
+ constexpr double calculateScalar(std::array<double, ORDER + 1> numerator,
+                                 std::array<double, ORDER + 1> denominator) {
+ 
+ 
+   double denResult = 0;
+   double numResult = 0;
+ 
+   for (size_t i = 0; i < ORDER + 1; i++) {
+     denResult += denominator[i];
+     numResult += numerator[i];
+   }
+ 
+   // den is over num in the result as it is in the form 1/(num/den) to apply a
+   // scalar for the gain
+   return static_cast<double>((denResult / numResult));
+ }
+ 
+ template <uint8_t ORDER>
+ class Butterworth {
+ 
+ public:
+   /**
+    * Initializes a butterworth filter with a given order, cuttoff frequency, and
+    * sample time
+    *
+    * @note make sure to understand what the a given order affects before
+    * implementing, mainly the potential phase delays
+    *
+    * @param[in] wc the cutoff frequency, roughly the frequency where attenuation
+    * begins.
+    * @param[in] n the order of the Butterworth, higher orders mean a sharper
+    * droppoff in frequency.
+    * @param[in] Ts the sample time in seconds, for example at 500hz the value
+    * should be 0.02
+    */
+   constexpr Butterworth(double wc, double Ts)
+       : naturalResponseCoefficients(), forcedResponseCoefficients() {
+     int n = ORDER;
+     // Warp frequency for bilinear transform
+     wc = (2 / Ts) * std::atan(wc * (Ts / 2));
+ 
+     // generate poles for butterworth filter
+     std::array<std::complex<double>, ORDER> poles;
+     for (int k = 0; k < static_cast<int>(n); ++k) {
+       double theta = M_PI * (2.0 * k + 1) / (2.0 * n) + M_PI / 2;
+       std::complex<double> pole =
+           std::complex<double>(wc * cos(theta), wc * sin(theta));
+       poles[k] = pole;
+     }
+ 
+     // Preform the z transform for each pole
+     std::array<std::complex<double>, ORDER> zPoles;
+     for (int i = 0; i < ORDER; ++i) {
+       zPoles[i] = s2z(poles[i], Ts);
+     }
+ 
+     // zeros calculation, for a butterworth all zeros in the Z domain are -1
+     std::complex<double> zZero(-1, 0);
+     std::array<std::complex<double>, ORDER> zZeros;
+     zZeros.fill(zZero);
+ 
+     // Calculate polynomial coefficients
+     forcedResponseCoefficients = expandPolynomial<ORDER>(zZeros);
+     naturalResponseCoefficients = expandPolynomial<ORDER>(zPoles);
+ 
+     // Calculate and apply the scalar so the DC gain is 1
+     double scale_factor = calculateScalar<ORDER>(forcedResponseCoefficients,
+                                                 naturalResponseCoefficients);
+ 
+     for (size_t i = 0; i < ORDER + 1; i++) {
+       forcedResponseCoefficients[i] *= scale_factor;
+     }
+   }
+ 
+   std::array<double, ORDER + 1> getNaturalResponseCoefficients() const {
+     return naturalResponseCoefficients;
+   }
+   std::array<double, ORDER + 1> getForcedResponseCoefficients() const {
+     return forcedResponseCoefficients;
+   }
+ 
+ private:
+   std::array<double, ORDER + 1> naturalResponseCoefficients;
+ 
+   std::array<double, ORDER + 1> forcedResponseCoefficients;
+ };
+ 
+ } // namespace algorithms
+ 
+ } // namespace tap
+ 
