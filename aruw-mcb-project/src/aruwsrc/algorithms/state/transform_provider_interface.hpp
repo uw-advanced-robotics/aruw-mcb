7149eb64b708ff5adee09ac57c6844962c695645
pie474 <chinmaymurthy474@gmail.com>
2025-03-01 14:32:35 -0800

    define getTransform in transform provider

--- aruw-mcb-project/src/aruwsrc/algorithms/state/transform_provider_interface.hpp
+++ aruw-mcb-project/src/aruwsrc/algorithms/state/transform_provider_interface.hpp
@@ -35,7 +35,7 @@ class TransformProviderInterface : public TranslationProviderInterface<BASE, FOL
                                    public OrientationProviderInterface<BASE, FOLLOWER>
 {
 public:
-    virtual Transform getTransform();
+    inline Transform getTransform() { return Transform(getTranslation(), getOrientation()) }
 };
 
 }  // namespace aruwsrc::algorithms::state
