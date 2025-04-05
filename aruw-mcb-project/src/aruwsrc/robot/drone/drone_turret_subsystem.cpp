d191f991185fcc4306c80c360fc71e211e3259e7
ananyaa06 <ananyaa06@gmail.com>
2025-04-05 11:49:11 -0700

    More formatting.

--- aruw-mcb-project/src/aruwsrc/robot/drone/drone_turret_subsystem.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/drone/drone_turret_subsystem.cpp
@@ -22,7 +22,6 @@
 #include "aruwsrc/communication/can/turret_mcb_can_comm.hpp"
 namespace aruwsrc::control::turret
 {
-
 float DroneTurretSubsystem::getWorldYaw() const { return getTurretMCB()->getYaw(); }
 
 float DroneTurretSubsystem::getWorldPitch() const { return getTurretMCB()->getPitch(); }
