a90e441aec8a88ff697fbcdb980d6ad29ab3f7ce
pie474 <chinmaymurthy474@gmail.com>
2025-04-12 12:48:42 -0700

    rebuild tap

--- aruw-mcb-project/taproot/test/tap/mock/abstract_imu_mock.cpp
+++ aruw-mcb-project/taproot/test/tap/mock/abstract_imu_mock.cpp
@@ -3,7 +3,7 @@
 /*****************************************************************************/
 
 /*
- * Copyright (c) 2022 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ * Copyright (c) 2025 Advanced Robotics at the University of Washington <robomstr@uw.edu>
  *
  * This file is part of Taproot.
  *
@@ -21,27 +21,10 @@
  * along with Taproot.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef TAPROOT_IMU_TERMINAL_SERIAL_HANDLER_MOCK_HPP_
-#define TAPROOT_IMU_TERMINAL_SERIAL_HANDLER_MOCK_HPP_
-
-#include <gmock/gmock.h>
-
-#include "tap/communication/sensors/imu/imu_terminal_serial_handler.hpp"
+#include "abstract_imu_mock.hpp"
 
 namespace tap::mock
 {
-class ImuTerminalSerialHandlerMock : public communication::sensors::imu::ImuTerminalSerialHandler
-{
-public:
-    ImuTerminalSerialHandlerMock(
-        tap::Drivers* drivers,
-        communication::sensors::imu::AbstractIMU* imu);
-    virtual ~ImuTerminalSerialHandlerMock();
-
-    MOCK_METHOD(void, init, (), (override));
-    MOCK_METHOD(bool, terminalSerialCallback, (char*, modm::IOStream&, bool), (override));
-    MOCK_METHOD(void, terminalSerialStreamCallback, (modm::IOStream&), (override));
-};
+AbstractImuMock::AbstractImuMock() {}
+AbstractImuMock::~AbstractImuMock() {}
 }  // namespace tap::mock
-
-#endif  // TAPROOT_IMU_TERMINAL_SERIAL_HANDLER_MOCK_HPP_
