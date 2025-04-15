418e135612ec93cc8599a3ab37d0bd5378991cda
pie474 <chinmaymurthy474@gmail.com>
2025-04-14 21:55:22 -0700

    did?

--- aruw-mcb-project/src/aruwsrc/robot/engineer/engineer_chassis_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/engineer/engineer_chassis_constants.hpp
@@ -20,6 +20,7 @@
 #ifndef ENGINEER_CHASSIS_CONSTANTS_HPP_
 #define ENGINEER_CHASSIS_CONSTANTS_HPP_
 
+#include "tap/algorithms/smooth_pid.hpp"
 #include "tap/communication/gpio/analog.hpp"
 
 #include "modm/math/filter/pid.hpp"
@@ -74,6 +75,14 @@ static constexpr float VELOCITY_PID_KS = 0.0;
  * -20 ~ 0 ~ 20 A.
  */
 static constexpr float VELOCITY_PID_MAX_OUTPUT = 16'000.0f;
+
+static constexpr tap::algorithms::SmoothPidConfig WHEEL_VELOCITY_PID_CONFIG = {
+    .kp = VELOCITY_PID_KP,
+    .ki = VELOCITY_PID_KI,
+    .kd = VELOCITY_PID_KD,
+    .maxICumulative = VELOCITY_PID_MAX_ERROR_SUM,
+    .maxOutput = VELOCITY_PID_MAX_OUTPUT,
+};
 
 /**
  * Rotation PID: A PD controller for chassis autorotation. The PID parameters for the
