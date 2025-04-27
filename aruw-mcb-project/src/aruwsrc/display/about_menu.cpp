6691bc9c7a8a9c11e564f9207f7e9a00bdd582ec
pie474 <chinmaymurthy474@gmail.com>
2025-04-27 16:59:32 -0700

    about menu define fix

--- aruw-mcb-project/src/aruwsrc/display/about_menu.cpp
+++ aruw-mcb-project/src/aruwsrc/display/about_menu.cpp
@@ -37,6 +37,8 @@ static constexpr char ROBOT_NAME[] = "TARGET_HERO_PERSEUS";
 #elif defined(TARGET_STANDARD_NULL)
 static constexpr char ROBOT_NAME[] = "TARGET_STANDARD_NULL";
 #elif defined(TARGET_STANDARD_ORION)
+static constexpr char ROBOT_NAME[] = "TARGET_STANDARD_ORION";
+#else
 static constexpr char ROBOT_NAME[] = "TARGET_UNKNOWN";
 #endif
 
