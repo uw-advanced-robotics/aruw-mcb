4dcb6162ec52d60c0772edca96e6365e5b486de5
pie474 <chinmaymurthy474@gmail.com>
2025-04-30 13:52:31 -0700

    format

--- aruw-mcb-project/src/aruwsrc/util_macros.hpp
+++ aruw-mcb-project/src/aruwsrc/util_macros.hpp
@@ -44,8 +44,7 @@
 #define ALL_SENTRIES
 #endif
 
-#if defined(TARGET_STANDARD_ORION) || \
-    defined(TARGET_HERO_PERSEUS) || defined(TARGET_SENTRY_HYDRA)
+#if defined(TARGET_STANDARD_ORION) || defined(TARGET_HERO_PERSEUS) || defined(TARGET_SENTRY_HYDRA)
 #define SSH1106_OLED
 #endif
 
