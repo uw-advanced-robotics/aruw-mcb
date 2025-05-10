0674c5deb2dbc29b4f56907cc03747fbc5b50342
pie474 <chinmaymurthy474@gmail.com>
2025-05-10 14:15:03 -0700

    mario pipe and megalovania sequences

--- aruw-mcb-project/src/aruwsrc/control/buzzer/buzzer_sequences.hpp
+++ aruw-mcb-project/src/aruwsrc/control/buzzer/buzzer_sequences.hpp
@@ -31,8 +31,22 @@ static constexpr std::array<uint8_t, 27> MARIO_MUSHROOM_NOTES{{37, 32, 37, 41, 4
                                                                40, 45, 40, 45, 49, 52, 57, 52, 35,
                                                                39, 42, 47, 42, 47, 51, 54, 59, 54}};
 
+static constexpr uint32_t MARIO_1UP_NOTE_LENGTH_MS = 130;
+static constexpr std::array<uint8_t, 6> MARIO_1UP_NOTES{{41, 44, 53, 49, 51, 56}};
+
+// https://onlinesequencer.net/3348282#t16
+static constexpr uint32_t MARIO_PIPE_NOTE_LENGTH_MS = 20;
+static constexpr std::array<uint8_t, 48> MARIO_PIPE_NOTES{
+    {53, 46, 39, 53, 46, 39, 32, 25, 18, 11, 0, 0, 0, 0, 0, 0,
+     53, 46, 39, 53, 46, 39, 32, 25, 18, 11, 0, 0, 0, 0, 0, 0,
+     53, 46, 39, 53, 46, 39, 32, 25, 18, 11, 0, 0, 0, 0, 0, 0}};
+
 static constexpr uint32_t ZELDA_SECRET_NOTE_LENGTH_MS = 130;
 static constexpr std::array<uint8_t, 8> ZELDA_SECRET_NOTES{{44, 43, 40, 34, 33, 41, 45, 49}};
+
+static constexpr uint32_t MEGALOVANIA_NOTE_LENGTH_MS = 125;
+static constexpr std::array<uint8_t, 16> MEGALOVANIA_NOTES{
+    {27, 27, 39, 0, 34, 0, 0, 33, 0, 31, 0, 29, 29, 26, 29, 31}};
 
 static constexpr uint32_t IMU_CALIBRATE_SOUND_NOTE_LENGTH_MS = MARIO_MUSHROOM_NOTE_LENGTH_MS;
 static constexpr auto& IMU_CALIBRATE_SOUND_NOTES = MARIO_MUSHROOM_NOTES;
