106cf6cbdd615fc9c0d42edfcb1f6bd435e9b4e6
pie474 <chinmaymurthy474@gmail.com>
2025-05-13 14:35:27 -0700

    header

--- aruw-mcb-project/src/aruwsrc/control/turret/turret_mcb_world_orientation_observer.hpp
+++ aruw-mcb-project/src/aruwsrc/control/turret/turret_mcb_world_orientation_observer.hpp
@@ -1,5 +1,26 @@
+/*
+ * Copyright (c) 2025-2025 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef TURRET_MCB_WORLD_ORIENTATION_OBSERVER_HPP_
 #define TURRET_MCB_WORLD_ORIENTATION_OBSERVER_HPP_
+
+#include "aruwsrc/communication/can/turret_mcb_can_comm.hpp"
 
 #include "imu_world_orientation_observer.hpp"
 
