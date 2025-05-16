aa4a50330ce0a5bfa259070643649b18fea63784
pie474 <chinmaymurthy474@gmail.com>
2025-05-15 23:34:25 -0700

    gravity compensation kinda

--- aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_turret_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_turret_constants.hpp
@@ -44,9 +44,9 @@ static constexpr float MAJOR_USER_YAW_INPUT_SCALAR = 0.007f;
 static constexpr float MINOR_USER_YAW_INPUT_SCALAR = 0.008f;
 static constexpr float MINOR_USER_PITCH_INPUT_SCALAR = 0.008f;
 
-static constexpr float TURRET_CG_X = 32.5f;
-static constexpr float TURRET_CG_Z = 32.3f;
-static constexpr float GRAVITY_COMPENSATION_SCALAR = 0.0f;
+static constexpr float TURRET_CG_X = 14.72264593f;
+static constexpr float TURRET_CG_Z = 34.51638903f - 40.0f;
+static constexpr float GRAVITY_COMPENSATION_SCALAR = -5000.0f;
 // -13'000.0f;  // Right turret is -14'000 for some reason
 
 static constexpr float TURRET_MINOR_OFFSET = 0.132f;
