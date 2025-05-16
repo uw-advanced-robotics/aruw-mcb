e20fec492a36617b2856dd91023c116ff27df6b6
Tong Lin <tlin01@uw.edu>
2025-05-15 21:33:35 -0700

    good baud rate I think

--- aruw-mcb-project/src/aruwsrc/communication/serial/engineer_cv_communication.hpp
+++ aruw-mcb-project/src/aruwsrc/communication/serial/engineer_cv_communication.hpp
@@ -33,7 +33,7 @@ class EngineerCVCommunication : public tap::communication::serial::DJISerial
 public:
     static constexpr tap::communication::serial::Uart::UartPort ENGINEER_CV_RX_UART_PORT =
         tap::communication::serial::Uart::UartPort::Uart8;
-    static constexpr size_t ENGINEER_CV_UART_BAUD_RATE = 115'200;
+    static constexpr size_t ENGINEER_CV_UART_BAUD_RATE = 57'600;
 
     EngineerCVCommunication(tap::Drivers* drivers);
     DISALLOW_COPY_AND_ASSIGN(EngineerCVCommunication);
