98660c6af261f2ec8d102c145016c637f1db6f89
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-05-19 15:46:18 -0700

    new trust

--- aruw-mcb-project/src/aruwsrc/control/aruco/aruco_reset_subsystem.hpp
+++ aruw-mcb-project/src/aruwsrc/control/aruco/aruco_reset_subsystem.hpp
@@ -53,7 +53,7 @@ private:
     TransformerInterface& transformer;
 
     // Higher value here means we trust AruCo measurements more
-    float VISION_TRUST = 0.5f;
+    float VISION_TRUST = 0.05f;
 };  // class ArucoResetSubsystem
 
 }  // namespace aruwsrc::control::aruco
