bba317f86c1a101c0b3ea209c9b22ce1ad7a7604
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-05-21 23:17:57 -0700

    new transform update

--- aruw-mcb-project/src/aruwsrc/algorithms/odometry/standard_and_hero_transformer.cpp
+++ aruw-mcb-project/src/aruwsrc/algorithms/odometry/standard_and_hero_transformer.cpp
@@ -29,7 +29,7 @@ namespace aruwsrc::algorithms::transforms
 {
 #if defined(TARGET_STANDARD_NULL)
 static Transform TURRET_TO_ARDUCAM_OFFSET =
-    Transform(Position(0.016, 0.126, 0.056), Orientation(0, 0, 0));
+    Transform(Position(0.08, 0.1, 0.056), Orientation(0, 0, 0));
 #else
 static Transform TURRET_TO_ARDUCAM_OFFSET = Transform(Position(0, 0, 0), Orientation(0, 0, 0));
 #endif
