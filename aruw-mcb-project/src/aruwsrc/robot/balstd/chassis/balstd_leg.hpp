11643dd9de7454e1ce76b771881ac19cf499a40c
AidenP <aprevey@uw.edu>
2025-05-21 18:59:22 -0700

    fixed matrix size so it builds

--- aruw-mcb-project/src/aruwsrc/robot/balstd/chassis/balstd_leg.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/balstd/chassis/balstd_leg.hpp
@@ -158,7 +158,7 @@ struct BalstdLegState
             sqrtf(config.lowerLinkLength * config.lowerLinkLength - P2_Ph_mag * P2_Ph_mag);
 
         // P3 = Ph ± ||P3-Ph|| / ||P2-P4|| * (P4-P2)
-        CMSISMat<3,1> P3_2 = Ph + P3_Ph_mag / P4_P2_mag * CMSISMat<2, 1>({P4_P2.data[1], -P4_P2.data[0]});
+        CMSISMat<3,1> P3_2 = Ph + P3_Ph_mag / P4_P2_mag * CMSISMat<3, 1>({P4_P2.data[1], -P4_P2.data[0], 0});
         
         P3 = {{P3_2.data[0], P3_2.data[1], 0}};
     }
