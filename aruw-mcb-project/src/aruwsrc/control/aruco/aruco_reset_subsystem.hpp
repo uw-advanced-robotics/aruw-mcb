eee666bd9d63f7b16115130a8c79ad01f23c1e8f
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-05-21 19:57:29 -0700

    it can do 0.1 now

--- aruw-mcb-project/src/aruwsrc/control/aruco/aruco_reset_subsystem.hpp
+++ aruw-mcb-project/src/aruwsrc/control/aruco/aruco_reset_subsystem.hpp
@@ -53,7 +53,7 @@ private:
     TransformerInterface& transformer;
 
     // Higher value here means we trust AruCo measurements more
-    float VISION_TRUST = 0.05f;
+    float VISION_TRUST = 0.1f;
 
     void processRealsenseData();
     void processArducamData();
