1b3f516ee9a1a13d95de52c00480f6f0c5a81ba2
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-05-21 20:29:55 -0700

    undo that stuff

--- aruw-mcb-project/src/aruwsrc/control/aruco/aruco_reset_subsystem.hpp
+++ aruw-mcb-project/src/aruwsrc/control/aruco/aruco_reset_subsystem.hpp
@@ -53,8 +53,7 @@ private:
     TransformerInterface& transformer;
 
     // Higher value here means we trust AruCo measurements more
-    float VISION_TRUST_X = 0.1f;
-    float VISION_TRUST_Y = 0.025f;
+    float VISION_TRUST = 0.025f;
 
     void processRealsenseData();
     void processArducamData();
