60626d054016ff06ca7ecff87d7e612e88918324
Acacia Hwang <acaciah@uw.edu>
2025-05-23 01:52:26 +0000

    Final Cube Storage
    Co-authored-by: swaras <swaras@uw.edu>
    Co-authored-by: SumedhP <43325361+SumedhP@users.noreply.github.com>

---
+++ aruw-mcb-project/src/aruwsrc/robot/engineer/cube_lift/cube_move_position_command.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2025-2025 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "cube_move_position_command.hpp"
+
+#include <cstdlib>
+
+#include "aruwsrc/robot/engineer/cube_lift/engineer_lift_constants.hpp"
+namespace aruwsrc::robot::engineer
+{
+CubeMovePositionCommand::CubeMovePositionCommand(CubeStorageSubsystem &cubeLift, float setpoint)
+    : cubeLift(cubeLift),
+      setpoint(setpoint)
+{
+    addSubsystemRequirement(&cubeLift);
+}
+
+void CubeMovePositionCommand::initialize()
+{
+    cubeLift.setPIDState(PIDState::POSITION_PID);
+    cubeLift.setPositionSetpoint(setpoint);
+}
+
+void CubeMovePositionCommand::execute() {}
+
+void CubeMovePositionCommand::end(bool) {}
+
+bool CubeMovePositionCommand::isFinished() const
+{
+    return abs(setpoint - cubeLift.getMotorPosition()) < 0.5;
+}
+}  // namespace aruwsrc::robot::engineer
