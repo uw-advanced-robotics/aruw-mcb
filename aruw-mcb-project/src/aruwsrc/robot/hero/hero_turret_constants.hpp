72d6593950bd1522bbd84b124a81fb84e987a934
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-05-22 20:56:07 -0700

    clean

--- aruw-mcb-project/src/aruwsrc/robot/hero/hero_turret_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/hero/hero_turret_constants.hpp
@@ -82,17 +82,6 @@ static constexpr tap::algorithms::SmoothPidConfig YAW_POS_PID_CONFIG = {
 };
 
 static constexpr tap::algorithms::SmoothPidConfig YAW_POS_PID_AUTO_AIM_CONFIG = {
-    // .kp = 25.5f,
-    // .ki = 0.0f,
-    // .kd = 0.03f,
-    // .maxICumulative = 0.0f,
-    // .maxOutput = 1.0f,
-    // .tQDerivativeKalman = 1.0f,
-    // .tRDerivativeKalman = 0.0f,
-    // .tQProportionalKalman = 1.0f,
-    // .tRProportionalKalman = 0.0f,
-    // .errDeadzone = 0.0f,
-    // .errorDerivativeFloor = 0.0f,
     .kp = 0.0f,
     .ki = 0.0f,
     .kd = 0.0f,
@@ -166,17 +155,6 @@ static constexpr tap::algorithms::SmoothPidConfig PITCH_VEL_PID_CONFIG = {
 namespace world_rel_chassis_imu
 {
 static constexpr tap::algorithms::SmoothPidConfig YAW_PID_CONFIG = {
-    // .kp = 2500.0f,
-    // .ki = 0.0f,
-    // .kd = 0.0f,
-    // .maxICumulative = 0.0f,
-    // .maxOutput = 1.0f,
-    // .tQDerivativeKalman = 1.0f,
-    // .tRDerivativeKalman = 40.0f,
-    // .tQProportionalKalman = 1.0f,
-    // .tRProportionalKalman = 0.0f,
-    // .errDeadzone = 0.0f,
-    // .errorDerivativeFloor = 0.0f,
     .kp = 0.0f,
     .ki = 0.0f,
     .kd = 0.0f,
@@ -195,17 +173,6 @@ static constexpr tap::algorithms::SmoothPidConfig YAW_PID_CONFIG = {
 namespace chassis_rel
 {
 static constexpr tap::algorithms::SmoothPidConfig YAW_PID_CONFIG = {
-    // .kp = 2500.0f,
-    // .ki = 0.0f,
-    // .kd = 0.0f,
-    // .maxICumulative = 0.0f,
-    // .maxOutput = 1.0f,
-    // .tQDerivativeKalman = 1.0f,
-    // .tRDerivativeKalman = 40.0f,
-    // .tQProportionalKalman = 1.0f,
-    // .tRProportionalKalman = 0.0f,
-    // .errDeadzone = 0.0f,
-    // .errorDerivativeFloor = 0.0f,
     .kp = 0.0f,
     .ki = 0.0f,
     .kd = 0.0f,
