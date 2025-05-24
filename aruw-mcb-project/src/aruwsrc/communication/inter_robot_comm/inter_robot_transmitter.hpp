b02ccec31c9fbf619265f923cb30fe3344632b21
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-05-23 17:07:17 -0700

    started this

---
+++ aruw-mcb-project/src/aruwsrc/communication/inter_robot_comm/inter_robot_transmitter.hpp
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2025 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of Taproot.
+ *
+ * Taproot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Taproot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Taproot.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef INTER_ROBOT_TRANSMITTER_HPP_
+#define INTER_ROBOT_TRANSMITTER_HPP_
+
+#include "tap/communication/serial/ref_serial.hpp"
+#include "tap/communication/serial/ref_serial_transmitter.hpp"
+
+#include "modm/processing/protothread.hpp"
+
+namespace aruwsrc::communication::serial::inter_robot_comm
+{
+using namespace tap::communication::serial;
+class InterRobotTransmitter : public modm::pt::Protothread, RefSerial::RobotToRobotMessageHandler
+{
+public:
+    struct Message
+    {
+        int x = 1;
+        int y = 2;
+        int z = 3;
+    };
+
+    InterRobotTransmitter(
+        tap::communication::serial::RefSerial* refSerial,
+        tap::communication::serial::RefSerialTransmitter* refSerialTransmitter)
+        : refSerial(refSerial),
+          refSerialTransmitter(refSerialTransmitter)
+    {
+        refSerial->attachRobotToRobotMessageHandler(MSG_ID, this);
+    }
+
+    bool sendMessage()
+    {
+        PT_BEGIN()
+        // Send the message using the refSerialTransmitter
+        while (true)
+        {
+            targetId = getAllyRobotId();
+            memcpy(&robotToRobotMessage.dataAndCRC16[0], &message, sizeof(Message));
+
+            PT_CALL(refSerialTransmitter->sendRobotToRobotMsg(
+                &robotToRobotMessage,
+                MSG_ID,
+                targetId,
+                sizeof(Message)));
+        }
+        PT_END();
+    }
+
+    void operator()(const DJISerial::ReceivedSerialMessage& message) override
+    {
+        memcpy(&incomingMessage, &message.data[sizeof(RefSerialData::Tx::InteractiveHeader)], sizeof(Message));
+    }
+
+private:
+    Message message;
+    Message incomingMessage;
+    int parsingAMessage = 0;
+
+    RefSerial* refSerial;
+    RefSerialTransmitter* refSerialTransmitter;
+    RefSerialData::Tx::RobotToRobotMessage robotToRobotMessage;
+
+    uint16_t MSG_ID = 0x201;
+    RefSerial::RobotId targetId;
+
+    RefSerialTransmitter::RobotId getAllyRobotId() const
+    {
+        const auto& robotData = refSerial->getRobotData();
+        if (robotData.robotId == RefSerialData::RobotId::INVALID)
+        {
+            return RefSerialData::RobotId::INVALID;
+        }
+
+        bool isBlue = RefSerial::isBlueTeam(robotData.robotId);
+        if (isBlue)
+        {
+            return (robotData.robotId == RefSerialData::RobotId::BLUE_HERO)
+                       ? RefSerialData::RobotId::BLUE_SOLDIER_3
+                       : RefSerialData::RobotId::BLUE_HERO;
+        }
+        else
+        {
+            return (robotData.robotId == RefSerialData::RobotId::RED_HERO)
+                       ? RefSerialData::RobotId::RED_SOLDIER_3
+                       : RefSerialData::RobotId::RED_HERO;
+        }
+    }
+};
+
+}  // namespace aruwsrc::communication::serial::inter_robot_comm
+
+#endif
