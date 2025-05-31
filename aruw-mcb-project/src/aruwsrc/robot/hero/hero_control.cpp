b19ece873833289bcf822ab3000cd96a06d59b53
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-05-31 12:17:17 -0700

    update hero and standard controls to calibrate them

--- aruw-mcb-project/src/aruwsrc/robot/hero/hero_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/hero/hero_control.cpp
@@ -381,7 +381,8 @@ imu::ImuCalibrateCommand imuCalibrateCommand(
     &chassis,
     imu::ImuCalibrateCommand::DEFAULT_VELOCITY_ZERO_THRESHOLD,
     imu::ImuCalibrateCommand::DEFAULT_POSITION_ZERO_THRESHOLD,
-    &odometrySubsystem);
+    &odometrySubsystem,
+    {&drivers()->ism330});
 
 // beyblade governors
 
