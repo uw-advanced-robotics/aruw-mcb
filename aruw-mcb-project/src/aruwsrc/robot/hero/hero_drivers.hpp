76f009767344544d1ff19d17f875f9eb4b80714d
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-06-02 12:32:31 -0700

    fix build warnings

--- aruw-mcb-project/src/aruwsrc/robot/hero/hero_drivers.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/hero/hero_drivers.hpp
@@ -34,6 +34,7 @@
 
 #include "aruwsrc/algorithms/plate_hit_tracker.hpp"
 #include "aruwsrc/communication/can/turret_mcb_can_comm.hpp"
+#include "aruwsrc/communication/inter_robot_comm/inter_robot_transmitter.hpp"
 #include "aruwsrc/communication/sensors/imu/ism330.hpp"
 #include "aruwsrc/communication/serial/vision_coprocessor.hpp"
 #include "aruwsrc/display/oled_display.hpp"
