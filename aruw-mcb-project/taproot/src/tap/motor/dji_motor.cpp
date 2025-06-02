6d03f0408a30e1255f98265150aac6c5794a2b94
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-06-02 16:23:15 -0700

    erm?

--- aruw-mcb-project/taproot/src/tap/motor/dji_motor.cpp
+++ aruw-mcb-project/taproot/src/tap/motor/dji_motor.cpp
@@ -65,8 +65,9 @@ DjiMotor::DjiMotor(
       encoder(
           {externalEncoder != nullptr ? externalEncoder
                                       : const_cast<Encoder*>(&this->getInternalEncoder()),
-           externalEncoder != nullptr ? const_cast<Encoder*>(&this->getInternalEncoder())
-                                      : nullptr})
+           //    externalEncoder != nullptr ? const_cast<Encoder*>(&this->getInternalEncoder())
+           //                               : nullptr})
+           nullptr})
 {
     motorDisconnectTimeout.stop();
 }
