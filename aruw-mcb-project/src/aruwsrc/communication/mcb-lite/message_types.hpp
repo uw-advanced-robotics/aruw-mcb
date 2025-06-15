2bd928534879f7798752aaadefa2d20a2f6c8520
pie474 <chinmaymurthy474@gmail.com>
2025-06-15 00:29:50 -0700

    actually fix license header

--- aruw-mcb-project/src/aruwsrc/communication/mcb-lite/message_types.hpp
+++ aruw-mcb-project/src/aruwsrc/communication/mcb-lite/message_types.hpp
@@ -1,20 +1,20 @@
 /*
- * Copyright (c) 2023-2024 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ * Copyright (c) 2023-2025 Advanced Robotics at the University of Washington <robomstr@uw.edu>
  *
  * This file is part of aruw-mcb.
  *
- * aruw-mcb-lite is free software: you can redistribute it and/or modify
+ * aruw-mcb is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * aruw-mcb-lite is distributed in the hope that it will be useful,
+ * aruw-mcb is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with aruw-mcb-lite.  If not, see <https://www.gnu.org/licenses/>.
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef MESSAGE_TYPES_HPP_
