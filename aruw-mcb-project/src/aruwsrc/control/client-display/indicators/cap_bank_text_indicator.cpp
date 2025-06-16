f823c6141d55faebbbaf31e77e4215f28c60046e
Eli Orona <eliorona@live.com>
2025-06-16 02:06:39 -0700

    Create text graphic for cap bank

---
+++ aruw-mcb-project/src/aruwsrc/control/client-display/indicators/cap_bank_text_indicator.cpp
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2021-2022 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "cap_bank_text_indicator.hpp"
+
+#include "tap/communication/serial/ref_serial.hpp"
+#include "tap/communication/serial/ref_serial_transmitter.hpp"
+#include "tap/drivers.hpp"
+
+using namespace tap::communication::serial;
+
+namespace aruwsrc::control::client_display
+{
+CapBankTextIndicator::CapBankTextIndicator(
+    tap::communication::serial::RefSerialTransmitter &refSerialTransmitter,
+    const can::capbank::CapacitorBank *capBank)
+    : HudIndicator(refSerialTransmitter),
+      capBank(capBank)
+{
+}
+
+modm::ResumableResult<void> CapBankTextIndicator::sendInitialGraphics()
+{
+    this->previousState = can::capbank::State::UNKNOWN;
+    voltageUpdateTimer.restart(500);
+
+    RF_BEGIN(0);
+
+    // remove initial graphics
+    RF_CALL(refSerialTransmitter.sendGraphic(&capBankVoltageLevel));
+    RF_CALL(refSerialTransmitter.sendGraphic(&capBankTextGraphic));
+
+    RF_END();
+}
+
+modm::ResumableResult<void> CapBankTextIndicator::update()
+{
+    float voltage_squared = 0;
+    can::capbank::State state = can::capbank::UNKNOWN;
+
+    RF_BEGIN(1);
+
+    if (capBank != nullptr)
+    {
+        if (capBank->isOnline())
+        {
+            capBankVoltageLevel.graphicData.operation =
+                capBankVoltageLevel.graphicData.operation == Tx::GRAPHIC_DELETE
+                    ? Tx::GRAPHIC_ADD
+                    : Tx::GRAPHIC_MODIFY;
+            capBankTextGraphic.graphicData.operation =
+                capBankTextGraphic.graphicData.operation == Tx::GRAPHIC_DELETE ? Tx::GRAPHIC_ADD
+                                                                               : Tx::GRAPHIC_MODIFY;
+
+            // Update the voltage
+            voltage_squared = pow(capBank->getVoltage(), 2);
+
+            if (voltage_squared < VOLTAGE_SQUARED_MIN)
+            {
+                voltage_squared = VOLTAGE_SQUARED_MIN;
+            }
+
+            RefSerialTransmitter::configInteger(
+                SIZE,
+                WIDTH,
+                NUMBER_X,
+                TEXT_Y,
+                (voltage_squared - VOLTAGE_SQUARED_MIN) /
+                    (VOLTAGE_SQUARED_MAX - VOLTAGE_SQUARED_MIN) * 100,
+                &capBankVoltageLevel.graphicData);
+
+            // Update the status
+            state = capBank->getState();
+            switch (state)
+            {
+                case can::capbank::State::RESET:
+                    strncpy(capBankTextGraphic.msg, "RST ", 5);
+                    break;
+                case can::capbank::State::SAFE:
+                    strncpy(capBankTextGraphic.msg, "SAFE", 5);
+                    break;
+                case can::capbank::State::CHARGE:
+                    strncpy(capBankTextGraphic.msg, "CHRG", 5);
+                    break;
+                case can::capbank::State::CHARGE_DISCHARGE:
+                    strncpy(capBankTextGraphic.msg, "CHDS", 5);
+                    break;
+                case can::capbank::State::DISCHARGE:
+                    strncpy(capBankTextGraphic.msg, "DSCH", 5);
+                    break;
+                case can::capbank::State::BATTERY_OFF:
+                    strncpy(capBankTextGraphic.msg, "BOFF", 5);
+                    break;
+                case can::capbank::State::DISABLED:
+                    strncpy(capBankTextGraphic.msg, "OFF ", 5);
+                    break;
+                default:
+                    strncpy(capBankTextGraphic.msg, "UNK ", 5);
+                    break;
+            }
+            // Update the text
+            capBankTextGraphic.graphicData.endAngle = 5;  // Sets the length of the string
+
+            // Send data
+            if (state != this->previousState)
+            {
+                this->previousState = state;
+                RF_CALL(refSerialTransmitter.sendGraphic(&capBankTextGraphic));
+            }
+            if (voltageUpdateTimer.execute())
+            {
+                RF_CALL(refSerialTransmitter.sendGraphic(&capBankVoltageLevel));
+            }
+        }
+    }
+
+    RF_END();
+}
+
+void CapBankTextIndicator::initialize()
+{
+    uint8_t capBankName[3];
+
+    getUnusedGraphicName(capBankName);
+    RefSerialTransmitter::configGraphicGenerics(
+        &capBankVoltageLevel.graphicData,
+        capBankName,
+        Tx::GRAPHIC_DELETE,
+        DEFAULT_GRAPHIC_LAYER + 1,
+        Tx::GraphicColor::WHITE);
+
+    getUnusedGraphicName(capBankName);
+    RefSerialTransmitter::configGraphicGenerics(
+        &capBankTextGraphic.graphicData,
+        capBankName,
+        Tx::GRAPHIC_DELETE,
+        DEFAULT_GRAPHIC_LAYER,
+        Tx::GraphicColor::WHITE);
+
+    if (capBank != nullptr)
+    {
+        RefSerialTransmitter::configInteger(
+            SIZE,
+            WIDTH,
+            NUMBER_X,
+            TEXT_Y,
+            0,
+            &capBankVoltageLevel.graphicData);
+
+        RefSerialTransmitter::configCharacterMsg(
+            SIZE,
+            WIDTH,
+            TEXT_X,
+            TEXT_Y,
+            "",
+            &capBankTextGraphic);
+    }
+}
+}  // namespace aruwsrc::control::client_display
---
+++ aruw-mcb-project/src/aruwsrc/control/client-display/indicators/cap_bank_text_indicator.hpp
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef CAP_BANK_TEXT_INDICATOR_HPP_
+#define CAP_BANK_TEXT_INDICATOR_HPP_
+
+#include "tap/architecture/periodic_timer.hpp"
+#include "tap/communication/referee/state_hud_indicator.hpp"
+#include "tap/communication/serial/ref_serial_data.hpp"
+
+#include "aruwsrc/communication/can/capacitor_bank.hpp"
+#include "modm/processing/resumable.hpp"
+
+#include "hud_indicator.hpp"
+
+namespace tap
+{
+class Drivers;
+}
+
+namespace aruwsrc::control::client_display
+{
+/**
+ * Draws a bar on the HUD that represents the current charge of the cap bank.
+ */
+class CapBankTextIndicator : public HudIndicator, protected modm::Resumable<2>
+{
+public:
+    /**
+     * Construct a CapBankIndicator.
+     *
+     * @param[in] refSerialTransmitter Transmitter that stores ref serial transmission state for the
+     * protothread that this indicator is used in.
+     * @param[in] capBank A pointer to the capacitor bank for the robot.
+     */
+    CapBankTextIndicator(
+        tap::communication::serial::RefSerialTransmitter &refSerialTransmitter,
+        const can::capbank::CapacitorBank *capBank);
+
+    modm::ResumableResult<void> sendInitialGraphics() override final;
+
+    modm::ResumableResult<void> update() override final;
+
+    void initialize() override final;
+
+private:
+    // X position of the text
+    static constexpr uint16_t TEXT_X = SCREEN_WIDTH / 2 - 150;
+    // Y position of the text
+    static constexpr uint16_t TEXT_Y = 320;
+    // WIDTH of the text
+    static constexpr uint16_t WIDTH = 4;
+    // SIZE of the text
+    static constexpr uint16_t SIZE = 40;
+    static constexpr uint16_t NUMBER_X = TEXT_X + 175;
+
+    // Indicator bar is in units of voltage squared so it is proportional to energy,
+    //   but doesnt depend on differences in capacitance between robots, and limits can be easily
+    //   changed
+    // maximum displayed voltage of the supercapacitors (full charge)
+    static constexpr uint16_t VOLTAGE_SQUARED_MAX = pow(30, 2);
+    // minimum displayed voltage of the supercapacitors (minimum usable voltage)
+    static constexpr uint16_t VOLTAGE_SQUARED_MIN =
+        pow(can::capbank::CAPACITOR_BANK_MIN_VOLTAGE, 2);
+
+    const can::capbank::CapacitorBank *capBank;
+
+    /**
+     * A int that shows the charge of the Capacitor Bank 0 - 100%.
+     */
+    Tx::Graphic1Message capBankVoltageLevel;
+    tap::arch::PeriodicMilliTimer voltageUpdateTimer;
+
+    /**
+     * A graphic that represents the current status of the Capacitor Bank.
+     */
+    Tx::GraphicCharacterMessage capBankTextGraphic;
+
+    can::capbank::State previousState;
+};
+}  // namespace aruwsrc::control::client_display
+
+#endif  //  CAP_BANK_TEXT_INDICATOR_HPP_
