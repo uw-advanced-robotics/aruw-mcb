760c0df4ccd039042b3f5a09a1f13d4f0b48b02d
pie474 <chinmaymurthy474@gmail.com>
2025-06-16 15:51:59 -0700

    manual ik command header

---
+++ aruw-mcb-project/src/aruwsrc/robot/engineer/manual_ik_command.hpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2025-2025 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef MANUAL_IK_COMMAND_HPP_
+#define MANUAL_IK_COMMAND_HPP_
+
+#include "algorithms/abstract_ik_command.hpp"
+
+namespace aruwsrc::engineer
+{
+
+class ManualIKCommand : public algorithms::AbstractIKCommand
+{
+public:
+    ManualIKCommand();
+
+    void initialize() override;
+
+    void execute() override;
+
+    void end(bool interrupted) override;
+
+    bool isFinished() const override;
+
+    const char* getName() const override { return "Manual IK Command"; }
+
+    tap::algorithms::transforms::Transform getWorldToEEDesired() override;
+};  // class ManualIKCommand
+
+}  // namespace aruwsrc::engineer
+#endif  // MANUAL_IK_COMMAND_HPP_
