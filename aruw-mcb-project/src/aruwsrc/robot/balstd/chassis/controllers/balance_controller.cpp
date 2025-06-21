ff97813816d641918d82648f72ab4d3acdd4302b
pie474 <chinmaymurthy474@gmail.com>
2025-06-21 01:29:41 -0700

    gain schedule lqr wrt leg length

--- aruw-mcb-project/src/aruwsrc/robot/balstd/chassis/controllers/balance_controller.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/balstd/chassis/controllers/balance_controller.cpp
@@ -4,6 +4,8 @@
 #include "tap/algorithms/math_user_utils.hpp"
 #include "tap/algorithms/wrapped_float.hpp"
 
+#include "modm/architecture/utils.hpp"
+
 using tap::algorithms::Angle;
 using tap::algorithms::CMSISMat;
 using tap::algorithms::WrappedFloat;
@@ -104,14 +106,39 @@ Vector BalanceController::vmLegForces(
     return Vector(horizontalForce, downwardForce, 0);
 }
 
-CMSISMat<2, 6> BalanceController::getLQRGains(const float) const
+CMSISMat<2, 6> BalanceController::getLQRGains(const float legLength) const
 {
+    // TODO: use config for all this stuff
     // clang-format off
-    return CMSISMat<2, 6>({
-       -37.831,       -5.031,      -20.811 ,     -17.182    ,    31.92   ,    5.0632,
-       12.604,       1.6434 ,      8.1786  ,     6.2358   ,    68.298    ,   6.4245
-           }) * LQRScalar;
+    // return CMSISMat<2, 6>({  // for length = 0.17m
+    //    -37.831, -5.031, -20.811, -17.182, 31.92, 5.0632,
+    //    12.604, 1.6434, 8.1786, 6.2358, 68.298, 6.4245
+    //        }) * LQRScalar;
+    
+    float coeffs[5][12] {
+      {-9.0447, -1.6074, -16.313, -14.196,  61.599,  9.5572,
+        6.3627, 0.92331,  15.717,  11.688,  49.341,  3.1599},
+      {-204.39, -8.0768, -47.927, -15.125, -261.93, -45.14,
+        85.808,  6.4024, -60.963, -47.643,  206.25,  35.747},
+      { 247.44, -100.06,  171.53, -37.581,  638.76,  151.36,
+       -425.02, -16.212,  106.72,  113.15, -769.64, -136.04},
+      {-261.11,  199.68, -302.23,  169.06, -800.04, -279.79,
+         936.1,  22.581, -42.063, -137.25,  1429.5,  262.33},
+      { 111.38, -166.65,  212.08, -188.46,  387.98,  214.83,
+        -788.7,  -12.93, -59.553,  62.965, -1061.1, -202.77}};
     // clang-format on
+
+    CMSISMat<2, 6> K(coeffs[0]);
+    float x = legLength;
+    for (unsigned int i = 1; i < MODM_ARRAY_SIZE(coeffs); i++, x *= legLength)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            K.data[j] += coeffs[i][j] * x;
+        }
+    }
+
+    return K;
 }
 
 }  // namespace aruwsrc::balstd::chassis::controllers
