6f7d9c570495fd97135b11b6a69b8ab3d72c801a
Clementine Allen <callen42@uw.edu>
2025-06-24 12:09:36 -0700

    Updated beblade config

--- aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_chassis_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_chassis_constants.hpp
@@ -126,7 +126,7 @@ static constexpr float CHASSIS_GEARBOX_RATIO = (1.0f / 19.0f);
 static constexpr BeybladeConfig BEYBLADE_CONFIG{
     .beybladeRotationalSpeedFractionOfMax = 0.75f,
     .beybladeTranslationalSpeedMultiplier = 0.5f,
-    .beybladeRotationalSpeedMultiplierWhenTranslating = 0.6f,
+    .beybladeRotationalSpeedMultiplierWhenTranslating = 0.7f,
     .translationalSpeedThresholdMultiplierForRotationSpeedDecrease = 0.25f,
     .beybladeRampRate = 100,
 };
