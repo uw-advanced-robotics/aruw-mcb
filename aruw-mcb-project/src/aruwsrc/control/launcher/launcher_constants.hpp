49bc5f09a5bd019aa76a1b9148c8d357bdcae371
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-06-25 23:43:50 -0700

    remove bs

--- aruw-mcb-project/src/aruwsrc/control/launcher/launcher_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/control/launcher/launcher_constants.hpp
@@ -83,7 +83,6 @@ static constexpr modm::Pair<float, float> LAUNCH_SPEED_TO_FRICTION_WHEEL_RPM_LUT
     {0.0f, 0.0f},
     {4.0f, 1900.0f},
     {10.0f, 3850.0f},
-    {14.0f, 5250.0f}, // This constant is BS
     {15.0f, 5750.0f},
     {16.0f, 6500.0f},
     {18.0f, 8500.0f},
