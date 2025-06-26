30f8f80552b2cf249204500ceda49660e87d1229
Clementine Allen <callen42@uw.edu>
2025-06-26 10:14:05 -0700

    Bigger tolerances

--- aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_imu_calibrate_command.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_imu_calibrate_command.hpp
@@ -58,14 +58,14 @@ public:
     /**
      * Threshold around 0 where turret pitch and yaw velocity is considered to be 0, in radians/s
      */
-    static constexpr float VELOCITY_ZERO_THRESHOLD = modm::toRadian(1e-3);
+    static constexpr float VELOCITY_ZERO_THRESHOLD = modm::toRadian(1);
     /**
      * Threshold around 0 where turret pitch and yaw position from the center considered to be 0,
      * in radians.
      *
      * @note is 0 for max precision on sentry, which chassis-rel controllers can reach.
      */
-    static constexpr float POSITION_ZERO_THRESHOLD = modm::toRadian(0.08f);
+    static constexpr float POSITION_ZERO_THRESHOLD = modm::toRadian(1);
 
     /**
      * @param[in] drivers A pointer to the global drivers object.
