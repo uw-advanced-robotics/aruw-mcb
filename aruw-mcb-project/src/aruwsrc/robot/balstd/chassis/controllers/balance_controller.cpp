bc22efa5ba83b437be380745a9c825124ae42a23
AidenP <aprevey@uw.edu>
2025-08-02 22:28:16 -0700

    forgot this file

--- aruw-mcb-project/src/aruwsrc/robot/balstd/chassis/controllers/balance_controller.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/balstd/chassis/controllers/balance_controller.cpp
@@ -55,6 +55,10 @@ void BalanceController::initialize(const BalstdChassisState& state)
     vmRef.data = {0, 0, state.virtualWheelPos, 0, 0, 0};
 }
 
+float leftHipTorque = 0;
+float rightHipTorque = 0;
+float hipTorque;
+
 BalstdChassisOutput BalanceController::runController(const BalstdChassisState& currState, float dt)
 {
     // update state references
@@ -82,7 +86,7 @@ BalstdChassisOutput BalanceController::runController(const BalstdChassisState& c
 
     // u = K(x_d - x)
     CMSISMat<2, 1> vmOuts = getLQRGains(currState.virtualLegState.L) * (vmRef - vmState);
-    float hipTorque = -vmOuts.data[1] / 2 * LQRHipScalar;
+    hipTorque = -vmOuts.data[1] / 2 * LQRHipScalar;
     float wheelTorque = vmOuts.data[0] / 2 * LQRWheelScalar;
     // virtual model has 1 hip/wheel, so we divide by 2 because we have 2
 
@@ -108,8 +112,8 @@ BalstdChassisOutput BalanceController::runController(const BalstdChassisState& c
     float leftDownForce = -chassisWeight * gravityScalar + heightControllerOut + rollControllerOut;
     float rightDownForce = -chassisWeight * gravityScalar + heightControllerOut - rollControllerOut;
 
-    float leftHipTorque = hipTorque + splitControllerOut;
-    float rightHipTorque = hipTorque - splitControllerOut;
+    leftHipTorque = hipTorque + splitControllerOut;
+    rightHipTorque = hipTorque - splitControllerOut;
 
     float leftWheelTorque = wheelTorque - yawControllerOut;
     float rightWheelTorque = wheelTorque + yawControllerOut;
@@ -126,30 +130,31 @@ Vector BalanceController::vmLegForces(
     float downwardForce,
     const BalstdLegState& currState) const
 {
-    float horizontalForce = -hipTorque / currState.L;
-    tap::algorithms::rotateVector(&horizontalForce, &downwardForce, currState.alpha);
-    return Vector(horizontalForce, downwardForce, 0);
+    float tangentForce = -hipTorque / currState.L;
+    float radialForce = 0;
+    tap::algorithms::rotateVector(&tangentForce, &radialForce, currState.alpha);
+    return Vector(tangentForce, downwardForce + radialForce, 0);
 }
 
 CMSISMat<2, 6> BalanceController::getLQRGains(const float legLength) const
 {
     // TODO: use config for all this stuff
     // clang-format off
-    float coeffs[1][12] {{  // for length = 0.17m
-       -37.831, -5.031, -20.811, -17.182, 31.92, 5.0632,
-       12.604, 1.6434, 8.1786, 6.2358, 68.298, 6.4245}};
+    // float coeffs[1][12] {{  // for length = 0.17m
+    //    -37.831, -5.031, -20.811, -17.182, 31.92, 5.0632,
+    //    12.604, 1.6434, 8.1786, 6.2358, 68.298, 6.4245}};
     
-    // float coeffs[5][12] {
-    //   {-9.0447, -1.6074, -16.313, -14.196,  61.599,  9.5572,
-    //     6.3627, 0.92331,  15.717,  11.688,  49.341,  3.1599},
-    //   {-204.39, -8.0768, -47.927, -15.125, -261.93, -45.14,
-    //     85.808,  6.4024, -60.963, -47.643,  206.25,  35.747},
-    //   { 247.44, -100.06,  171.53, -37.581,  638.76,  151.36,
-    //    -425.02, -16.212,  106.72,  113.15, -769.64, -136.04},
-    //   {-261.11,  199.68, -302.23,  169.06, -800.04, -279.79,
-    //      936.1,  22.581, -42.063, -137.25,  1429.5,  262.33},
-    //   { 111.38, -166.65,  212.08, -188.46,  387.98,  214.83,
-    //     -788.7,  -12.93, -59.553,  62.965, -1061.1, -202.77}};
+    float coeffs[5][12] {
+      {-9.0447, -1.6074, -16.313, -14.196,  61.599,  9.5572,
+        6.3627, 0.92331,  15.717,  11.688,  49.341,  3.1599},
+      {-204.39, -8.0768, -47.927, -15.125, -261.93, -45.14,
+        85.808,  6.4024, -60.963, -47.643,  206.25,  35.747},
+      { 247.44, -100.06,  171.53, -37.581,  638.76,  151.36,
+       -425.02, -16.212,  106.72,  113.15, -769.64, -136.04},
+      {-261.11,  199.68, -302.23,  169.06, -800.04, -279.79,
+         936.1,  22.581, -42.063, -137.25,  1429.5,  262.33},
+      { 111.38, -166.65,  212.08, -188.46,  387.98,  214.83,
+        -788.7,  -12.93, -59.553,  62.965, -1061.1, -202.77}};
     // // clang-format on
 
     CMSISMat<2, 6> K(coeffs[0]);
