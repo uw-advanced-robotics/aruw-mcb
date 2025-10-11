dbef3f9fb30b582f6b0c6c243ac122acf0885e4b
pie474 <chinmaymurthy474@gmail.com>
2025-10-11 11:40:50 -0700

    prep

--- aruw-mcb-project/src/aruwsrc/control/buzzer/buzzer_subsystem.cpp
+++ aruw-mcb-project/src/aruwsrc/control/buzzer/buzzer_subsystem.cpp
@@ -23,19 +23,7 @@
 
 namespace aruwsrc::control::buzzer
 {
-BuzzerSubsystem::BuzzerSubsystem(tap::Drivers* drivers) : Subsystem(drivers) {}
 
-void BuzzerSubsystem::playFrequency(float frequency)
-{
-    tap::buzzer::playNote(&(drivers->pwm), static_cast<uint32_t>(frequency));
-}
-
-void BuzzerSubsystem::playNote(uint8_t note)
-{
-    if (note > 63) return;
-    playFrequency(NOTE_FREQUENCIES[note]);
-}
-
-void BuzzerSubsystem::stop() { tap::buzzer::silenceBuzzer(&(drivers->pwm)); }
+// TODO: Write function implementations
 
 }  // namespace aruwsrc::control::buzzer
