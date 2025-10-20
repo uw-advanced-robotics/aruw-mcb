88e5692fb932cbeadc85b095c773846b3fab8ed9
Acacia <acaciah@uw.edu>
2025-10-19 17:49:51 -0700

    idek what happened Revert "Merge branch 'AcaciaH/edu/buzzer-subsystem' of https://gitlab.com/aruw/controls/aruw-mcb into AcaciaSwara/uncook-engineer-code"
    This reverts commit cd3c9ccc3b11f117553f7e43aa250da9707e2076, reversing
    changes made to b92fd477e8e7106209f9785a9c54f68c95a3ff99.

--- aruw-mcb-project/src/aruwsrc/control/buzzer/buzzer_subsystem.cpp
+++ aruw-mcb-project/src/aruwsrc/control/buzzer/buzzer_subsystem.cpp
@@ -23,7 +23,19 @@
 
 namespace aruwsrc::control::buzzer
 {
+BuzzerSubsystem::BuzzerSubsystem(tap::Drivers* drivers) : Subsystem(drivers) {}
 
-// TODO: Write function implementations
+void BuzzerSubsystem::playFrequency(float frequency)
+{
+    tap::buzzer::playNote(&(drivers->pwm), static_cast<uint32_t>(frequency));
+}
+
+void BuzzerSubsystem::playNote(uint8_t note)
+{
+    if (note > 63) return;
+    playFrequency(NOTE_FREQUENCIES[note]);
+}
+
+void BuzzerSubsystem::stop() { tap::buzzer::silenceBuzzer(&(drivers->pwm)); }
 
 }  // namespace aruwsrc::control::buzzer
