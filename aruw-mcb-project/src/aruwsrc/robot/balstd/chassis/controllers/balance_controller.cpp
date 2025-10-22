462dbf768cd0e7be82eb60df4763bcdf6c29dd6d
pie474 <chinmaymurthy474@gmail.com>
2025-10-22 12:24:53 -0700

    format

--- aruw-mcb-project/src/aruwsrc/robot/balstd/chassis/controllers/balance_controller.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/balstd/chassis/controllers/balance_controller.cpp
@@ -61,11 +61,10 @@ BalstdChassisOutput BalanceController::runController(const BalstdChassisState& c
     vmRef.data[2] += controlOperatorInterface.getXVel() * dt;
     yawSetpoint += controlOperatorInterface.getYawVel() * dt;
 
-    heightSetpoint.setTarget(
-        std::clamp(
-            heightSetpoint.getTarget() + controlOperatorInterface.getHeightVel() * dt,
-            config.minHeight,
-            config.maxHeight));
+    heightSetpoint.setTarget(std::clamp(
+        heightSetpoint.getTarget() + controlOperatorInterface.getHeightVel() * dt,
+        config.minHeight,
+        config.maxHeight));
     heightSetpoint.update(config.maxHeightSetpointVel * dt);
 
     rollSetpoint.setTarget(controlOperatorInterface.getRoll());
