3c5f4eeef47b4db03f42e2c9a2502d94fc773cb1
EricJGoossen <fada@uw.edu>
2025-10-29 10:11:01 -0700

    Finished PID thingy

--- aruw-mcb-project/src/aruwsrc/robot/motor_tester/motor_subsystem.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/motor_tester/motor_subsystem.hpp
@@ -4,29 +4,42 @@
 #include "tap/algorithms/math_user_utils.hpp"
 #include "tap/control/subsystem.hpp"
 #include "tap/motor/dji_motor.hpp"
+#include "tap/algorithms/smooth_pid.hpp"
+#include "tap/algorithms/wrapped_float.hpp"
 
 namespace aruwsrc::robot::motor_tester {
 
 class MotorSubsystem : public tap::control::Subsystem {
 public:
-    MotorSubsystem(tap::Drivers* drivers, tap::motor::MotorInterface& motor_);
+    MotorSubsystem(tap::Drivers* drivers, tap::motor::MotorInterface& motor, tap::algorithms::SmoothPidConfig& pidConfig, float deltaT) :
+                    Subsystem(drivers), motor(motor), pid(pidConfig), targetPosition(tap::algorithms::Angle(0)), deltaT(deltaT) {}
 
-    void setDesiredOutput(int32_t desiredOutput_) {
-        int32_t cap = tap::motor::DjiMotor::MAX_OUTPUT_C620;
-        desiredOutput = std::clamp(desiredOutput_, -cap, cap);
+    void setTargetVelocity(float targetVelocity_) {
+        targetVelocity = targetVelocity_;
     }
 
-    int32_t getDesiredOutput() const {
-        return desiredOutput;
+    float getTargetPosition() const {
+        return targetPosition.getWrappedValue();
     }
 
-    void refresh() {
-        motor.setDesiredOutput(desiredOutput);
+    void refresh() override {
+        targetPosition += targetVelocity * deltaT;
+
+        tap::algorithms::WrappedFloat error = targetPosition - motor.getEncoder()->getPosition();
+        float dError = -motor.getEncoder()->getVelocity();
+
+        float output = pid.runController(error.getUnwrappedValue(), dError, deltaT);
+
+        float cap = tap::motor::DjiMotor::MAX_OUTPUT_C620;
+        motor.setDesiredOutput(std::clamp(output, -cap, cap));
     }
 
 private: 
     tap::motor::MotorInterface& motor;
-    int32_t desiredOutput;
+    tap::algorithms::SmoothPid pid;
+    tap::algorithms::WrappedFloat targetPosition;
+    float targetVelocity;
+    float deltaT;
 };
 
 } 
