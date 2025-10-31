1602457b2863a5f4aaf1c200ce7ae8b2741c5bf6
Acacia <acaciah@uw.edu>
2025-10-30 17:52:40 -0700

    asdfoihsdf restarting again...

--- aruw-mcb-project/src/aruwsrc/robot/hero/hero_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/hero/hero_control.cpp
@@ -84,7 +84,8 @@
 #include "aruwsrc/control/governor/yellow_carded_governor.hpp"
 #include "aruwsrc/control/imu/imu_calibrate_command.hpp"
 #include "aruwsrc/control/launcher/friction_wheel_spin_ref_limited_command.hpp"
-#include "aruwsrc/control/launcher/referee_feedback_friction_wheel_subsystem.hpp"
+#include "aruwsrc/control/launcher/n_flywheel/modular_referee_feedback_friction_wheel_subsystem.hpp"
+#include "aruwsrc/control/launcher/launcher_constants.hpp"
 #include "aruwsrc/control/safe_disconnect.hpp"
 #include "aruwsrc/control/turret/algorithms/chassis_frame_turret_controller.hpp"
 #include "aruwsrc/control/turret/algorithms/world_frame_chassis_imu_turret_controller.hpp"
@@ -115,6 +116,7 @@ using namespace aruwsrc::control::turret;
 using namespace aruwsrc::hero;
 using tap::control::CommandMapper;
 using tap::control::RemoteMapState;
+using namespace aruwsrc::control::launcher::modular_flywheel;
 
 /*
  * NOTE: We are using the DoNotUse_getDrivers() function here
@@ -183,11 +185,29 @@ XDriveChassisSubsystem chassis(
     aruwsrc::chassis::WHEEL_VELOCITY_PID_CONFIG,
     &drivers()->capacitorBank);
 
-RefereeFeedbackFrictionWheelSubsystem<aruwsrc::control::launcher::LAUNCH_SPEED_AVERAGING_DEQUE_SIZE>
+    std::array<u_int32_t, 2> wheelIDs = {aruwsrc::control::launcher::LEFT_MOTOR_ID, aruwsrc::control::launcher::RIGHT_MOTOR_ID};
+    modm::Pid<float> velocityPIDLeft(aruwsrc::control::launcher::LAUNCHER_PID_KP,
+          aruwsrc::control::launcher::LAUNCHER_PID_KI,
+          aruwsrc::control::launcher::LAUNCHER_PID_KD,
+          aruwsrc::control::launcher::LAUNCHER_PID_MAX_ERROR_SUM,
+          aruwsrc::control::launcher::LAUNCHER_PID_MAX_OUTPUT);
+          modm::Pid<float> velocityPIDRight(aruwsrc::control::launcher::LAUNCHER_PID_KP,
+          aruwsrc::control::launcher::LAUNCHER_PID_KI,
+          aruwsrc::control::launcher::LAUNCHER_PID_KD,
+          aruwsrc::control::launcher::LAUNCHER_PID_MAX_ERROR_SUM,
+          aruwsrc::control::launcher::LAUNCHER_PID_MAX_OUTPUT);
+    FlywheelConfig wheelConfigLeft = {
+        velocityPIDLeft, true, "Left Flywheel"
+    };
+    FlywheelConfig wheelConfigRight = {
+        velocityPIDRight, false, "Right Flywheel"
+    };
+    std::array<FlywheelConfig, 2> wheelConfigs = {wheelConfigLeft, wheelConfigRight};
+ModularRefereeFeedbackFrictionWheelSubsystem<aruwsrc::control::launcher::LAUNCH_SPEED_AVERAGING_DEQUE_SIZE, 2>
     frictionWheels(
         drivers(),
-        aruwsrc::control::launcher::LEFT_MOTOR_ID,
-        aruwsrc::control::launcher::RIGHT_MOTOR_ID,
+        wheelIDs,
+        wheelConfigs,
         aruwsrc::control::launcher::CAN_BUS_MOTORS,
         &getTurretMCBCanComm(),
         tap::communication::serial::RefSerialData::Rx::MechanismID::TURRET_42MM);
