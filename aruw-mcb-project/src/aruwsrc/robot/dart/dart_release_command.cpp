d144a89e66aa189a858a48a72108a975264c05f0
Swara Seshadri <swara_s@outlook.com>
2025-11-05 01:30:40 +0000

    Resolve "Dart Launcher Subsystem"
    Co-authored-by: fallgold <acaciah@uw.edu>
    Co-authored-by: AidenP <aprevey@uw.edu>
    Co-authored-by: ananyaa06 <ananyaa06@gmail.com>
    Co-authored-by: swaras <swaras@uw.edu>
    Co-authored-by: Eli Orona <eliorona@live.com>

---
+++ aruw-mcb-project/src/aruwsrc/robot/dart/dart_release_command.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2025 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aruw-mcb.
+ *
+ * aruw-mcb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aruw-mcb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "dart_release_command.hpp"
+
+#include "dart_launcher_subsystem.hpp"
+
+namespace aruwsrc::robot::dart
+{
+DartReleaseCommand::DartReleaseCommand(DartLauncherSubsystem &dartLauncher, int32_t desiredOutput)
+    : dartLauncher(dartLauncher),
+      desiredOutput(desiredOutput)
+{
+    addSubsystemRequirement(&dartLauncher);
+}
+
+void DartReleaseCommand::initialize() { dartLauncher.moveMotor(desiredOutput); }
+
+void DartReleaseCommand::end(bool) { dartLauncher.moveMotor(0); }
+
+bool DartReleaseCommand::isFinished() const
+{
+    // return dartLauncher.isLimitSwitched();
+    // TODO: uncomment when limit switch is added
+    return false;
+}
+
+}  // namespace aruwsrc::robot::dart
