6a5c3ed18557192b6dfdf1e9160410cbee616c37
AidenP <aprevey@uw.edu>
2025-11-20 13:26:23 -0800

    fix sentry name oops

--- aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_control.cpp
@@ -237,9 +237,9 @@ SentryTurretMinorSubsystem turretRight(
 SentryChassisWorldYawObserver chassisYawObserver(drivers()->turretMajorImu, turretMajor);
 
 // Turret Compensators
-turretGravitationalForceOffset turretGravityCompensation(
+TurretGravitationalForceOffset turretGravityCompensation(
     TURRET_CG_X,
-    TURRET_GC_Z,
+    TURRET_CG_Z,
     GRAVITY_COMPENSATION_SCALAR);
 
 struct TurretMinorChassisControllers
@@ -1018,11 +1018,13 @@ void initSubsystemCommands(aruwsrc::sentry::Drivers *drivers)
 }  // namespace aruwsrc::sentry
 
 #ifndef PLATFORM_HOSTED
-std::vector<aruwsrc::control::autotune::TurretAutotuneInterface<std::array<float, 3>> *> getGravityAutotuneCommands()
+std::vector<aruwsrc::control::autotune::TurretAutotuneInterface<std::array<float, 3>> *>
+getGravityAutotuneCommands()
 {
-    static std::vector<aruwsrc::control::autotune::TurretAutotuneInterface<std::array<float, 3>> *> commands = {
-        &sentry_control::gravityAutotuneCommandLeft,
-        &sentry_control::gravityAutotuneCommandRight};
+    static std::vector<aruwsrc::control::autotune::TurretAutotuneInterface<std::array<float, 3>> *>
+        commands = {
+            &sentry_control::gravityAutotuneCommandLeft,
+            &sentry_control::gravityAutotuneCommandRight};
     return commands;
 }
 // imu::ImuCalibrateCommand *getImuCalibrateCommand() { return
