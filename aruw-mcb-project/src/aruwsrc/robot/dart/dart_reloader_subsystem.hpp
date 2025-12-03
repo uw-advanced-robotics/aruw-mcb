98766af36af3abb6f3a63d5b8f9055e1cdf736a5
Ethan <ethanmwgo@gmail.com>
2025-12-02 17:57:49 -0800

    fart reloader

---
+++ aruw-mcb-project/src/aruwsrc/robot/dart/dart_reloader_subsystem.hpp
@@ -0,0 +1,49 @@
+/*
+* Copyright (c) 2025-2025 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+*
+* This file is part of aruw-mcb.
+*
+* aruw-mcb is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* aruw-mcb is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with aruw-mcb.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#ifndef DART_RELOADER_SUBSYSTEM_HPP_
+#define DART_RELOADER_SUBSYSTEM_HPP_
+
+#include "tap/control/subsystem.hpp"
+#include <tap/motor/dji_motor.hpp>
+
+namespace aruwsrc::robot::dart
+{
+
+class DartReloaderSubsystem : public tap::control::Subsystem
+{
+public: 
+    DartReloaderSubsystem(
+        tap::Drivers* drivers);
+
+    void initialize() override;
+
+    void setMotorOutput() override;
+
+    void refresh() override;
+
+    void refreshSafeDisconnect() override;
+
+    const char* getName() const override { return "Dart Reloader Subsystem"; }
+
+private:
+    tap::motor::MotorInterface motor;
+};  // class CLASS_NAME
+
+}  // NAMESPACE
+#endif  // DART_RELOADER_SUBSYSTEM_HPP_
