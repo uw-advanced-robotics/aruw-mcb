744080d2d4897afef883bf8aa7492d0a7cc1f707
Clementine Allen <callen42@uw.edu>
2026-01-16 14:30:28 -0800

    imple

--- aruw-mcb-project/src/aruwsrc/robot/standard/standard_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/standard/standard_control.cpp
@@ -73,6 +73,7 @@
 #include "aruwsrc/control/client-display/indicators/text_hud_indicators.hpp"
 
 //#include "aruwsrc/control/client-display/indicators/vision_assistance_indicator.hpp"
+#include "aruwsrc/algorithms/odometry/wheel_ekf_odometry_2d_subsystem.hpp"
 #include "aruwsrc/control/autotune/gravity_autotune.hpp"
 #include "aruwsrc/control/autotune/spring_autotune.hpp"
 #include "aruwsrc/control/client-display/old-indicators/vision_target_indicator.hpp"
@@ -238,13 +239,50 @@ tap::encoder::CanEncoder perpendicularOmni(
     tap::encoder::CanEncoderId::ID0,
     tap::can::CanBus::CAN_BUS2);
 
+const tap::motor::DjiMotor *chassisMotors[4] = {
+    &leftFrontChassisMotor,
+    &leftBackChassisMotor,
+    &rightFrontChassisMotor,
+    &rightBackChassisMotor};
+
+// FourWheelEKFOdometry::ChassisWheelConfig LEFT_FRONT_WHEEL_CONFIG = {
+//     .wheelRadius = aruwsrc::chassis::WHEEL_RADIUS,
+//     .wheelbaseDistance = aruwsrc::chassis::CENTER_TO_WHEELBASE_RADIUS,
+//     .wheelOrientationToForwardRadians = M_PI_4  // +45°, contributes +X, -Y
+// };
+// FourWheelEKFOdometry::ChassisWheelConfig LEFT_BACK_WHEEL_CONFIG = {
+//     .wheelRadius = aruwsrc::chassis::WHEEL_RADIUS,
+//     .wheelbaseDistance = aruwsrc::chassis::CENTER_TO_WHEELBASE_RADIUS,
+//     .wheelOrientationToForwardRadians = -M_PI_4  // -45°, contributes +X, +Y
+// };
+
+// FourWheelEKFOdometry::ChassisWheelConfig RIGHT_FRONT_WHEEL_CONFIG = {
+//     .wheelRadius = aruwsrc::chassis::WHEEL_RADIUS,
+//     .wheelbaseDistance = aruwsrc::chassis::CENTER_TO_WHEELBASE_RADIUS,
+//     .wheelOrientationToForwardRadians = 3*M_PI_4  // +135°, contributes -X, -Y
+// };
+
+// FourWheelEKFOdometry::ChassisWheelConfig RIGHT_BACK_WHEEL_CONFIG = {
+//     .wheelRadius = aruwsrc::chassis::WHEEL_RADIUS,
+//     .wheelbaseDistance = aruwsrc::chassis::CENTER_TO_WHEELBASE_RADIUS,
+//     .wheelOrientationToForwardRadians = -3*M_PI_4  // -135°, contributes -X, +Y
+// };
+
+// static const FourWheelEKFOdometry::ChassisWheelConfig *chassisWheelConfigs[4] = {
+//     &LEFT_FRONT_WHEEL_CONFIG,
+//     &LEFT_BACK_WHEEL_CONFIG,
+//     &RIGHT_FRONT_WHEEL_CONFIG,
+//     &RIGHT_BACK_WHEEL_CONFIG
+// };
+
 aruwsrc::algorithms::odometry::OttoChassisWorldYawObserver yawObserver(turret);
-aruwsrc::algorithms::odometry::ChassisCFOdometry odometrySubsystem(
-    drivers(),
-    chassis,
+
+aruwsrc::algorithms::odometry::WheelEKFOdometry2DSubsystem odometrySubsystem(
+    *drivers(),
+    chassisMotors,
+    // chassisWheelConfigs,
     yawObserver,
-    // drivers()->ism330,
-    drivers()->mpu6500,
+    &chassis,
     modm::Vector2f(
         aruwsrc::control::chassis::INITIAL_CHASSIS_POSITION_X,
         aruwsrc::control::chassis::INITIAL_CHASSIS_POSITION_Y));
