14b64369bfb15e0926aff73fe52a9f936e7c0fcd
fallgold <acaciah@uw.edu>
2026-02-06 15:19:44 -0800

    fix 2025 engi

--- aruw-mcb-project/src/aruwsrc/robot/2025engineer/engineer_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/2025engineer/engineer_control.cpp
@@ -38,6 +38,9 @@
 #include "aruwsrc/control/client-display/client_display_command.hpp"
 #include "aruwsrc/control/client-display/client_display_subsystem.hpp"
 #include "aruwsrc/control/cycle_state_command_mapping.hpp"
+#include "aruwsrc/control/digital/digital_out_command.hpp"
+#include "aruwsrc/control/digital/digital_out_subsystem.hpp"
+#include "aruwsrc/control/digital/digital_out_toggle_command.hpp"
 #include "aruwsrc/control/joint/homing/homing_command.hpp"
 #include "aruwsrc/control/joint/homing/trigger/limit_switch_trigger.hpp"
 #include "aruwsrc/control/joint/homing/trigger_homed_dual_joint_subsystem.hpp"
@@ -45,9 +48,6 @@
 #include "aruwsrc/control/safe_disconnect.hpp"
 #include "aruwsrc/drivers_singleton.hpp"
 #include "aruwsrc/robot/2025engineer/cubelift_switch_command.hpp"
-#include "aruwsrc/robot/2025engineer/digital_out_command.hpp"
-#include "aruwsrc/robot/2025engineer/digital_out_subsystem.hpp"
-#include "aruwsrc/robot/2025engineer/digital_out_toggle_command.hpp"
 #include "aruwsrc/robot/2025engineer/engineer_cube_lift_constants.hpp"
 #include "aruwsrc/robot/2025engineer/engineer_drivers.hpp"
 #include "aruwsrc/robot/2025engineer/engineer_gantry_constants.hpp"
@@ -64,6 +64,7 @@
 
 using namespace aruwsrc::control::client_display;
 using namespace aruwsrc::control::client_display::indicators;
+using namespace aruwsrc::control::digital;
 using namespace aruwsrc::control::joint;
 using namespace aruwsrc::control::joint::homing;
 using namespace aruwsrc::control::joint::homing::trigger;
