650f2aba7e081a1b977ad20d3c99a8f5825c6f51
fallgold <acaciah@uw.edu>
2026-02-11 00:21:51 -0800

    perhaps its cooked

--- aruw-mcb-project/src/aruwsrc/control/launcher/launcher_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/control/launcher/launcher_constants.hpp
@@ -99,7 +99,7 @@ static constexpr float LAUNCHER_PID_KP = 20.0f;
 static constexpr float LAUNCHER_PID_KI = 100.0f;
 static constexpr float LAUNCHER_PID_KD = 0.0f;
 static constexpr float LAUNCHER_PID_MAX_ERROR_SUM = 5'000.0f;
-static constexpr float LAUNCHER_PID_MAX_OUTPUT = 16'000.0f;
+static constexpr float LAUNCHER_PID_MAX_OUTPUT = 15'500.0f;
 #endif
 
 static constexpr tap::algorithms::SmoothPidConfig VELOCITY_PID_CONFIG(
