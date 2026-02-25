08dd6e00a4a9d831c68f8e434001229e6b2187c0
Acacia Hwang <acaciah@uw.edu>
2026-02-25 01:59:33 +0000

    Flywheel Testing pt 2
    Co-authored-by: Fall Gold <92420381+Fall-Gold@users.noreply.github.com>

--- aruw-mcb-project/src/aruwsrc/control/launcher/launcher_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/control/launcher/launcher_constants.hpp
@@ -50,15 +50,13 @@ struct FlywheelRpms
     float rightRpm;
     float lowerRpm;
     float upperRpm;
-    float smallUpperRpm;
 };
 
 static constexpr FlywheelRpms flywheelTestingRpms{
-    .leftRpm = 0.0f,
-    .rightRpm = 0.0f,
-    .lowerRpm = 60.0f,
-    .upperRpm = 60.0f,
-    .smallUpperRpm = 60.0f};
+    .leftRpm = 6000.0f,
+    .rightRpm = 6000.0f,
+    .lowerRpm = 6000.0f,
+    .upperRpm = 6000.0f};
 #endif
 
 #if defined(ALL_SENTRIES)
@@ -66,7 +64,6 @@ static constexpr tap::motor::MotorId LEFT_MOTOR_ID = tap::motor::MOTOR2;
 static constexpr tap::motor::MotorId RIGHT_MOTOR_ID = tap::motor::MOTOR1;
 #elif defined(TARGET_FLYWHEEL_TESTING)
 static constexpr tap::motor::MotorId UPPER_MOTOR_ID = tap::motor::MOTOR6;
-static constexpr tap::motor::MotorId UPPER_SMALL_MOTOR_ID = tap::motor::MOTOR2;
 static constexpr tap::motor::MotorId LOWER_MOTOR_ID = tap::motor::MOTOR4;
 static constexpr tap::motor::MotorId LEFT_MOTOR_ID = tap::motor::MOTOR3;
 static constexpr tap::motor::MotorId RIGHT_MOTOR_ID = tap::motor::MOTOR1;
@@ -94,6 +91,19 @@ static constexpr float LAUNCHER_PID_KI = 31.6228f;
 static constexpr float LAUNCHER_PID_KD = 0.0f;
 static constexpr float LAUNCHER_PID_MAX_ERROR_SUM = 4'000.0f;
 static constexpr float LAUNCHER_PID_MAX_OUTPUT = tap::motor::DjiMotor::MAX_OUTPUT_C610;
+#elif defined(TARGET_FLYWHEEL_TESTING)
+static constexpr float LAUNCHER_PID_KP = 20.0f;
+static constexpr float LAUNCHER_PID_KI = 100.0f;
+static constexpr float LAUNCHER_PID_KD = 0.0f;
+static constexpr float LAUNCHER_PID_MAX_ERROR_SUM = 5'000.0f;
+static constexpr float LAUNCHER_PID_MAX_OUTPUT = tap::motor::DjiMotor::MAX_OUTPUT_820R;
+static constexpr tap::algorithms::SmoothPidConfig LEFT_VELOCITY_PID_CONFIG(
+    LAUNCHER_PID_KP,
+    LAUNCHER_PID_KI,
+    LAUNCHER_PID_KD,
+    LAUNCHER_PID_MAX_ERROR_SUM,
+    tap::motor::DjiMotor::MAX_OUTPUT_C620);
+static constexpr FlywheelConfig LEFT_WHEEL_CONFIG = {LEFT_VELOCITY_PID_CONFIG, 270.0f};
 #else
 static constexpr float LAUNCHER_PID_KP = 14.0106f;
 static constexpr float LAUNCHER_PID_KI = 31.6228f;
