cd7096c7b658216ea9f931c8f4d9eb14d03fdd27
Clementine Allen <callen42@uw.edu>
2026-02-28 13:40:10 -0800

    analog to can encoder

--- aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_control.cpp
@@ -28,7 +28,9 @@
 #include "tap/motor/double_dji_motor.hpp"
 
 #include "aruwsrc/algorithms/odometry/wheel_ekf_odometry_2d_subsystem.hpp"
+#include "aruwsrc/communication/can/aruw_analog_sensor.hpp"
 #include "aruwsrc/communication/can/aruw_voltage_current_sensor.hpp"
+#include "aruwsrc/communication/sensors/encoder/analog_sensor_encoder.hpp"
 #include "aruwsrc/control/agitator/constant_fire_rate_agitator_command.hpp"
 #include "aruwsrc/control/agitator/constant_velocity_agitator_command.hpp"
 #include "aruwsrc/control/agitator/constants/agitator_constants.hpp"
@@ -118,6 +120,27 @@ namespace sentry_control
 {
 MatchRunningGovernor matchRunningGovernor(drivers()->refSerial);
 
+aruwsrc::communication::can::AruwAnalogSensor turretMajorYawAnalogSensor(
+    drivers(),
+    turretMajor::YAW_ANALOG_SENSOR_CAN_BUS,
+    turretMajor::YAW_ANALOG_SENSOR_CAN_ID);
+
+aruwsrc::communication::sensors::encoder::AnalogSensorEncoder::Calibration
+    turretMajorYawAnalogCalibration{
+        .rawMin = turretMajor::YAW_ANALOG_RAW_MIN,
+        .rawMax = turretMajor::YAW_ANALOG_RAW_MAX,
+        .rawZero = turretMajor::YAW_ANALOG_RAW_ZERO,
+        .outputRangeRadians = turretMajor::YAW_ANALOG_OUTPUT_RANGE_RADIANS,
+    };
+
+aruwsrc::communication::sensors::encoder::AnalogSensorEncoder turretMajorYawAnalogEncoder(
+    &turretMajorYawAnalogSensor,
+    turretMajor::YAW_ANALOG_SENSOR_CHANNEL == 0
+        ? aruwsrc::communication::sensors::encoder::AnalogSensorEncoder::Channel::AI0
+        : aruwsrc::communication::sensors::encoder::AnalogSensorEncoder::Channel::AI1,
+    turretMajorYawAnalogCalibration,
+    turretMajor::YAW_ANALOG_SENSOR_INVERTED);
+
 tap::motor::DjiMotor turretMajorYawMotor(
     drivers(),
     tap::motor::MOTOR5,
@@ -126,7 +149,8 @@ tap::motor::DjiMotor turretMajorYawMotor(
     "Major Yaw Turret",
     false,
     tap::motor::DjiMotorEncoder::GEAR_RATIO_M3508 *(27.0f / 95.0f),  // pulley ratio
-    turretMajor::YAW_MOTOR_CONFIG.startEncoderValue);
+    turretMajor::YAW_MOTOR_CONFIG.startEncoderValue,
+    &turretMajorYawAnalogEncoder);
 
 struct TurretMinorMotors
 {
@@ -689,6 +713,7 @@ RemoteSafeDisconnectFunction remoteSafeDisconnectFunction(drivers());
 void initializeSubsystems()
 {
     voltageCurrentSensor.initialize();
+    turretMajorYawAnalogSensor.initialize();
     buzzer.initialize();
     chassis.initialize();
     turretWidow.initialize();
@@ -696,10 +721,6 @@ void initializeSubsystems()
     odometrySubsystem.initialize();
     transformerSubsystem.initialize();
     arucoResetSubsystem.initialize();
-
-    // rightOmni.initialize();
-    // leftOmni.initialize();
-
     turretWidowFrictionWheels.initialize();
 
     turretWidowAgitator.initialize();
--- aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_turret_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/sentry/sentry_turret_constants.hpp
@@ -94,6 +94,16 @@ static constexpr TurretMotorConfig YAW_MOTOR_CONFIG = {
     .limitMotorAngles = false,
 };
 
+// Analog yaw encoder boilerplate. Keep disabled until calibrated.
+static constexpr tap::can::CanBus YAW_ANALOG_SENSOR_CAN_BUS = tap::can::CanBus::CAN_BUS2;
+static constexpr uint16_t YAW_ANALOG_SENSOR_CAN_ID = 0x1D6;
+static constexpr uint8_t YAW_ANALOG_SENSOR_CHANNEL = 0;  // 0 = AI0, 1 = AI1
+static constexpr bool YAW_ANALOG_SENSOR_INVERTED = false;
+static constexpr uint16_t YAW_ANALOG_RAW_MIN = 0;
+static constexpr uint16_t YAW_ANALOG_RAW_MAX = 4095;
+static constexpr uint16_t YAW_ANALOG_RAW_ZERO = 0;
+static constexpr float YAW_ANALOG_OUTPUT_RANGE_RADIANS = M_TWOPI;
+
 static const tap::algorithms::transforms::Transform TURRET_MAJOR_IMU_MOUNTING_TRANSFORM(
     0,
     0,
