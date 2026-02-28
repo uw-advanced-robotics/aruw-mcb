9408cd33799cbe06adb6e5b5a5e0ecdb3e5790f8
@snowshoes <sliben@uw.edu>
2026-02-28 13:00:02 -0800

    clang p2

--- aruw-mcb-project/src/aruwsrc/robot/standard/standard_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/standard/standard_control.cpp
@@ -77,6 +77,7 @@
 //#include "aruwsrc/control/client-display/indicators/vision_assistance_indicator.hpp"
 #include "aruwsrc/control/autotune/gravity_autotune.hpp"
 #include "aruwsrc/control/autotune/spring_autotune.hpp"
+#include "aruwsrc/control/client-display/indicators/prediction_indicator.hpp"
 #include "aruwsrc/control/client-display/old-indicators/vision_target_indicator.hpp"
 #include "aruwsrc/control/cycle_state_command_mapping.hpp"
 #include "aruwsrc/control/governor/cv_on_target_governor.hpp"
@@ -108,7 +109,6 @@
 #include "aruwsrc/robot/standard/standard_chassis_constants.hpp"
 #include "aruwsrc/robot/standard/standard_drivers.hpp"
 #include "aruwsrc/robot/standard/standard_turret_subsystem.hpp"
-#include "aruwsrc/control/client-display/indicators/prediction_indicator.hpp"
 
 #ifdef PLATFORM_HOSTED
 #include "tap/communication/can/can.hpp"
@@ -638,8 +638,7 @@ PredictionIndicator predictionIndicator(
     turret,
     frictionWheelSpeedPredictor,
     transformAdapter.getWorldToVTM(),
-    30
-    );
+    30);
 
 std::vector<HudIndicator *> hudIndicators = {
     &capBankIndicator,
