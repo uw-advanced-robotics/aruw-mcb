7773aa9325144e1ef0dc83fc4e3df218c5f8cdc0
sarhaw <wusarah@uw.edu>
2026-03-29 20:34:30 -0700

    fdjaj;

--- aruw-mcb-project/src/aruwsrc/robot/blank/blank_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/blank/blank_control.cpp
@@ -48,7 +48,7 @@ NoteSequenceCommand hesAPirateCommand(buzzer, HES_A_PIRATE_NOTES, HES_A_PIRATE_N
 void initializeSubsystems()
 {
     // TODO: register initialize buzzer subsystem
-
+    
     buzzer.setDefaultCommand(&hesAPirateCommand);
 }
 
