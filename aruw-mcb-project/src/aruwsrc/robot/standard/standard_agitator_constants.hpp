510336422026b867aa8b61965d89225c096a508d
oliverstivers <oliverstivers@gmail.com>
2026-03-31 19:05:23 -0700

    agitator func

--- aruw-mcb-project/src/aruwsrc/robot/standard/standard_agitator_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/standard/standard_agitator_constants.hpp
@@ -53,12 +53,12 @@ static constexpr tap::algorithms::SmoothPidConfig AGITATOR_PID_CONFIG = {
     .errDeadzone = 0.0f,
     .errorDerivativeFloor = 0.0f,
 };
-static constexpr int AGITATOR_NUM_POCKETS = 8;          // number of balls in one rotation
+static constexpr int AGITATOR_NUM_POCKETS = 9;          // number of balls in one rotation
 static constexpr float AGITATOR_MAX_ROF = 30.0f;        // balls per second
 static constexpr float OVERSHOOT_FUDGE_FACTOR = 0.37f;  // how much agitator overshoots
 
 static constexpr aruwsrc::control::agitator::VelocityAgitatorSubsystemConfig AGITATOR_CONFIG = {
-    .gearRatio = 1.0f / 36.0f,
+    .gearRatio = 1.0f / 36.0f * (5.0f / 2.0f ),
     .agitatorMotorId = tap::motor::MOTOR3,
     .agitatorCanBusId = tap::can::CanBus::CAN_BUS1,
     .isAgitatorInverted = false,
