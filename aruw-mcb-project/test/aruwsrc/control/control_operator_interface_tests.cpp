37c0de68a3f79cf4af5b1a2e3d7387075345f5d9
Clementine Allen <callen42@uw.edu>
2026-04-02 10:39:47 -0700

    fix

--- aruw-mcb-project/test/aruwsrc/control/control_operator_interface_tests.cpp
+++ aruw-mcb-project/test/aruwsrc/control/control_operator_interface_tests.cpp
@@ -62,9 +62,9 @@ protected:
 static constexpr float MAX_3508_PRE_GEARBOX_RPM = 482.0f * (1 / (187.0f / 3591.0f));
 static constexpr float MAX_CHASSIS_WHEEL_SPEED_RPM =
     MAX_3508_PRE_GEARBOX_RPM *
-    CHASSIS_GEARBOX_RATIO;  // Max theoretical M3508 wheel speed post gearbox
+    aruwsrc::control::chassis::CHASSIS_GEARBOX_RATIO;  // Max theoretical M3508 wheel speed post gearbox
 
-// static constexpr float MAX_CHASSIS_SPEED =
+static constexpr float MAX_CHASSIS_SPEED = MAX_CHASSIS_WHEEL_SPEED_RPM;
 
 using COIChassisTuple = std::tuple<float, bool, bool, float>;
 
