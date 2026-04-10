8c215609514b2c798ab4215791aa695dc268bcbd
pie474 <chinmaymurthy474@gmail.com>
2026-04-09 17:07:53 -0700

    update

--- aruw-mcb-project/build_tools/generate_ozone_projects.py
+++ aruw-mcb-project/build_tools/generate_ozone_projects.py
@@ -45,11 +45,9 @@ def generate_ozone(env, robot=""):
 
         project_file_path = f"{env['BUILDPATH']}/{env['CONFIG_PROJECT_NAME']}.jdebug"
 
-        ip_arg = str(ARGUMENTS.get("ip", "")).lower()
-
         def use_ip(ip):
             nonlocal project_content
-            print(f"Using IP({ip}) connection...")
+            print(f"Using IP ({ip}) connection...")
             project_content = project_content.replace("${OZONE_CONNECTION}", f"Project.SetHostIF (\"IP\", \"{ip}\");")
 
         def use_usb():
@@ -57,41 +55,52 @@ def generate_ozone(env, robot=""):
             print(f"Using USB connection...")
             project_content = project_content.replace("${OZONE_CONNECTION}", f"Project.SetHostIF (\"USB\", \"\");")
 
-        def fetch_robot_ip(robot):
-            try:
-                fleet_status = get(FLEET_API_ENDPOINT)
-                fleet_status = fleet_status.json()
-                fleet_status = fleet_status["robotPis"]
-            except:
-                print("Unable to query Fleet API")
+        def fetch_robot_ip(robot_target):
+            if not robot_target:
                 return None
                 
+            try:
+                fleet_status = get(FLEET_API_ENDPOINT, timeout=6).json()
+                fleet_status = fleet_status.get("robotPis", [])
+            except Exception as e:
+                print(f"Unable to query Fleet API: {e}")
+                return None
+                
+            def get_last_update(pi):
+                try:
+                    date_str = pi.get("mcbData", {}).get("lastUpdate")
+                    return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+                except (ValueError, TypeError):
+                    return datetime.min
+
             pi_candidates = [
-                {
-                    **pi, 
-                    "mcbData": {
-                        **data,  #
-                        "lastUpdate": datetime.strptime(data["lastUpdate"], "%Y-%m-%dT%H:%M:%S.%fZ"),
-                        "lastUpdate": datetime.strftime(mcbData["lastUpdate"], "%Y-%m-%dT%H:%M:%S.%fZ")
-                    }
-                }
-                for pi in fleet_status
-                if (data := pi.get("mcbData")) and data.get("buildTarget") == robot and data.get("lastUpdate")
+                pi for pi in fleet_status
+                if pi.get("mcbData", {}).get("buildTarget") == robot_target and pi.get("mcbData", {}).get("lastUpdate")
             ]
+            
             if not pi_candidates:
-                print("No Pis with matching MCB build target")
+                print(f"No Pis found with matching MCB build target for '{robot_target}'")
                 return None
 
-            pi_match = max(pi_candidates, key=lambda p: p.get("lastUpdated"))
+            pi_match = max(pi_candidates, key=get_last_update)
             return pi_match.get("ip")
 
-        if not ip_arg and robot:
-            ip = fetch_robot_ip(robot)
-            use_ip(ip) if ip else use_usb()
-        elif ip_arg == "usb":
+        # Look for SCons variables (ip=x / usb=1) OR options (--ip=x / --usb)
+        ip_arg = ARGUMENTS.get("ip") or GetOption("ip")
+        usb_arg = "usb" in ARGUMENTS or GetOption("usb")
+
+        if ip_arg:
+            use_ip(ip_arg)
+        elif usb_arg:
             use_usb()
         else:
-            use_ip(ip_arg)
+            print("No connection argument provided, attempting to fetch IP...")
+            fetched_ip = fetch_robot_ip(robot)
+            if fetched_ip:
+                use_ip(fetched_ip)
+            else:
+                print("Fetching IP failed. Falling back to USB.")
+                use_usb()
 
         project_content = project_content.replace("${BUILD_DIR}", env['BUILDPATH'])
         project_content = project_content.replace("${BUILD_DIR_LOWER}", env['BUILDPATH'].lower())
@@ -102,8 +111,8 @@ def generate_ozone(env, robot=""):
         with open(f"{project_file_path}", "w+") as w:
             w.write(project_content)
 
-        with open("./build_tools/example_ozone_project/example.jdebug.user") as w:
-            project_user_content = w.read()
+        with open("./build_tools/example_ozone_project/example.jdebug.user") as r:
+            project_user_content = r.read()
 
         with open(f"{project_file_path}.user", "w+") as w:
             w.write(project_user_content)
@@ -112,6 +121,12 @@ def generate_ozone(env, robot=""):
     return env.AlwaysBuild(env.Alias("ozone_generate", '', action))
 
 def generate(env, **kw):
+    try:
+        AddOption('--ip', dest='ip', type='string', nargs=1, action='store', help='Specify IP address for Ozone')
+        AddOption('--usb', dest='usb', action='store_true', help='Use USB connection for Ozone')
+    except Exception:
+        pass
+
     env.AddMethod(run_ozone, "RunOzoneConfig")
     env.AddMethod(generate_ozone, "GenerateOzoneConfig")
 
