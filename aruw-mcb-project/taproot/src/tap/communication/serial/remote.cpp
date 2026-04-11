188f0db932eea729f8a7df2eff79264de7fac9ab
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2026-04-11 15:21:41 -0700

    hopefully right taproot version now??

--- aruw-mcb-project/taproot/src/tap/communication/serial/remote.cpp
+++ aruw-mcb-project/taproot/src/tap/communication/serial/remote.cpp
@@ -26,6 +26,7 @@
 #include "tap/algorithms/math_user_utils.hpp"
 #include "tap/architecture/clock.hpp"
 #include "tap/communication/serial/uart.hpp"
+#include "tap/control/remote_map_state.hpp"
 #include "tap/drivers.hpp"
 #include "tap/errors/create_errors.hpp"
 
@@ -141,8 +142,11 @@ void Remote::parseBuffer()
         RAISE_ERROR(drivers, "invalid remote joystick values");
     }
 
+    tap::control::RemoteMapState mapState;
+    mapState.initKeys(remote.key);
+    mapState.updateState(*this);
+    drivers->commandMapper.handleKeyStateChange(mapState);
     drivers->commandMapper.pollTriggerBindings();
-    drivers->commandMapper.handleKeyStateChange(*this, remote.key);
 
     remote.updateCounter++;
 }
@@ -176,9 +180,6 @@ void Remote::reset()
     remote.key = 0;
     remote.wheel = 0;
     clearRxBuffer();
-
-    drivers->commandMapper.pollTriggerBindings();
-    drivers->commandMapper.handleKeyStateChange(*this, 0);
 }
 
 uint32_t Remote::getUpdateCounter() const { return remote.updateCounter; }
