c1b422673109e72f7c07678438272976975c70a9
AidenP <aprevey@uw.edu>
2026-04-11 17:14:51 -0700

    fixed?

--- aruw-mcb-project/test/aruwsrc/communication/can/turret_mcb_can_comm_tests.cpp
+++ aruw-mcb-project/test/aruwsrc/communication/can/turret_mcb_can_comm_tests.cpp
@@ -107,11 +107,12 @@ TEST(TurretMCBCanComm, receive_limit_switch_info)
             drivers.canRxHandler.CanRxHandler::attachReceiveHandler(listener);
         });
 
-    modm::can::Message limitSwitchMsg(
-        TurretMCBCanComm::CanIDs::TURRET_STATUS_RX_CAN_ID,
-        1,
-        {1},
-        false);
+    modm::can::Message limitSwitchMsg;
+    limitSwitchMsg.identifier = TurretMCBCanComm::CanIDs::TURRET_STATUS_RX_CAN_ID;
+    limitSwitchMsg.length = 4;
+    limitSwitchMsg.setExtended(false);
+    limitSwitchMsg.data[0] = 0b1;  // limit switch depressed
+
     ON_CALL(drivers.can, getMessage(tap::can::CanBus::CAN_BUS1, _))
         .WillByDefault([&](tap::can::CanBus, modm::can::Message* message) {
             *message = limitSwitchMsg;
