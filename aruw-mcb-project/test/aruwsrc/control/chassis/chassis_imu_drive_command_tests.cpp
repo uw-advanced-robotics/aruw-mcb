e07d7b02362c53fd6f0a4c897ebeeb6e5fdf3364
oliverstivers <oliverstivers@gmail.com>
2026-04-21 19:09:45 -0700

    time to ropemaxx

--- aruw-mcb-project/test/aruwsrc/control/chassis/chassis_imu_drive_command_tests.cpp
+++ aruw-mcb-project/test/aruwsrc/control/chassis/chassis_imu_drive_command_tests.cpp
@@ -269,23 +269,28 @@ TEST_P(
     const float userX = std::get<0>(GetParam());
     const float userY = std::get<1>(GetParam());
     const float userR = std::get<2>(GetParam());
-    const float rotationLimitedMaxTranslationalSpeed =
-        chassis.calculateRotationTranslationalGain(userR) * MAX_SPEED;
-    float xExpected = tap::algorithms::limitVal(
-        userX,
-        -rotationLimitedMaxTranslationalSpeed,
-        rotationLimitedMaxTranslationalSpeed);
-    float yExpected = tap::algorithms::limitVal(
-        userY,
-        -rotationLimitedMaxTranslationalSpeed,
-        rotationLimitedMaxTranslationalSpeed);
     const float expectedAngleFromDesiredRotation =
         userR * ChassisImuDriveCommand::USER_INPUT_TO_ANGLE_DELTA_SCALAR - modm::toRadian(10);
-    tap::algorithms::rotateVector(&xExpected, &yExpected, expectedAngleFromDesiredRotation);
+    EXPECT_CALL(chassis, setDesiredOutput(_, _, _))
+        .WillOnce([&](float x, float y, float r) {
+            const float rotationLimitedMaxTranslationalSpeed =
+                chassis.calculateRotationTranslationalGain(r) * MAX_SPEED;
+            float xExpected = tap::algorithms::limitVal(
+                userX,
+                -rotationLimitedMaxTranslationalSpeed,
+                rotationLimitedMaxTranslationalSpeed);
+            float yExpected = tap::algorithms::limitVal(
+                userY,
+                -rotationLimitedMaxTranslationalSpeed,
+                rotationLimitedMaxTranslationalSpeed);
+            tap::algorithms::rotateVector(
+                &xExpected,
+                &yExpected,
+                expectedAngleFromDesiredRotation);
 
-    EXPECT_CALL(
-        chassis,
-        setDesiredOutput(FloatNear(xExpected, 1E-1), FloatNear(yExpected, 1E-1), _));
+            EXPECT_NEAR(xExpected, x, 1E-1);
+            EXPECT_NEAR(yExpected, y, 1E-1);
+        });
 
     chassisImuDriveCommand.execute();
 }
