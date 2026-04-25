abed701189c476a3609007c9e7a3b867fd9d39a1
Clementine Allen <callen42@uw.edu>
2026-04-25 18:59:48 +0000

    Find and replace never failed me (Hero 2026)
    Co-authored-by: oliverstivers <oliverstivers@gmail.com>
    Co-authored-by: jmakif <avatarred13@gmail.com>
    Co-authored-by: AidenP <aprevey@uw.edu>
    Co-authored-by: Ethan <ethanmwgo@gmail.com>
    Co-authored-by: Ethan Go <goeth25@uw.edu>
    Co-authored-by: sarhaw <wusarah@uw.edu>
    Co-authored-by: pie474 <chinmaymurthy474@gmail.com>

--- aruw-mcb-project/src/main.cpp
+++ aruw-mcb-project/src/main.cpp
@@ -55,7 +55,7 @@ tap::arch::PeriodicMilliTimer sendMotorTimeout(1000.0f / MAIN_LOOP_FREQUENCY);
 using namespace aruwsrc::standard;
 #elif defined(ALL_SENTRIES)
 using namespace aruwsrc::sentry;
-#elif defined(TARGET_HERO_ZERO)
+#elif defined(TARGET_HERO_NEPTUNE)
 using namespace aruwsrc::hero;
 #elif defined(TARGET_DRONE)
 using namespace aruwsrc::drone;
