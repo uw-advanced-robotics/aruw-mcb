c123d59beabd25a95f04a1642e67252d9200f74d
pie474 <chinmaymurthy474@gmail.com>
2026-05-03 17:19:59 -0700

    reintroduce turret gravity compensator params for tests

--- aruw-mcb-project/src/aruwsrc/robot/engineer/engineer_turret_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/engineer/engineer_turret_constants.hpp
@@ -63,6 +63,9 @@ static constexpr TurretMotorConfig PITCH_MOTOR_CONFIG = {
     .limitMotorAngles = true,
 };
 
+static constexpr algorithms::TurretGravitationalForceOffset::TurretGravityParams
+    TURRET_GRAVITY_CONFIG{.cgX = 0.0f, .cgZ = 0.0f, .gravityCompensatorMax = 0.0f};
+
 namespace world_rel_turret_imu
 {
 static constexpr tap::algorithms::SmoothPidConfig YAW_POS_PID_CONFIG = {
