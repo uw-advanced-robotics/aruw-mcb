f9843baf3a1a2440fa9fad2a60b19558a72f10b2
Iris Litiu <irisl2@uw.edu>
2026-05-15 06:44:51 +0000

    imu oversampling
    Co-authored-by: Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>

--- aruw-mcb-project/test/aruwsrc/control/turret/algorithms/world_frame_turret_imu_turret_controller_tests.cpp
+++ aruw-mcb-project/test/aruwsrc/control/turret/algorithms/world_frame_turret_imu_turret_controller_tests.cpp
@@ -298,8 +298,11 @@ TEST_F(
 
 // Pitch controller tests
 
-static constexpr algorithms::TurretGravitationalForceOffset::TurretGravityParams
-    TURRET_GRAVITY_CONFIG_TEST{.cgX = 0.1f, .cgZ = 0.1f, .gravityCompensatorMax = 1000.0f};
+static constexpr aruwsrc::control::turret::algorithms::TurretGravitationalForceOffset::
+    TurretGravityParams TURRET_GRAVITY_CONFIG_TEST{
+        .cgX = 0.1f,
+        .cgZ = 0.1f,
+        .gravityCompensatorMax = 1000.0f};
 
 static int16_t computeCGOffset(float pitchAngleFromCenter)
 {
--- aruw-mcb-project/test/aruwsrc/control/turret/user/turret_user_control_command_tests.cpp
+++ aruw-mcb-project/test/aruwsrc/control/turret/user/turret_user_control_command_tests.cpp
@@ -37,8 +37,11 @@ using namespace testing;
 
 #define SETUP_TEST()
 
-static constexpr algorithms::TurretGravitationalForceOffset::TurretGravityParams
-    TURRET_GRAVITY_CONFIG_TEST{.cgX = 0.1f, .cgZ = 0.1f, .gravityCompensatorMax = 1000.0f};
+static constexpr aruwsrc::control::turret::algorithms::TurretGravitationalForceOffset::
+    TurretGravityParams TURRET_GRAVITY_CONFIG_TEST{
+        .cgX = 0.1f,
+        .cgZ = 0.1f,
+        .gravityCompensatorMax = 1000.0f};
 namespace
 {
 float computeGravitationalForceOffset(const float pitchAngleRad)
