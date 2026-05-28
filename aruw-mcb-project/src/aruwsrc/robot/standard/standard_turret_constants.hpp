4f6ee8a0b48f91f9581e94fce0045a24c5733320
AidenP <aprevey@uw.edu>
2026-05-28 03:28:09 -0700

    fix turret constants

--- aruw-mcb-project/src/aruwsrc/robot/standard/standard_turret_constants.hpp
+++ aruw-mcb-project/src/aruwsrc/robot/standard/standard_turret_constants.hpp
@@ -69,9 +69,7 @@ inline const tap::algorithms::transforms::Transform TURRET_MCB_ISM330_MOUNTING_T
 
 static constexpr tap::can::CanBus CAN_BUS_MOTORS = tap::can::CanBus::CAN_BUS1;
 #if defined(TARGET_STANDARD_NULL)
-// static constexpr tap::motor::MotorId PITCH_MOTOR_ID = tap::motor::MOTOR6;
-// static constexpr tap::motor::MotorId YAW_MOTOR_ID = tap::motor::MOTOR5;
-static constexpr tap::motor::MotorId PITCH_MOTOR_ID = tap::motor::MOTOR7;
+static constexpr tap::motor::MotorId PITCH_MOTOR_ID = tap::motor::MOTOR6;
 static constexpr tap::motor::MotorId YAW_MOTOR_ID = tap::motor::MOTOR5;
 #else
 static constexpr tap::motor::MotorId PITCH_MOTOR_ID = tap::motor::MOTOR7;
