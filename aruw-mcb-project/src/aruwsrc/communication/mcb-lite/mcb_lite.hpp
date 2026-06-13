be998c87b0d4f4ee10720f98d775bb30225887eb
Swara Seshadri <swara_s@outlook.com>
2026-06-13 22:26:05 +0000

    engineer mcblite stuff
    Co-authored-by: swaras <swaras@uw.edu>
    Co-authored-by: pie474 <chinmaymurthy474@gmail.com>

--- aruw-mcb-project/src/aruwsrc/communication/mcb-lite/mcb_lite.hpp
+++ aruw-mcb-project/src/aruwsrc/communication/mcb-lite/mcb_lite.hpp
@@ -31,6 +31,7 @@
 
 #include "message_types.hpp"
 #include "virtual_analog.hpp"
+#include "virtual_analog_sensor.hpp"
 #include "virtual_can_encoder.hpp"
 #include "virtual_digital.hpp"
 #include "virtual_imu.hpp"
@@ -91,6 +92,10 @@ private:
 
     void processVoltageCurrentMessage(const ReceivedSerialMessage& completeMessage);
 
+    void processAnalogSensorMessage(const ReceivedSerialMessage& completeMessage);
+
+    void processServoFeedbackMessage(const ReceivedSerialMessage& completeMessage);
+
     tap::communication::serial::Uart::UartPort port;
 
     IMUMessage currentIMUData;
@@ -103,6 +108,7 @@ private:
     VirtualCanEncoder* can2Encoders[8];
 
     VirtualVoltageCurrentSensor* voltageCurrentSensor;
+    VirtualAnalogSensor* analogSensor;
 
     bool initialized = false;
 };
