a41f732d2abe0cbe360c967cd322a788db1581cf
Swara Seshadri <swara_s@outlook.com>
2026-06-30 21:24:29 +0000

    not negative
    Co-authored-by: swaras <swaras@uw.edu>

--- aruw-mcb-project/src/aruwsrc/control/control_operator_interface.cpp
+++ aruw-mcb-project/src/aruwsrc/control/control_operator_interface.cpp
@@ -206,14 +206,14 @@ float ControlOperatorInterface::getTurretPitchInput(uint8_t turretID)
         case 0:
             return -drivers->remote.getChannel(Remote::Channel::RIGHT_VERTICAL) +
                    static_cast<float>(limitVal<int16_t>(
-                       drivers->remote.getMouseY(),
+                       -drivers->remote.getMouseY(),
                        -USER_MOUSE_PITCH_MAX,
                        USER_MOUSE_PITCH_MAX)) *
                        getUserMousePitchScalar();
         case 1:
             return -drivers->remote.getChannel(Remote::Channel::LEFT_VERTICAL) +
                    static_cast<float>(limitVal<int16_t>(
-                       drivers->remote.getMouseY(),
+                       -drivers->remote.getMouseY(),
                        -USER_MOUSE_PITCH_MAX,
                        USER_MOUSE_PITCH_MAX)) *
                        getUserMousePitchScalar();
