25fb11802cf8c610e90ca7f0c50ba159cac6d237
swaras <swaras@uw.edu>
2026-07-02 14:23:01 -0400

    yay matrix hud

--- aruw-mcb-project/src/aruwsrc/robot/standard/standard_control.cpp
+++ aruw-mcb-project/src/aruwsrc/robot/standard/standard_control.cpp
@@ -703,7 +703,7 @@ VisionTargetIndicator visionTargetIndicator(
 
 std::vector<HudIndicator*> hudIndicators = {
     &capBankIndicator,
-    &textHudIndicators,
+    &positionHudIndicators,
     &ammoIndicator,
     &circleCrosshair,
     &damageIndicator,
